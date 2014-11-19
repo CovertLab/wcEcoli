d5c6a6b04aa5771afb5de3bed3ee13272740bb58
nruggero <nruggero@server>
2014-11-19 11:31:19 -0800

    Importing sparkline functions into growth rate control analysis script

--- models/ecoli/analysis/single/growthRateControl.py
+++ models/ecoli/analysis/single/growthRateControl.py
@@ -19,43 +19,10 @@ import cPickle
 
 import wholecell.utils.constants
 from wholecell.utils import units
+from wholecell.utils.sparkline import sparklineAxis, setAxisMaxMinY, setAxisMaxMinX
 
 FONT = {'size'	:	8}
 PPGPP_IDX = 2
-
-def setAxisMaxMinY(axis, data):
-	if np.isnan(data).all():
-		return
-
-	ymax = np.nanmax(data)
-	ymin = 0
-	if ymin == ymax:
-		axis.set_yticks([ymin])
-	else:
-		axis.set_yticks([ymin, ymax])
-
-def setAxisMaxMinX(axis, data):
-	if np.isnan(data).all():
-		return
-
-	xmax = np.nanmax(data)
-	xmin = 0
-	if xmin == xmax:
-		axis.set_xticks([xmin])
-	else:
-		axis.set_xticks([xmin, xmax])
-
-def sparklineAxis(axis, x, y, tickPos, lineType, color):
-	axis.plot(x, y, linestyle = 'steps' + lineType, color = color, linewidth = 2)
-	axis.spines['top'].set_visible(False)
-	axis.spines['bottom'].set_visible(False)
-	axis.yaxis.set_ticks_position(tickPos)
-	axis.xaxis.set_ticks_position('none')
-	axis.tick_params(which = 'both', direction = 'out')
-	#axis.tick_params(labelbottom = 'off')
-	for tl in axis.get_yticklabels():
-		tl.set_color(color)
-
 
 def main(simOutDir, plotOutDir, plotOutFileName, kbFile):
 
