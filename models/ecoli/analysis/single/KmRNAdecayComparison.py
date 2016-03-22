403d448618f813088a39cae41aa2bcc77f2827e5
javiercarrera <jcarrera@stanford.edu>
2016-03-22 12:27:23 -0700

    Added figures RNA decay model

---
+++ models/ecoli/analysis/single/KmRNAdecayComparison.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+"""
+Plots counts of rna degraded and the resulting free NMPs
+
+@author: Javier Carrera
+@organization: Covert Lab, Department of Bioengineering, Stanford University
+@date: Created 1/15/2015 - Updated 8/10/2015
+"""
+
+from __future__ import division
+
+import argparse
+import os
+
+import numpy as np
+import matplotlib
+matplotlib.use("Agg")
+from matplotlib import pyplot as plt
+import cPickle
+
+import wholecell.utils.constants
+from wholecell.io.tablereader import TableReader
+
+FONT = {
+		'size'	:	14
+		}
+
+
+def setAxisMaxMin(axis, data):
+	ymax = np.max(data)
+	ymin = 0
+	if ymin == ymax:
+		axis.set_yticks([ymin])
+	else:
+		axis.set_yticks([ymin, ymax])
+
+def sparklineAxis(axis, x, y, tickPos, lineType, color):
+	axis.plot(x, y, linestyle = 'steps' + lineType, color = color, linewidth = 2)
+	axis.spines['top'].set_visible(False)
+	axis.spines['bottom'].set_visible(False)
+	axis.yaxis.set_ticks_position(tickPos)
+	axis.xaxis.set_ticks_position('none')
+	axis.tick_params(which = 'both', direction = 'out')
+	axis.tick_params(labelbottom = 'off')
+	for tl in axis.get_yticklabels():
+		tl.set_color(color)
+
+
+def main(simOutDir, plotOutDir, plotOutFileName, simDataFile, validationDataFile, metadata = None):
+
+	if not os.path.isdir(simOutDir):
+		raise Exception, "simOutDir does not currently exist as a directory"
+
+	if not os.path.exists(plotOutDir):
+		os.mkdir(plotOutDir)
+
+	# Load data from KB
+	sim_data = cPickle.load(open(simDataFile, "rb"))
+
+	KmFirstOrderDecay = np.log10(sim_data.process.rna_decay.KmFirstOrderDecay)
+	KmNonLinearDecay = np.log10(sim_data.process.transcription.rnaData["KmEndoRNase"].asNumber())
+
+	FC = np.log10(1 - (sim_data.process.transcription.rnaData["KmEndoRNase"].asNumber() / sim_data.process.rna_decay.KmFirstOrderDecay))
+
+	# Compute deviation
+	Error = np.average(np.abs(sim_data.process.rna_decay.KmFirstOrderDecay 
+							- sim_data.process.transcription.rnaData["KmEndoRNase"].asNumber())
+							/ sim_data.process.rna_decay.KmFirstOrderDecay 
+				* 100)
+
+	# Plotting
+	plt.figure(figsize = (5.5, 11))
+	matplotlib.rc('font', **FONT)
+	max_yticks = 5
+
+	plt.subplot(2,1,1)
+	plt.plot(KmFirstOrderDecay, KmNonLinearDecay, 'o', markeredgecolor = 'k', markerfacecolor = 'none')
+
+	minLine = np.round(1.2 * min(KmFirstOrderDecay.min(), KmNonLinearDecay.min()))
+	plt.plot([minLine, 0], [minLine, 0], '--r')
+
+	plt.xlabel("Km First Order Decay (Log10, M)", fontsize = 14)
+	plt.ylabel("Km Non-linear Decay (Log10, M)", fontsize = 14)	
+	plt.title("Relative error = %.2f%%" % Error, fontsize = 16) 
+	print np.corrcoef(KmFirstOrderDecay, KmNonLinearDecay)[0,1]
+
+	plt.subplot(2,1,2)
+	GprimeKm = sim_data.process.rna_decay.KmConvergence
+	FprimeKm = np.log10(1 - GprimeKm[GprimeKm < 1])
+	plt.hist(FprimeKm)
+	
+	plt.ylabel("Number of genes", fontsize = 14)
+	plt.xlabel("Log10(1 - g\'(Km))", fontsize = 14)
+	PercentageConvergence = len(GprimeKm[GprimeKm < 1.]) / float(len(GprimeKm)) * 100.
+	plt.title("Convergence of %.0f%% Km\'s" % PercentageConvergence, fontsize = 16) 
+
+	plt.subplots_adjust(hspace = 0.3, wspace = 0.35)
+
+	from wholecell.analysis.analysis_tools import exportFigure
+	exportFigure(plt, plotOutDir, plotOutFileName, metadata)
+
+if __name__ == "__main__":
+	defaultSimDataFile = os.path.join(
+			wholecell.utils.constants.SERIALIZED_KB_DIR,
+			wholecell.utils.constants.SERIALIZED_KB_MOST_FIT_FILENAME
+			)
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument("simOutDir", help = "Directory containing simulation output", type = str)
+	parser.add_argument("plotOutDir", help = "Directory containing plot output (will get created if necessary)", type = str)
+	parser.add_argument("plotOutFileName", help = "File name to produce", type = str)
+	parser.add_argument("--simDataFile", help = "KB file name", type = str, default = defaultSimDataFile)
+	parser.add_argument("--validationDataFile", help = "KB file name", type = str, default = "None")
+
+	args = parser.parse_args().__dict__
+
+	main(args["simOutDir"], args["plotOutDir"], args["plotOutFileName"], args["simDataFile"], args["validationDataFile"])
