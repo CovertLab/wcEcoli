85124f078bde10954f8f2839bcfddd8a7adc6704
heejochoi <heejochoi@gmail.com>
2016-04-19 17:58:01 -0700

    Adding uptakeRate variant to explore environments 4 through 9 at different doubling times.

---
+++ models/ecoli/sim/variants/uptake_rate.py
@@ -0,0 +1,44 @@
+# from wholecell.utils import units
+from reconstruction.ecoli.knowledge_base_raw import KnowledgeBaseEcoli
+from reconstruction.ecoli.fit_sim_data_1 import fitSimData_1
+
+CONTROL_OUTPUT = dict(
+	shortName = "control",
+	desc = "Control simulation"
+	)
+
+def uptakeRateTotalIndices(sim_data):
+	nEnvironments = len(sim_data.envDict)
+	return nEnvironments
+
+
+def uptakeRate(sim_data, index):
+	# if index == 0:
+	# 	return CONTROL_OUTPUT, sim_data
+
+	environments = {
+		1: "000004_unbound_glucose", 
+		2: "000005_unbound_glucose_aa",
+		3: "000006_unbound_glycerol",
+		4: "000007_unbound_glycerol_aa",
+		5: "000008_unbound_succinate",
+		6: "000009_unbound_succinate_aa",
+	}
+
+	growthRates = {
+		1: 44.,
+		2: 25.,
+		3: 59.,
+		4: 33.,
+		5: 80.,
+		6: 37.,
+	}
+
+	raw_data = KnowledgeBaseEcoli()
+	sim_data = fitSimData_1(raw_data, doubling_time = growthRates[int(index)])
+	sim_data.environment = environments[index]
+
+	return dict(
+		shortName = "{}_uptake".format(environments[index]),
+		desc = "Simulation of uptake rate {}.".format(environments[index])
+		), sim_data
