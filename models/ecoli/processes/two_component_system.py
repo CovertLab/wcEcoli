dcfd427f1260bccc004eb978670eaaa4b8d1093f
heejochoi <heejochoi@gmail.com>
2016-05-12 10:11:04 -0700

    Adding two component system process.

---
+++ models/ecoli/processes/two_component_system.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+"""
+Two component system
+
+Two component system sub-model
+
+@author: Heejo Choi
+@organization: Covert Lab, Department of Bioengineering, Stanford University
+@date: Created 5/3/2016
+"""
+
+from __future__ import division
+
+import numpy as np
+import scipy.integrate
+from wholecell.utils import units
+import theano.tensor as T
+import theano
+
+import wholecell.processes.process
+
+
+class TwoComponentSystem(wholecell.processes.process.Process):
+	""" Two component system """
+
+	_name = "TwoComponentSystem"
+
+	# Constructor
+	def __init__(self):
+
+		super(TwoComponentSystem, self).__init__()
+
+
+	# Construct object graph
+	def initialize(self, sim, sim_data):
+		super(TwoComponentSystem, self).initialize(sim, sim_data)
+
+		self.nAvogadro = sim_data.constants.nAvogadro.asNumber(1 / units.mol)
+		self.cellDensity = sim_data.constants.cellDensity.asNumber(units.g / units.L)
+
+		# Create matrices and vectors
+
+		self.stoichMatrix = sim_data.process.two_component_system.stoichMatrix().astype(np.int64)
+		self.Rp = sim_data.process.two_component_system.Rp
+		self.Pp = sim_data.process.two_component_system.Pp
+		self.derivatives = sim_data.process.two_component_system.derivatives
+		self.derivativesJacobian = sim_data.process.two_component_system.derivativesJacobian
+		self.metsToRxnFluxes = sim_data.process.two_component_system.metsToRxnFluxes
+
+		self.fluxesAndMoleculesToSS = sim_data.process.two_component_system.fluxesAndMoleculesToSS
+
+		# Build views
+
+		moleculeNames = sim_data.process.two_component_system.moleculeNames
+
+		self.molecules = self.bulkMoleculesView(moleculeNames)
+
+	def calculateRequest(self):
+		moleculeCounts = self.molecules.total()
+
+		cellMass = (self.readFromListener("Mass", "cellMass") * units.fg).asNumber(units.g)
+		cellVolume = cellMass / self.cellDensity
+
+		self.rxnFluxes, self.req = self.fluxesAndMoleculesToSS(moleculeCounts, cellVolume, self.nAvogadro)
+
+		self.molecules.requestIs(self.req)
+
+	def evolveState(self):
+		moleculeCounts = self.molecules.counts()
+
+		rxnFluxes = self.rxnFluxes.copy()
+
+		# Kill "bad" reactions where we have insufficient metabolites
+		for badMetIdx in np.where(self.req > moleculeCounts)[0]:
+			rxnFluxes[self.stoichMatrix[badMetIdx, :]!= 0] = 0
+
+		assert(np.all(moleculeCounts + np.dot(self.stoichMatrix, rxnFluxes) >= 0))
+
+		self.molecules.countsInc(
+			np.dot(self.stoichMatrix, rxnFluxes)
+			)
