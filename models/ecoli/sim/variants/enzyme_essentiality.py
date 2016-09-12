c872848d81ec355e28f190abd9a9bc2b52a56ec9
Morgan L. Paull <mpaull@stanford.edu>
2016-09-12 12:43:53 -0700

    Add ability to use manual list of enzymeKnockouts to enzymeEssentiality

--- models/ecoli/sim/variants/enzyme_essentiality.py
+++ models/ecoli/sim/variants/enzyme_essentiality.py
@@ -1,3 +1,42 @@
+knockoutEnzymes = [
+"FDNG-MONOMER[p]",
+"FORMATEDEHYDROGH-MONOMER[c]",
+"G7106-MONOMER[c]",
+"MONOMER0-12[i]",
+"XANTHOSINEPHOSPHORY-MONOMER[c]",
+"ACPSUB-MONOMER[c]",
+"ATOA-MONOMER[c]",
+"CARBODEHYDRAT-MONOMER[c]",
+"DMSA-MONOMER[i]",
+"EG11234-MONOMER[c]",
+"EG11245-MONOMER[c]",
+"EG12279-MONOMER[c]",
+"FUCPALDOL-MONOMER[c]",
+"G6140-MONOMER[c]",
+"G6710-MONOMER[c]",
+"G6716-MONOMER[m]",
+"G7145-MONOMER[c]",
+"GLYOCARBOLIG-MONOMER[c]",
+"LYXK-MONOMER[c]",
+"MONOMER-162[c]",
+"NARK-MONOMER[i]",
+"NIKA-MONOMER[i]",
+"NIKB-MONOMER[i]",
+"NIKC-MONOMER[i]",
+"RIBULPEPIM-MONOMER[c]",
+"TAGAALDOL1-MONOMER[c]",
+"XYLH-MONOMER[i]",
+"YCBM-MONOMER[i]",
+"YJCW-MONOMER[i]",
+"BETAGALACTOSID-MONOMER[c]",
+"G6277-MONOMER[c]",
+"G6711-MONOMER[c]",
+"G7234-MONOMER[c]",
+"G7921-MONOMER[o]",
+"GALACTOACETYLTRAN-MONOMER[c]",
+"KDPB-MONOMER[i]",
+"TNAB-MONOMER[i]",
+]
 
 CONTROL_OUTPUT = dict(
 	shortName = "control",
@@ -5,7 +44,11 @@ CONTROL_OUTPUT = dict(
 	)
 
 def enzymeEssentialityTotalIndices(sim_data):
-	nEnzymes = len(sim_data.process.metabolism.rescueEnzymes)
+	if len(knockoutEnzymes) > 0:
+		nEnzymes = len(knockoutEnzymes)
+	else:
+		nEnzymes = len(sim_data.process.metabolism.rescueEnzymes)
+
 	nConditions = nEnzymes + 1
 	return nConditions
 
@@ -20,7 +63,10 @@ def enzymeEssentiality(sim_data, index):
 
 	enzymeIndex = (index - 1) % nConditions
 
-	enzymeID = sorted(sim_data.process.metabolism.rescueEnzymes)[enzymeIndex]
+	if len(knockoutEnzymes) > 0:
+		enzymeID = knockoutEnzymes[enzymeIndex]
+	else:
+		enzymeID = sorted(sim_data.process.metabolism.rescueEnzymes)[enzymeIndex]
 
 	sim_data.process.metabolism.rescueEnzymes.remove(enzymeID)
 
