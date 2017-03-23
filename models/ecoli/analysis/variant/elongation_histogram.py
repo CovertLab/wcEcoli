e410ba0be0716fa76573dacdaf82cec176b9dc78
nruggero <nruggero@server>
2017-03-22 17:54:09 -0700

    Fixed up plot for thesis

--- models/ecoli/analysis/variant/elongation_histogram.py
+++ models/ecoli/analysis/variant/elongation_histogram.py
@@ -24,6 +24,8 @@ from wholecell.io.tablereader import TableReader
 import wholecell.utils.constants
 from wholecell.utils import units
 
+from wholecell.utils.sparkline import whitePadSparklineAxis
+
 FONT_SIZE = 7
 
 def main(inputDir, plotOutDir, plotOutFileName, validationDataFile = None, metadata = None):
@@ -37,9 +39,12 @@ def main(inputDir, plotOutDir, plotOutFileName, validationDataFile = None, metad
 
 	allCells = ap.get_cells()
 
-	# sim_data = cPickle.load(open(ap.get_variant_kb(0), "rb"))
 
-	# fig = plt.figure(figsize = (8.5, 11))
+	fig = plt.figure()
+	fig.set_figwidth(6)
+	fig.set_figheight(6)
+
+	title_list = ["Glucose minimal anaerobic\n" + r"$\tau = $" + "100 min", "Glucose minimal\n" + r"$\tau = $" + "44 min", "Glucose minimal\n+ 20 amino acids\n" + r"$\tau = $" + "22 min"]
 
 
 	for var_idx in range(ap.n_variant):
@@ -64,15 +69,40 @@ def main(inputDir, plotOutDir, plotOutFileName, validationDataFile = None, metad
 			net_variant_histogram += actualElongationHistogram.sum(axis=0)
 			net_variant_noterm_histogram += elongationsNonTerminatingHist.sum(axis=0)
 
-		ax0 = plt.subplot2grid((ap.n_variant,1), (var_idx,0))
-		ax0.bar(np.arange(0,21) , net_variant_histogram / net_variant_histogram.sum())
+		idx_dict = {0 : 1,
+					1 : 0,
+					2: 2}
+		ax0 = plt.subplot2grid((ap.n_variant,1), (idx_dict[var_idx],0))
+		ax0.bar(np.arange(0,21) , net_variant_histogram / net_variant_histogram.sum(), color="black", linewidth=0, alpha=0.7)
 
 		hist_avg = np.dot(net_variant_histogram / net_variant_histogram.sum().astype(np.float), np.arange(0,21))
-		ax0.axvline(hist_avg, linewidth=2)
-		ax0.set_xlabel("Number of elongations ribosome accomplished during timestep")
+		ax0.axvline(hist_avg, linewidth=2, color="black")
+		ax0.set_xlim([0,21])
+
+		if var_idx == 2:
+			whitePadSparklineAxis(ax0)
+			ax0.set_xlabel("Number of elongations ribosome accomplished during timestep", fontsize=FONT_SIZE)
+		else:
+			whitePadSparklineAxis(ax0, False)
+
+
+		if var_idx == 1:
+			ax0.set_ylabel(title_list[0], fontsize=FONT_SIZE)
+		elif var_idx == 0:
+			ax0.set_ylabel(title_list[1], fontsize=FONT_SIZE)
+		elif var_idx == 2:
+			ax0.set_ylabel(title_list[2], fontsize=FONT_SIZE)
 
 
 
+		for a in [ax0]:
+			for tick in a.yaxis.get_major_ticks():
+				tick.label.set_fontsize(FONT_SIZE) 
+			for tick in a.xaxis.get_major_ticks():
+				tick.label.set_fontsize(FONT_SIZE) 
+
+
+	plt.subplots_adjust(left = 0.2, bottom = 0.14)
 
 	from wholecell.analysis.analysis_tools import exportFigure
 	exportFigure(plt, plotOutDir, plotOutFileName, metadata)
