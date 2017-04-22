f424acd8f27b0d1d194e2ca6f1c803d026989032
nruggero <nruggero@server>
2017-04-22 07:49:17 -0700

    Powered up lpad detectlostruns

--- detect_lostruns_time.sh
+++ detect_lostruns_time.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 while [ "1" = "1" ]; do
-		echo $(date +"%Y.%m.%d.%S.%N") lpad detect_lostruns --refresh
+		echo $(date +"%Y.%m.%d.%S.%N") lpad detect_lostruns --refresh/rerun
         lpad detect_lostruns --refresh
+        lpad detect_lostruns --time 3600 --rerun
         echo "Waiting 3 min"
         sleep 180
 done
