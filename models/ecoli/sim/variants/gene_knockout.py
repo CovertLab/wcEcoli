f5b46043279952b396f3b162a9794875343f1e79
Nicole Ferraro <nferraro@sherlock-ln02.stanford.edu>
2017-05-29 15:06:54 -0700

    Knockout variants

--- models/ecoli/sim/variants/gene_knockout.py
+++ models/ecoli/sim/variants/gene_knockout.py
@@ -12,25 +12,23 @@ def geneKnockoutTotalIndices(sim_data):
 
 def geneKnockout(sim_data, index):
 	# Knocks-out genes in order
-
 	nConditions = geneKnockoutTotalIndices(sim_data)
 
 	if index % nConditions == 0:
 		return CONTROL_OUTPUT, sim_data
 
 	geneIndex = (index - 1) % nConditions
-
-	synthProb = sim_data.process.transcription.rnaData["synthProb"]
-
-	synthProb[geneIndex] = 0.0
-
-	synthProb /= synthProb.sum()
-
-	sim_data.process.transcription.rnaData["synthProb"] = synthProb
-
 	geneID = sim_data.process.transcription.rnaData["id"][geneIndex]
+	KO_dict = {geneID: 0.0}
+	sim_data.genetic_perturbations = KO_dict
+
+    #Test in conditions with amino acids added, per experimental data
+	sim_data.nutrientsTimeSeriesLabel = "000003_aa"
+
+	sim_data.process.transcription.rnaExpression[sim_data.condition][geneIndex] = 0.0
 
 	return dict(
 		shortName = "{}_KO".format(geneID),
 		desc = "Complete knockout of {}.".format(geneID)
 		), sim_data
+
---
+++ models/ecoli/sim/variants/gene_knockout_essential.py
@@ -0,0 +1,62 @@
+import cPickle
+import numpy as np
+import pandas as pd
+import wholecell.states.bulk_molecules
+from wholecell.utils.fitting import normalize
+
+CONTROL_OUTPUT = dict(
+	shortName = "control",
+	desc = "Control simulation"
+	)
+
+def geneKnockoutEssential(sim_data, validation_data, index):
+	
+	#Read in all essential genes as defined in validation data
+	#esGenes = validation_data.essentialGenes.essentialGenes
+	
+	#Reading in list of genes that were not impacted in first generation
+	#gene_file = '~/wcEcoli/function_no_impact_geneIDs.txt'
+	gene_file = '~/wcEcoli/basal_genes_large_impact.txt'
+	prot_file = '~/wcEcoli/validation/ecoli/flat/essentialGenes.tsv'
+	df_esGenes = pd.read_table(gene_file, sep='\t')
+	df_prot = pd.read_table(prot_file, sep='\t')
+	esGenes = df_esGenes['Genes'].tolist()
+	proteinIDs = df_prot['proteinID'].tolist()
+
+	nConditions = len(esGenes)
+
+	if index % nConditions == 0:
+		#sim_data.nutrientsTimeSeriesLabel = "000003_aa"
+		#sim_data.condition = "with_aa"
+		return CONTROL_OUTPUT, sim_data
+
+	esGene = esGenes[index - 1] + '_RNA[c]'
+	protID = proteinIDs[np.where(df_prot['FrameID'] == esGenes[index - 1])[0]]
+
+	scaling_factor = 0.5
+	KO_dict = {esGene: scaling_factor}
+	sim_data.genetic_perturbations = KO_dict
+	ind = np.where(sim_data.process.transcription.rnaData["id"] == esGene)[0][0]
+
+	sim_data.process.transcription.rnaExpression[sim_data.condition][ind] = 0.0
+	normalize(sim_data.process.transcription.rnaExpression[sim_data.condition])
+
+	#Mapping gene -> enzyme -> catalyst -> complex
+	try:
+		enzyme = sim_data.process.metabolism.enzymeIdList.index(protID)
+		#rxns = [x for x in sim_data.process.metabolism.reactionCatalystsList if enGene[0:len(enGene)-3] in x]
+		EN_dict = {protID: scaling_factor}
+		sim_data.enzyme_perturbations = EN_dict
+	
+	except:
+		return dict(
+			shortName = "{}_KO".format(esGene),
+			desc = "Complete knockout of {}.".format(esGene)
+			), sim_data
+
+
+	return dict(
+			shortName = "{}_KO".format(esGene),
+			desc = "Complete knockout of {}.".format(esGene)
+			), sim_data
+
