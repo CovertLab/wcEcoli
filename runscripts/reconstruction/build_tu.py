ef66a9cf0da2b0ae3fe913ba270d352389dc75c9
tahorst <thorst@stanford.edu>
2018-05-16 14:10:15 -0700

    Remove TU .pkl files

--- runscripts/reconstruction/build_tu.py
+++ runscripts/reconstruction/build_tu.py
@@ -1,9 +1,8 @@
 '''
 Script to generate flat file for TU information
-Inputs (from transcription_units flat directory:
-	geneTUMatrix.pkl - matrix of all genes to the transcription unit they belong
-	geneTUMatrix_Rows.pkl - row labels for the matrix (genes)
-	geneTUMatrix_Columns.pkl - column labels for the matrix (list of genes in TU)
+Input (from transcription_units flat directory):
+	tu.json - information about gene to transcription unit mapping generated from
+		$PI_SCRATCH/mialy/TranscriptionUnitStructure/process_tu_matrix/create_sparse_tu_matrix.py
 
 TODO - better annotation
 TODO - explain choices
@@ -27,71 +26,16 @@ TU_FLAT_OUTPUT_FILE = os.path.join(FLAT_DIR, 'transcription_units.tsv')
 TU_ID_FORMAT = 'TU{:05d}'
 
 TU_DIR = os.path.join(FLAT_DIR, 'transcription_units')
-MATRIX_FILE = os.path.join(TU_DIR, 'geneTUMatrix.pkl')
-ROWS_FILE = os.path.join(TU_DIR, 'geneTUMatrix_Rows.pkl')
-COLS_FILE = os.path.join(TU_DIR, 'geneTUMatrix_Columns.pkl')
 JSON_OUTPUT_FILE = os.path.join(TU_DIR, 'tu.json')
 
 OTHER_RNA_IDX = 10  # molecular weight index for other RNA
 N_MW_TYPES = 11  # number of molecular weight groups that are tracked
 
-def load_tu_matrix(raw_data, rerun=True):
-	if rerun:
-		# read cPickle data from Mialy
-		with open(MATRIX_FILE) as f:
-			mat = np.array(cPickle.load(f))
-		with open(ROWS_FILE) as f:
-			rows = cPickle.load(f)
-		with open(COLS_FILE) as f:
-			cols = cPickle.load(f)
-
-		# map ecocyc ID to wcm ID for row and col annotation
-		ec_to_wc_gene_map =  {gene['id']: gene['rnaId'] for gene in raw_data.genes}
-		wcm_rows = [ec_to_wc_gene_map[gene] for gene in rows]
-		wcm_cols = [sorted(set([ec_to_wc_gene_map[gene] for gene in col if gene in ec_to_wc_gene_map])) for col in cols]
-
-		# remove phantom genes in data but not included in model
-		n_genes_in_tu = np.sum(mat, axis=0)
-		wcm_cols = np.array(wcm_cols)[n_genes_in_tu > 0].tolist()
-		mat = mat[:, n_genes_in_tu > 0]
-
-		assert np.all(np.sum(mat, axis=0) == np.array([len(col) for col in wcm_cols]))
-
-		# represent matrix more compactly
-		mati, matj = np.where(mat)
-
-		# create new entries mapping genes that do not appear in data to their own operon
-		for gene_idx in np.where(np.sum(mat, axis=1) == 0)[0]:
-			mati = np.hstack((mati, gene_idx))
-			matj = np.hstack((matj, len(wcm_cols)))
-			wcm_cols.append([wcm_rows[gene_idx]])
-
-		# sort rows by name to match order in sim_data for synth prob
-		sorted_indices = {j: i for i, j in enumerate(np.argsort(wcm_rows))}
-		mati_sorted = np.array([sorted_indices[idx] for idx in mati])
-
-		# reconstruct matrix with new columns
-		dims = (len(wcm_rows), len(wcm_cols))
-		expanded_mat = np.zeros(dims)
-		expanded_mat[mati_sorted, matj] = 1
-
-		# create dict with all information
-		tu = {}
-		tu['mati'] = mati_sorted.tolist()
-		tu['matj'] = matj.tolist()
-		tu['rows'] = sorted(wcm_rows)
-		tu['cols'] = wcm_cols
-		tu['dims'] = dims
-	else:
-		with open(JSON_OUTPUT_FILE) as f:
-			tu = json.load(f)
+def load_tu_matrix():
+	with open(JSON_OUTPUT_FILE) as f:
+		tu = json.load(f)
 
 	return tu
-
-def save_tu_matrix(tu):
-	# save json object
-	with open(JSON_OUTPUT_FILE, 'w') as f:
-		json.dump(tu, f)
 
 def create_tu_matrix(tu):
 	# reconstruct matrix with new columns
@@ -125,8 +69,7 @@ letter_to_index = {nt[0]: i for i, nt in enumerate(ntps)}
 rna_mws = {rna['id']: rna['mw'] for rna in raw_data.rnas}
 
 # Create transcription unit matrix (genes x tu)
-tu = load_tu_matrix(raw_data)
-save_tu_matrix(tu)
+tu = load_tu_matrix()
 tu_mat = create_tu_matrix(tu)
 rows = tu['rows']
 cols = tu['cols']
