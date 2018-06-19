f6a793c285d144a15557b8c7afa84b53696683e9
tahorst <thorst@stanford.edu>
2018-06-19 11:22:11 -0700

    Add documentation to line_profile

--- wholecell/utils/profiler.py
+++ wholecell/utils/profiler.py
@@ -1,8 +1,26 @@
+'''
+File for profiling tools
+'''
+
 from line_profiler import LineProfiler
-from functools import wraps
 
 def line_profile(func):
-	@wraps(func)
+	'''
+	Decorator for line profiling of a given function.
+	Prints time spent on each line within a function every time the
+	function is called.
+
+	@profile decorator from LineProfiler requires running the script
+	through kernprof and additional setup but this decorator can be
+	used in front of any function in the model with the lines below.
+
+	Usage:
+		from wholecell.utils.profiler import line_profile
+		@line_profile
+		def function_to_profile():
+			...
+	'''
+
 	def wrapper(*args, **kwargs):
 		lp = LineProfiler()
 		try:
