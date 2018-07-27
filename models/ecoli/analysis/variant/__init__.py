8ac3e3daabda16cb460172c01d7f56023c7d74f7
tahorst <thorst@stanford.edu>
2018-07-27 12:31:43 -0700

    Rename figure2c to adder_sizer variant

--- models/ecoli/analysis/variant/__init__.py
+++ models/ecoli/analysis/variant/__init__.py
@@ -1,13 +1,13 @@
 # Active analysis modules to run, in this order.
 # Tip: Edit this during development to run ones you're working on first.
 ACTIVE = [
+	"adder_sizer.py",
 	"conditionalFlux.py",
 	"distribution_cell_division_time.py",
 	"distribution_division_fluxome_proteome.py",
 	"distribution_fluxome_correlation.py",
 	"distribution_proteome_correlation.py",
 	"doubling_time_histogram.py",
-	"figure2c.py",
 	"figure3_new_parameterShuffle.py",
 	"figure5_parameterShuffle.py",
 	"figureS3.py",
