a377516dacc1039d1183bfdc95d83234c1b25536
tahorst <thorst@stanford.edu>
2018-08-02 15:35:35 -0700

    Create analysis plot templates

---
+++ models/ecoli/analysis/multigen/template.py
@@ -0,0 +1,53 @@
+"""
+Template for multigen analysis plots
+
+@organization: Covert Lab, Department of Bioengineering, Stanford University
+@date: Created 8/2/18
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+
+import cPickle
+from matplotlib import pyplot as plt
+import numpy as np
+import os
+
+from models.ecoli.analysis import multigenAnalysisPlot
+from models.ecoli.analysis.AnalysisPaths import AnalysisPaths
+from wholecell.analysis.analysis_tools import exportFigure
+from wholecell.io.tablereader import TableReader
+from wholecell.utils import filepath
+
+
+class Plot(multigenAnalysisPlot.MultigenAnalysisPlot):
+	def do_plot(self, seedOutDir, plotOutDir, plotOutFileName, simDataFile, validationDataFile, metadata):
+		if not os.path.isdir(seedOutDir):
+			raise Exception, 'seedOutDir does not currently exist as a directory'
+
+		filepath.makedirs(plotOutDir)
+
+		sim_data = cPickle.load(open(simDataFile, 'rb'))
+		validation_data = cPickle.load(open(validationDataFile, 'rb'))
+
+		ap = AnalysisPaths(seedOutDir, multi_gen_plot=True)
+
+		for sim_dir in ap.get_cells():
+			simOutDir = os.path.join(sim_dir, 'simOut')
+
+			# Listeners used
+			main_reader = TableReader(os.path.join(simOutDir, 'Main'))
+
+			# Load data
+			time = main_reader.readColumn('time')
+
+		plt.figure()
+
+		### Create Plot ###
+
+		exportFigure(plt, plotOutDir, plotOutFileName, metadata)
+		plt.close('all')
+
+
+if __name__ == '__main__':
+	Plot().cli()
---
+++ models/ecoli/analysis/single/template.py
@@ -0,0 +1,47 @@
+"""
+Template for single analysis plots
+
+@organization: Covert Lab, Department of Bioengineering, Stanford University
+@date: Created 8/2/18
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+
+import cPickle
+from matplotlib import pyplot as plt
+import numpy as np
+import os
+
+from models.ecoli.analysis import singleAnalysisPlot
+from wholecell.analysis.analysis_tools import exportFigure
+from wholecell.io.tablereader import TableReader
+from wholecell.utils import filepath
+
+
+class Plot(singleAnalysisPlot.SingleAnalysisPlot):
+	def do_plot(self, simOutDir, plotOutDir, plotOutFileName, simDataFile, validationDataFile, metadata):
+		if not os.path.isdir(simOutDir):
+			raise Exception, 'simOutDir does not currently exist as a directory'
+
+		filepath.makedirs(plotOutDir)
+
+		sim_data = cPickle.load(open(simDataFile, 'rb'))
+		validation_data = cPickle.load(open(validationDataFile, 'rb'))
+
+		# Listeners used
+		main_reader = TableReader(os.path.join(simOutDir, 'Main'))
+
+		# Load data
+		time = main_reader.readColumn('time')
+
+		plt.figure()
+
+		### Create Plot ###
+
+		exportFigure(plt, plotOutDir, plotOutFileName, metadata)
+		plt.close('all')
+
+
+if __name__ == '__main__':
+	Plot().cli()
