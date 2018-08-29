819d1fdd3bd580e6337385ff70f7043d39f92647
Mialy <mialy@stanford.edu>
2018-08-29 16:50:02 -0700

    Get rid of line that cuts off what will be displayed for the instantaneous growth rate

--- models/ecoli/analysis/multigen/growth_dynamics_panel.py
+++ models/ecoli/analysis/multigen/growth_dynamics_panel.py
@@ -169,7 +169,6 @@ class Plot(multigenAnalysisPlot.MultigenAnalysisPlot):
 			#import ipdb; ipdb.set_trace()
 			growth_rate = (1 / units.s) * growth_rate
 			growth_rate = growth_rate.asNumber(1 / units.min)
-			#growth_rate[abs(growth_rate - np.median(growth_rate)) > 1.25 * np.nanstd(growth_rate)] = np.nan
 
 			# Calculate Ribosome Concentration:
 			ribosomeConcentration = ((1 / sim_data.constants.nAvogadro) * ribosomeCounts) / ((1.0 / sim_data.constants.cellDensity) * (cellMass_fg))
