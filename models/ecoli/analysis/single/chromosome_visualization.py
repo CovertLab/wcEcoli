08bd9b39787a6918e6230df3d6f3246488088a8d
Gwanggyu Sun <ggsun@stanford.edu>
2019-02-06 11:04:17 -0800

    Add domains and global indexes of replisomes to output

--- models/ecoli/analysis/single/chromosome_visualization.py
+++ models/ecoli/analysis/single/chromosome_visualization.py
@@ -1,8 +1,8 @@
 """
-Template for single analysis plots
+Generates .json file containing locations of molecules on the chromosome.
 
 @organization: Covert Lab, Department of Bioengineering, Stanford University
-@date: Created 8/2/18
+@date: Created 2/2/19
 """
 
 from __future__ import absolute_import
@@ -44,6 +44,20 @@ class Plot(singleAnalysisPlot.SingleAnalysisPlot):
 		# Load replisome attributes
 		fork_coordinates = replication_data_reader.readColumn("fork_coordinates")
 		domain_indexes = replication_data_reader.readColumn("fork_domains")
+		fork_global_index = replication_data_reader.readColumn("fork_global_index")
+
+		# Get unique global indexes
+		unique_global_indexes = np.unique(fork_global_index[~np.isnan(fork_global_index)])
+		n_replisomes = len(unique_global_indexes)
+
+		# Parse data such that one column corresponds to one unique replisome
+		fork_coordinates_parsed = np.zeros((fork_coordinates.shape[0], n_replisomes))
+		domain_indexes_parsed = np.zeros(n_replisomes, dtype=np.int64)
+
+		for mol_idx, global_idx in enumerate(unique_global_indexes):
+			rows, cols = np.where(fork_global_index == global_idx)
+			fork_coordinates_parsed[rows, mol_idx] = fork_coordinates[rows, cols]
+			domain_indexes_parsed[mol_idx] = domain_indexes[rows[0], cols[0]]
 
 		# Build dictionary of chromosome data
 		chromosome_data = {
@@ -52,14 +66,15 @@ class Plot(singleAnalysisPlot.SingleAnalysisPlot):
 			"right_replichore_length": replichore_lengths[0],
 			"left_replichore_length": replichore_lengths[1],
 			"replisomes": {
-					"coordinates": fork_coordinates.tolist(),
-					"domain_indexes": domain_indexes.tolist()
-				}
+					"counts": n_replisomes,
+					"coordinates": fork_coordinates_parsed.tolist(),
+					"domain_indexes": domain_indexes_parsed.tolist()
+				},
 			}
 
 		# Output dictionary to json file
-		out_file = open(os.path.join(plotOutDir, plotOutFileName + ".json"), 'w')
-		out_file.write(json.dumps(chromosome_data, indent=4))
+		with open(os.path.join(plotOutDir, plotOutFileName + ".json"), 'w') as f:
+			f.write(json.dumps(chromosome_data, indent=4))
 
 
 if __name__ == '__main__':
--- models/ecoli/listeners/replication_data.py
+++ models/ecoli/listeners/replication_data.py
@@ -37,7 +37,6 @@ class ReplicationData(wholecell.listeners.listener.Listener):
 
 		self.fork_coordinates = np.full(75, np.nan, np.float64)
 		self.fork_domains = np.full(75, np.nan, np.float64)
-		self.fork_replichore = np.full(75, np.nan, np.float64)
 		self.fork_global_index = np.full(75, np.nan, np.float64)
 
 		self.numberOfOric = np.nan
@@ -56,13 +55,19 @@ class ReplicationData(wholecell.listeners.listener.Listener):
 
 		self.fork_coordinates[:] = np.nan
 		if len(active_replisomes) > 0:
-			fork_coordinates, fork_domains, fork_replichore, fork_global_index = active_replisomes.attrs(
-				"coordinates", "domain_index", "right_replichore", "_globalIndex"
+			fork_coordinates, fork_domains, fork_global_index = active_replisomes.attrs(
+				"coordinates", "domain_index", "_globalIndex"
 				)
+			self.fork_coordinates[:] = np.nan
+			self.fork_domains[:] = np.nan
+			self.fork_global_index[:] = np.nan
 			self.fork_coordinates[:fork_coordinates.size] = fork_coordinates
 			self.fork_domains[:fork_domains.size] = fork_domains
-			self.fork_replichore[:fork_replichore.size] = fork_replichore
 			self.fork_global_index[:fork_global_index.size] = fork_global_index
+		else:
+			self.fork_coordinates[:] = np.nan
+			self.fork_domains[:] = np.nan
+			self.fork_global_index[:] = np.nan
 
 	def tableCreate(self, tableWriter):
 		pass
@@ -71,7 +76,6 @@ class ReplicationData(wholecell.listeners.listener.Listener):
 		tableWriter.append(
 			fork_coordinates = self.fork_coordinates,
 			fork_domains = self.fork_domains,
-			fork_replichore = self.fork_replichore,
 			fork_global_index = self.fork_global_index,
 			numberOfOric = self.numberOfOric,
 			criticalMassPerOriC = self.criticalMassPerOriC,
