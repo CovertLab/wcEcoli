669404b90e0dde3480dcb922a1c9e81fd1fdf8d6
Mialy DeFelice <mialymurie@gmail.com>
2019-03-07 17:12:32 -0800

    add function to make collection of monomer ID names and update calculate biomass function

---
+++ prototypes/operons/transform_rnas.py
@@ -0,0 +1,31 @@
+import os
+from functools import partial
+
+from reconstruction import spreadsheets
+
+DIALECT = "excel-tab"
+JsonReader = partial(spreadsheets.JsonReader, dialect = DIALECT)
+JsonWriter = partial(spreadsheets.JsonWriter, dialect = DIALECT)
+
+
+FLAT_DIR = os.path.join("reconstruction", "ecoli", "flat")
+RNA_FILE = os.path.join(FLAT_DIR, "rnas.tsv")
+output_file = os.path.join(FLAT_DIR, "operon_rnas.tsv")
+
+def make_collection():
+	with open(RNA_FILE, "r") as f:
+		reader = JsonReader(f)
+		entries = {entry["monomerId"]: entry for entry in reader}
+		fieldnames = reader.fieldnames
+		for entry in entries.values():
+			entry["monomerId"] = [entry["monomerId"]]
+
+	with open(output_file, "w") as f:
+		writer = JsonWriter(f, fieldnames)
+		writer.writeheader()
+
+		for key in sorted(entries.keys()):
+			writer.writerow(entries[key])
+
+if __name__ == "__main__":
+	make_collection()
