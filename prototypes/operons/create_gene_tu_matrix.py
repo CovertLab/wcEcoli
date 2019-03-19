04abc5d8595b3319eaf156e8f5be166bbdd43c2c
Mialy DeFelice <mialymurie@gmail.com>
2019-03-19 05:48:17 -0700

    created matrix till came across issue in operon_rnas.tsv file generation

--- prototypes/operons/create_gene_tu_matrix.py
+++ prototypes/operons/create_gene_tu_matrix.py
@@ -4,8 +4,13 @@ For now extract the TU list from the rna_operons.tsv file. Later this will be pr
 Extract the gene list from rnas.tsv.
 '''
 
-import csv
+import numpy as np
 import os
+import re
+from reconstruction import spreadsheets
+from functools import partial
+DIALECT = "excel-tab"
+JsonReader = partial(spreadsheets.JsonReader, dialect = DIALECT)
 
 def parse_tsv(tsv_file):
 	'''
@@ -14,15 +19,15 @@ def parse_tsv(tsv_file):
 	'''
 	tsv_list = []
 	with open(tsv_file) as tsvfile:
-		reader = csv.reader(tsvfile, delimiter = '\t')
-		reader.next()
+		reader = JsonReader(tsvfile)
+		#reader.next()
 		for row in reader:
 			tsv_list.append(row)
 	return tsv_list
-#this gives directory into wcEcoli. Particular to the current storage location 
-# of this folder within prototypes.
-parent_directory = os.path.dirname(os.path.dirname(os.getcwd()))
-flat_file_dirctory = os.path.join(parent_directory, 'reconstruction', 'ecoli', 'flat')
+
+
+#parent_directory = os.path.dirname(os.path.dirname(os.getcwd()))
+flat_file_dirctory = os.path.join('reconstruction', 'ecoli', 'flat')
 rnas_file = os.path.join(flat_file_dirctory, 'rnas.tsv')
 tus_file = os.path.join(flat_file_dirctory, 'operon_rnas.tsv')
 
@@ -34,6 +39,55 @@ tu_info = parse_tsv(tus_file)
 #Number of RNA's
 num_rnas = len(rna_info)
 num_tus = len(tu_info)
+
+#Create empty matrix of the intended size.
+gene_tu_matrix = np.zeros((num_rnas, num_tus))
+
+#Now go through the tu_info and find all the multi gene tus
+#This is probably not a good way to do this.
+
+multi_gene_tu = []
+#looking for length of molecule_id instead of string parsing gene_id, bc
+# i didnt feel like doing that for now. But def something to change later.
+
+#actually go and do just gene_id only look if geneId contains an underscore
+#if it does break up the gene_id to invidual ids to map. 
+
+count = 0
+tu_index = []
+for row in tu_info:
+	
+	gene_id = row['geneId']
+	split_delimiter = '_'
+	if split_delimiter in gene_id:
+		#genes_in_tu = re.split(split_delimiter, gene_id)
+		tu_index.append(count)
+		#multi_gene_tu.append(genes_in_tu)
+	count += 1
+
+# Now we have the locations of the TUs. Need to find individual genes in 
+# rnas.tsv.
+#LOTS of nested shit.
+
+
+count = 0
+for row in rna_info:
+	# Get the gene_id of the RNAs
+	gene_id = row['geneId']
+	# Go through the tus
+	gene_index = count
+	for index in tu_index:
+		genes_in_tu = re.split(split_delimiter, tu_info[tu_index[0]]['geneId'])
+
+		# Try to find if the current RNA id matches an id with a TU.
+		for gene in genes_in_tu:
+			if gene == gene_id:
+				gene_tu_matrix[gene_index, tu_index] = 1
+				
+
+
+
+
 
 
 
