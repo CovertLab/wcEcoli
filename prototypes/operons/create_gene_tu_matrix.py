24064bd9700d1df3d5e2a78094c27fab606c6588
Mialy DeFelice <mialymurie@gmail.com>
2019-03-20 17:21:13 -0700

    added function to extract RNA seq count vector, in order of the genes in the gene to TU matrix

--- prototypes/operons/create_gene_tu_matrix.py
+++ prototypes/operons/create_gene_tu_matrix.py
@@ -14,83 +14,99 @@ JsonReader = partial(spreadsheets.JsonReader, dialect = DIALECT)
 
 def parse_tsv(tsv_file):
 	'''
-	Takes in a tsv file, and creates a list of lists of the rows 
+	Takes in a tsv file, and creates a list of dicts of the rows 
 	contained within the TSV.
 	'''
 	tsv_list = []
 	with open(tsv_file) as tsvfile:
 		reader = JsonReader(tsvfile)
-		#reader.next()
 		for row in reader:
 			tsv_list.append(row)
 	return tsv_list
 
+def find_tu_indices(tu_info):
+	'''
+	Input:
+	A list of dicts containing all the information found in the TU_FILE.
+	Do:
+	Find all the TU's (based on the presence of the '_' dividng them.
+	Retreive the 'index', based on count down the file.
+	Return:
+	A list containin the indices of all the TUs.
+	Preserves order.
+	'''
+	count = 0
+	tu_ind = []
+	for row in tu_info:
+		gene_id = row['geneId']
+		if SPLIT_DELIMITER in gene_id:
+			tu_ind.append(count)
+		count += 1
+	return tu_ind
+
+def create_gene_to_tu_matrix():
+	'''
+	Input:
+	Paths to rnas.tsv (RNA_FILE) and operon_rnas.tsv (TU_FILE).
+	Do:
+	Parse tsv files then create a gene to tu matrix mapping genes in each TU
+	to its partner RNA.
+	Return:
+	Sparse numpy matrix, mapping TU to rna's. 0 = no mapping; 1 = mapping.
+	'''
+	rna_info = parse_tsv(RNA_FILE)
+	tu_info = parse_tsv(TU_FILE)
+
+	num_rnas = len(rna_info)
+	num_tus = len(tu_info)
+
+	gene_to_tu_matrix = np.zeros((num_rnas, num_tus))
+	tu_index = find_tu_indices(tu_info)
+
+	count = 0
+	gene_id_vector = []
+	for row in rna_info:
+		gene_id = row['geneId']
+		gene_index = count
+		for index in tu_index:
+			genes_in_tu = re.split(SPLIT_DELIMITER, tu_info[tu_index[0]]['geneId'])
+			for gene in genes_in_tu:
+				if gene == gene_id:
+					gene_to_tu_matrix[gene_index, tu_index] = 1
+		gene_id_vector.append(gene_id)
+		count += 1
+	return gene_to_tu_matrix, gene_id_vector
+
+def create_rnaseq_count_vector(gene_id_vector):
+	'''
+	The counts vector is not in the same order as the Gene_TU_Matrix.
+	Need to reoder and pull out count information. 
+
+
+	'''
+	rna_seq_data_all_cond = parse_tsv(RNA_SEQ_FILE)
+	rna_seq_gene_id_list = []
+	rna_seq_counts_vector = []
+	#have this nexted for loop structure bc, I am trying to preserve the 
+	#gene order from rnas.tsv so that the matrix and the vector match up.
+	for gene in gene_id_vector:
+		for row in rna_seq_data_all_cond:
+			if row['Gene'] == gene:
+				rna_seq_gene_id_list.append(row['Gene'])
+				rna_seq_counts_vector.append(row[CONDITION])
+	import ipdb; ipdb.set_trace()
+
+	return rna_seq_gene_id_list, rna_seq_counts_vector
+
 
 #parent_directory = os.path.dirname(os.path.dirname(os.getcwd()))
-flat_file_dirctory = os.path.join('reconstruction', 'ecoli', 'flat')
-rnas_file = os.path.join(flat_file_dirctory, 'rnas.tsv')
-tus_file = os.path.join(flat_file_dirctory, 'operon_rnas_2.tsv')
+FLAT_DIR = os.path.join('reconstruction', 'ecoli', 'flat')
+RNA_FILE = os.path.join(FLAT_DIR, 'rnas.tsv')
+TU_FILE = os.path.join(FLAT_DIR, 'operon_rnas_2.tsv')
+RNA_SEQ_FILE = os.path.join(FLAT_DIR, 'rna_seq_data', 'rnaseq_rsem_tpm_mean.tsv')
+CONDITION = 'M9 Glucose minus AAs'
+SPLIT_DELIMITER = '_'
 
-#rna_info contains all the rna info directly pulled from rnas.tsv as a list of lists.
-rna_info = parse_tsv(rnas_file)
-tu_info = parse_tsv(tus_file)
+gene_tu_matrix, gene_id_vector = create_gene_to_tu_matrix()
+rna_counts_vector = create_rnaseq_count_vector(gene_id_vector)
 
-
-#Number of RNA's
-num_rnas = len(rna_info)
-num_tus = len(tu_info)
-
-#Create empty matrix of the intended size.
-gene_tu_matrix = np.zeros((num_rnas, num_tus))
-
-#Now go through the tu_info and find all the multi gene tus
-#This is probably not a good way to do this.
-
-multi_gene_tu = []
-#looking for length of molecule_id instead of string parsing gene_id, bc
-# i didnt feel like doing that for now. But def something to change later.
-
-#actually go and do just gene_id only look if geneId contains an underscore
-#if it does break up the gene_id to invidual ids to map. 
-
-count = 0
-tu_index = []
-for row in tu_info:
-	
-	gene_id = row['geneId']
-	split_delimiter = '_'
-	if split_delimiter in gene_id:
-		#genes_in_tu = re.split(split_delimiter, gene_id)
-		tu_index.append(count)
-		#multi_gene_tu.append(genes_in_tu)
-	count += 1
-
-# Now we have the locations of the TUs. Need to find individual genes in 
-# rnas.tsv.
-#LOTS of nested shit.
-
-
-count = 0
-for row in rna_info:
-	# Get the gene_id of the RNAs
-	gene_id = row['geneId']
-	# Go through the tus
-	gene_index = count
-	for index in tu_index:
-		genes_in_tu = re.split(split_delimiter, tu_info[tu_index[0]]['geneId'])
-
-		# Try to find if the current RNA id matches an id with a TU.
-		for gene in genes_in_tu:
-			if gene == gene_id:
-				gene_tu_matrix[gene_index, tu_index] = 1
-
-
-
-
-
-
-
-
-
-import ipdb; ipdb.set_trace()
-#with open 
