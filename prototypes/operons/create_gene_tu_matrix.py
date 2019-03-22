0a144dbfe0eac193f86e019fd5f960d0ef0597b1
Mialy DeFelice <mialymurie@gmail.com>
2019-03-21 17:27:41 -0700

    Put remaining commands into a function calculate_tu_counts_vector

--- prototypes/operons/create_gene_tu_matrix.py
+++ prototypes/operons/create_gene_tu_matrix.py
@@ -12,6 +12,13 @@ from functools import partial
 DIALECT = "excel-tab"
 JsonReader = partial(spreadsheets.JsonReader, dialect = DIALECT)
 
+FLAT_DIR = os.path.join('reconstruction', 'ecoli', 'flat')
+RNA_FILE = os.path.join(FLAT_DIR, 'rnas.tsv')
+TU_FILE = os.path.join(FLAT_DIR, 'operon_rnas_2.tsv')
+RNA_SEQ_FILE = os.path.join(FLAT_DIR, 'rna_seq_data', 'rnaseq_rsem_tpm_mean.tsv')
+CONDITION = 'M9 Glucose minus AAs'
+SPLIT_DELIMITER = '_'
+
 def parse_tsv(tsv_file):
 	'''
 	Takes in a tsv file, and creates a list of dicts of the rows 
@@ -29,7 +36,7 @@ def find_tu_indices(tu_info):
 	Input:
 	A list of dicts containing all the information found in the TU_FILE.
 	Do:
-	Find all the TU's (based on the presence of the '_' dividng them.
+	Find all the TU's (based on the presence of the '_' dividing them.
 	Retreive the 'index', based on count down the file.
 	Return:
 	A list containin the indices of all the TUs.
@@ -37,20 +44,11 @@ def find_tu_indices(tu_info):
 	TODO: Add another column to the operon_rnas.tsv file to assign a type as
 	a TU.
 	'''
-	#count = 0
-	#tu_ind = []
+
 	return [
 		count 
 		for count, row in enumerate(tu_info) 
 		if SPLIT_DELIMITER in row['geneId']]
-	'''
-	for row in tu_info:
-		gene_id = row['geneId']
-		if SPLIT_DELIMITER in gene_id:
-			tu_ind.append(count)
-		count += 1
-	return tu_ind
-	'''
 
 def create_gene_to_tu_matrix(rna_info, tu_info):
 	'''
@@ -83,16 +81,7 @@ def create_gene_to_tu_matrix(rna_info, tu_info):
 		for gene in genes_in_tu:
 			gene_index = reverse_index[gene]
 			gene_to_tu_matrix[gene_index, index] = 1
-	import ipdb; ipdb.set_trace()
-	'''
-	for gene_index, row in enumerate(rna_info):
-		gene_id = row['geneId']
-		for index in tu_index:
-			genes_in_tu = re.split(SPLIT_DELIMITER, tu_info[index]['geneId'])
-			for gene in genes_in_tu:
-				if gene == gene_id:
-					gene_to_tu_matrix[gene_index, index] = 1
-	'''
+	#import ipdb; ipdb.set_trace()
 	return gene_to_tu_matrix, rnas_gene_order
 
 def create_rnaseq_count_vector(rnas_gene_order):
@@ -104,46 +93,28 @@ def create_rnaseq_count_vector(rnas_gene_order):
 	being run in.
 	'''
 	rna_seq_data_all_cond = parse_tsv(RNA_SEQ_FILE)
-	#rna_seq_gene_id_list = []
-	rna_seq_counts_vector = []
-	#have this nexted for loop structure bc, I am trying to preserve the 
-	#gene order from rnas.tsv so that the matrix and the vector match up.
 
 	rna_seq_data_index = {
 		row['Gene']: row[CONDITION] 
 		for row in rna_seq_data_all_cond}
-		
+
 	rna_seq_counts_vector = [
 		rna_seq_data_index[gene] 
 		for gene in rnas_gene_order]
-	
-	'''
-	for gene in rnas_gene_order:
-		rna_seq_counts_vector.append(rna_seq_data_index[gene])
-		
-		for row in rna_seq_data_all_cond:
-			if row['Gene'] == gene:
-				#rna_seq_gene_id_list.append(row['Gene'])
-				rna_seq_counts_vector.append(row[CONDITION])
-	'''	
 
 	return rna_seq_counts_vector
 
+def calculate_tu_counts_vector():
+	rna_info = parse_tsv(RNA_FILE)
+	tu_info = parse_tsv(TU_FILE)
+	gene_tu_matrix, rnas_gene_order = create_gene_to_tu_matrix(rna_info, tu_info)
+	rna_seq_ids, rna_seq_counts_vector = create_rnaseq_count_vector(rnas_gene_order)
+	#returns 0's for anything that is not in a TU.
+	#do i need to make a new vector contining the rna seq counts with tu_counts_soln?
+	tu_counts_vector = np.linalg.lstsq(gene_tu_matrix, rna_seq_counts_vector)[0]
 
-#parent_directory = os.path.dirname(os.path.dirname(os.getcwd()))
-FLAT_DIR = os.path.join('reconstruction', 'ecoli', 'flat')
-RNA_FILE = os.path.join(FLAT_DIR, 'rnas.tsv')
-TU_FILE = os.path.join(FLAT_DIR, 'operon_rnas_2.tsv')
-RNA_SEQ_FILE = os.path.join(FLAT_DIR, 'rna_seq_data', 'rnaseq_rsem_tpm_mean.tsv')
-CONDITION = 'M9 Glucose minus AAs'
-SPLIT_DELIMITER = '_'
-
-rna_info = parse_tsv(RNA_FILE)
-tu_info = parse_tsv(TU_FILE)
-gene_tu_matrix, rnas_gene_order = create_gene_to_tu_matrix(rna_info, tu_info)
-rna_seq_ids, rna_seq_counts_vector = create_rnaseq_count_vector(rnas_gene_order)
-#returns 0's for anything that is not in a TU.
-#do i need to make a new vector contining the rna seq counts with tu_counts_soln?
-tu_counts_vector = np.linalg.lstsq(gene_tu_matrix, rna_seq_counts_vector)[0]
+	return tu_counts_vector
 
 
+if __name__ == "__main__":
+	calculate_tu_counts_vector()
