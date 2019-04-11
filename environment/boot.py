4e9e411913b382f7be6b7f050477ccac4b8c101b
Eran Agmon <agmon.eran@gmail.com>
2019-04-10 17:55:00 -0700

    pass color to lens

--- environment/boot.py
+++ environment/boot.py
@@ -22,6 +22,8 @@ from wholecell.utils import constants
 import wholecell.utils.filepath as fp
 from models.ecoli.sim.variants import apply_variant
 
+DEFAULT_COLOR = [0.6, 0.4, 0.3]
+
 class EnvironmentAgent(Outer):
 	def build_state(self):
 		lattice = {
@@ -31,6 +33,7 @@ class EnvironmentAgent(Outer):
 		simulations = {
 			agent_id: {
 				'volume': simulation['state']['volume'],
+				'color': simulation['state'].get('color', DEFAULT_COLOR),
 				'location': self.environment.locations[agent_id][0:2].tolist(),
 				'orientation': self.environment.locations[agent_id][2],
 				'parent_id': simulation.get('parent_id', '')}
@@ -61,8 +64,7 @@ def boot_lattice(agent_id, agent_type, agent_config):
 	make_media = Media()
 	new_media = make_media.make_recipe(media)
 
-	concentrations = new_media
-	agent_config['concentrations'] = concentrations
+	agent_config['concentrations'] = new_media
 	environment = EnvironmentSpatialLattice(agent_config)
 
 	return EnvironmentAgent(agent_id, agent_type, agent_config, environment)
