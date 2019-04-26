3a197371e1a6dc65f44c53a985c5a74b56f75b48
tahorst <thorst@stanford.edu>
2019-04-26 10:07:28 -0700

    Add aa_conc cohort plot

---
+++ models/ecoli/analysis/cohort/aa_conc.py
@@ -0,0 +1,88 @@
+"""
+Comparison of average amino acid concentrations to measured in vivo concentrations
+
+@organization: Covert Lab, Department of Bioengineering, Stanford University
+@date: Created 4/26/19
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+
+import cPickle
+import os
+
+from matplotlib import pyplot as plt
+import numpy as np
+
+from models.ecoli.analysis import cohortAnalysisPlot
+from models.ecoli.analysis.AnalysisPaths import AnalysisPaths
+from wholecell.analysis.analysis_tools import exportFigure
+from wholecell.analysis.analysis_tools import read_bulk_molecule_counts
+from wholecell.io.tablereader import TableReader
+from wholecell.utils import filepath
+from wholecell.utils import units
+
+
+class Plot(cohortAnalysisPlot.CohortAnalysisPlot):
+	def do_plot(self, variantDir, plotOutDir, plotOutFileName, simDataFile, validationDataFile, metadata):
+		if not os.path.isdir(variantDir):
+			raise Exception, 'variantDir does not currently exist as a directory'
+
+		filepath.makedirs(plotOutDir)
+
+		with open(simDataFile, 'rb') as f:
+			sim_data = cPickle.load(f)
+
+		aa_ids = sim_data.moleculeGroups.aaIDs
+		targets = np.array([sim_data.process.metabolism.concDict[key].asNumber(units.mmol / units.L) for key in aa_ids])
+
+		ap = AnalysisPaths(variantDir, cohort_plot=True)
+
+		aa_conc = None
+		for sim_dir in ap.get_cells(generation=range(6, 12)):
+			simOutDir = os.path.join(sim_dir, 'simOut')
+
+			# Load data
+			## Readers
+			kinetics_reader = TableReader(os.path.join(simOutDir, 'EnzymeKinetics'))
+
+			## Read data
+			(aa_counts,) = read_bulk_molecule_counts(simOutDir, aa_ids)
+			counts_to_molar = kinetics_reader.readColumn('countsToMolar').reshape(-1, 1)
+
+			## Append for all generations
+			if aa_conc is None:
+				aa_conc = aa_counts[1:, :]*counts_to_molar[1:]
+			else:
+				aa_conc = np.vstack((aa_conc, aa_counts[1:, :]*counts_to_molar[1:]))
+
+		ave_conc = aa_conc.mean(axis=0)
+		max_conc = np.max((ave_conc, targets))
+
+		# Plot correlation to data
+		plt.figure()
+
+		plt.loglog(targets, ave_conc, 'o')
+		plt.loglog([0, max_conc], [0, max_conc], 'k--')
+
+		plt.tight_layout()
+		exportFigure(plt, plotOutDir, plotOutFileName, metadata)
+
+		# Plot histogram
+		plt.figure(figsize=(8.5, 11))
+
+		for idx in xrange(21):
+			plt.subplot(6, 4, idx + 1)
+			plt.hist(aa_conc[:, idx])
+			plt.axvline(targets[idx], color='r', linestyle='--')
+			plt.title(aa_ids[idx], fontsize=8)
+			plt.tick_params(labelsize=8)
+
+		plt.tight_layout()
+		exportFigure(plt, plotOutDir, plotOutFileName + '_hist', metadata)
+
+		plt.close('all')
+
+
+if __name__ == '__main__':
+	Plot().cli()
