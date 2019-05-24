c9da450051fb0c3a2e5837f173a59f4bcab222b7
tahorst <thorst@stanford.edu>
2019-05-23 17:18:35 -0700

    Adjust sensitivity bash script

--- runscripts/paper/sensitivity.sh
+++ runscripts/paper/sensitivity.sh
@@ -14,13 +14,11 @@ out_dir=$1
 start_var=$2
 end_var=$3
 
-date
-
 # Run simulation and remove sim_data to save space
 function simulation {
 	variant="param_sensitivity"
-	python runscripts/manual/runSim.py $1 --variant $variant $2 $2 > out/$1/metadata/$2.log
-	rm out/$1/${variant}_$(printf "%06d" $2)/kb/simData_Modified.cPickle
+	python runscripts/manual/runSim.py $1 --length_sec 10 --variant $variant $3 $3 > $2/$3.log 2>&1
+	rm out/$1/${variant}_$(printf "%06d" $3)/kb/simData_Modified.cPickle
 }
 export -f simulation
 
@@ -28,9 +26,11 @@ export -f simulation
 python runscripts/manual/runFitter.py --disable-ribosome-fitting --disable-rnapoly-fitting $out_dir
 
 # Run simulation variants in parallel
-seq $start_var $end_var | parallel simulation $out_dir
+log_dir=out/$out_dir/log
+if [ ! -e $log_dir ]; then
+	mkdir $log_dir
+fi
+seq $start_var $end_var | parallel simulation $out_dir $log_dir
 
 # Run analysis script
 python models/ecoli/analysis/variant/param_sensitivity.py $out_dir
-
-date
