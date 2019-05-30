67be88fd00a82329d23dc0a399e463f261579c37
tahorst <thorst@stanford.edu>
2019-05-29 20:32:36 -0700

    Calculate flux correlation for params

--- models/ecoli/analysis/variant/param_sensitivity.py
+++ models/ecoli/analysis/variant/param_sensitivity.py
@@ -9,34 +9,43 @@ from __future__ import absolute_import
 from __future__ import division
 
 import cPickle
-from matplotlib import pyplot as plt
 from multiprocessing import Pool
-import numpy as np
 import os
+import re
+
+from matplotlib import pyplot as plt
+import numpy as np
+from scipy import stats
 
 from models.ecoli.analysis import variantAnalysisPlot
 from models.ecoli.analysis.AnalysisPaths import AnalysisPaths
+from models.ecoli.processes.metabolism import COUNTS_UNITS, MASS_UNITS, TIME_UNITS, VOLUME_UNITS
 from models.ecoli.sim.variants.param_sensitivity import number_params, split_indices
 from wholecell.analysis.analysis_tools import exportFigure
 from wholecell.io.tablereader import TableReader
-from wholecell.utils import constants, filepath, parallelization, sparkline
+from wholecell.utils import constants, filepath, parallelization, sparkline, units
 
 
-def analyze_variant((variant, total_params, ap)):
+def analyze_variant((variant, total_params)):
 	'''
 	Method to map each variant to for parallel analysis.
+
+	Requires global variables:
+		sim_data (SimulationData object)
+		validation_data (ValidationData object)
+		ap (AnalysisPaths object)
 
 	Args:
 		variant (int): variant index
 		total_params (int): total number of parameters that are changed
-		ap (AnalysisPaths object): variant plot analysis paths object to get
-			simulation output directories for the variant
 
 	Returns:
 		ndarray[float]: number of times each parameter was increased
 		ndarray[float]: number of times each parameter was decreased
 		ndarray[float]: average growth rate for each parameter when increaed
 		ndarray[float]: average growth rate for each parameter when decreased
+		ndarray[float]: average flux correlation for each parameter when increaed
+		ndarray[float]: average flux correlation for each parameter when decreased
 	'''
 
 	increase_indices, decrease_indices = split_indices(sim_data, variant)
@@ -45,6 +54,15 @@ def analyze_variant((variant, total_params, ap)):
 	decrease_params_counts = np.zeros(total_params)
 	increase_params_growth_rate = np.zeros(total_params)
 	decrease_params_growth_rate = np.zeros(total_params)
+	increase_params_flux_correlation = np.zeros(total_params)
+	decrease_params_flux_correlation = np.zeros(total_params)
+
+	cell_density = sim_data.constants.cellDensity
+	flux_units = units.mmol / units.g / units.h
+
+	# Validation data
+	toya_reactions = validation_data.reactionFlux.toya2010fluxes["reactionID"]
+	toya_fluxes = [f.asNumber(flux_units) for f in validation_data.reactionFlux.toya2010fluxes["reactionFlux"]]
 
 	for sim_dir in ap.get_cells(variant=[variant]):
 		simOutDir = os.path.join(sim_dir, "simOut")
@@ -52,20 +70,55 @@ def analyze_variant((variant, total_params, ap)):
 		try:
 			# Listeners used
 			mass_reader = TableReader(os.path.join(simOutDir, 'Mass'))
+			fba_results_reader = TableReader(os.path.join(simOutDir, "FBAResults"))
 
 			# Load data
+			## Growth rate
 			growth_rate = np.nanmean(mass_reader.readColumn('instantaniousGrowthRate')[-5:])
-		except:
-			# Exclude failed sims
+
+			## Central carbon flux
+			dry_mass = mass_reader.readColumn('dryMass')[-5:]
+			cell_mass = mass_reader.readColumn('cellMass')[-5:]
+			coefficient = dry_mass / cell_mass * cell_density.asNumber(MASS_UNITS / VOLUME_UNITS)
+
+			reaction_ids = np.array(fba_results_reader.readAttribute("reactionIDs"))
+			reaction_fluxes = (COUNTS_UNITS / MASS_UNITS / TIME_UNITS) * (fba_results_reader.readColumn("reactionFluxes")[-5:].T / coefficient).T
+		# Exclude failed sims
+		except Exception as e:
+			print(e)
 			continue
+
+		# Extract fluxes in Toya data set from simulation output
+		model_fluxes = np.zeros_like(toya_fluxes)
+		for i, toya_reaction in enumerate(toya_reactions):
+			flux_time_course = []
+
+			for rxn in reaction_ids:
+				if re.findall(toya_reaction, rxn):
+					reverse = 1
+					if re.findall("(reverse)", rxn):
+						reverse = -1
+
+					if len(flux_time_course):
+						flux_time_course += reverse * reaction_fluxes[:, np.where(reaction_ids == rxn)]
+					else:
+						flux_time_course = reverse * reaction_fluxes[:, np.where(reaction_ids == rxn)]
+
+			if len(flux_time_course):
+				model_fluxes[i] = np.mean(flux_time_course).asNumber(flux_units)
+
+		flux_r, _ = stats.pearsonr(toya_fluxes, model_fluxes)
 
 		increase_params_counts[increase_indices] += 1
 		decrease_params_counts[decrease_indices] += 1
 		increase_params_growth_rate[increase_indices] += growth_rate
 		decrease_params_growth_rate[decrease_indices] += growth_rate
+		increase_params_flux_correlation[increase_indices] += flux_r
+		decrease_params_flux_correlation[decrease_indices] += flux_r
 
 	return (increase_params_counts, decrease_params_counts,
-		increase_params_growth_rate, decrease_params_growth_rate)
+		increase_params_growth_rate, decrease_params_growth_rate,
+		increase_params_flux_correlation, decrease_params_flux_correlation)
 
 def rna_mapping(sim_data, monomer_to_gene):
 	'''
@@ -104,14 +157,18 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 
 		filepath.makedirs(plotOutDir)
 
+		global ap
 		ap = AnalysisPaths(inputDir, variant_plot=True)
 		variants = ap.get_variants()
 		n_variants = len(variants)
 
 		# Load one instance of sim_data to get number of parameters and ids
 		global sim_data
-		with open(os.path.join(inputDir, 'kb', constants.SERIALIZED_FIT1_FILENAME)) as f:
+		global validation_data
+		with open(os.path.join(inputDir, 'kb', constants.SERIALIZED_FIT1_FILENAME), 'rb') as f:
 			sim_data = cPickle.load(f)
+		with open(validationDataFile, 'rb') as f:
+			validation_data = cPickle.load(f)
 
 		# sim_data information
 		total_params = np.sum(number_params(sim_data))
@@ -133,7 +190,6 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 		args = zip(
 			variants,
 			[total_params] * n_variants,
-			[ap] * n_variants,
 			)
 		results = pool.map(analyze_variant, args)
 		pool.close()
@@ -143,7 +199,9 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 			(_increase_params_counts,
 				_decrease_params_counts,
 				_increase_params_growth_rate,
-				_decrease_params_growth_rate) = result
+				_decrease_params_growth_rate,
+				_increase_params_flux_correlation,
+				_decrease_params_flux_correlation) = result
 
 			if initialize:
 				initialize = False
@@ -151,11 +209,15 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 				decrease_params_counts = _decrease_params_counts
 				increase_params_growth_rate = _increase_params_growth_rate
 				decrease_params_growth_rate = _decrease_params_growth_rate
+				increase_params_flux_correlation = _increase_params_flux_correlation
+				decrease_params_flux_correlation = _decrease_params_flux_correlation
 			else:
 				increase_params_counts += _increase_params_counts
 				decrease_params_counts += _decrease_params_counts
 				increase_params_growth_rate += _increase_params_growth_rate
 				decrease_params_growth_rate += _decrease_params_growth_rate
+				increase_params_flux_correlation += _increase_params_flux_correlation
+				decrease_params_flux_correlation += _decrease_params_flux_correlation
 
 		data = increase_params_growth_rate / increase_params_counts - decrease_params_growth_rate / decrease_params_counts
 		mean = data[np.isfinite(data)].mean()
