7141e26c4d373d4a815e4c8efb85366b6d763875
Jerry Morrison <1fish2@users.noreply.github.com>
2019-06-11 23:39:31 -0700

    merge the local Docker code from paper-release
    * Use `docker build --build-arg` to reuse the same Dockerfiles for local vs. Google Cloud build.
    * Merge in the container's welcome message.
    * Merge in instructions to use a container and compare with setting up a pyenv.

--- cloud/build.sh
+++ cloud/build.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-# Use Cloud Build servers to build the layered WCM Docker images and store them
-# in the Container Registry.
+# Use Google Cloud Build servers to build the tiered WCM Docker images and store
+# them in the Google Container Registry.
 #
 # ASSUMES: The current working dir is the wcEcoli/ project root.
 #
 # TODO: Try using `gcloud builds submit --substitutions TAG_NAME=...` for
 # different developers and CI builds to create images containing different code.
-# That should work for the config files but we'll have to substitute into the
-# Dockerfile "FROM" statements.
+# That should work for the config file, and use `--build-arg from=xyz` to change
+# the Dockerfile "FROM" base images.
 
 set -eu
 
@@ -16,13 +16,13 @@ set -eu
 # project root which would upload the entire project.
 cp requirements.txt cloud/docker/runtime/
 gcloud builds submit --timeout=2h --tag gcr.io/allen-discovery-center-mcovert/wcm-runtime cloud/docker/runtime/
-rm cloud/docker/runtime/requirements.txt
+rm -f cloud/docker/runtime/requirements.txt
 
 # 2. The Whole Cell Model code.
 # This needs a config file to identify the project files to upload and the
 # Dockerfile to run.
 gcloud builds submit --timeout=15m --config config-build-2-wcm-code.json
 
-# 3. The full WCM with Parca output, ready to run sims.
+# 3. The full WCM with Parca output, ready to run sims, esp. agent sims.
 # This build doesn't need to upload any payload files.
 gcloud builds submit --timeout=90m --tag gcr.io/allen-discovery-center-mcovert/wcm-full cloud/docker/full
