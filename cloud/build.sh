6cf9a1ed2818ebf6495925a6a0988b04dc46150b
Jerry Morrison <1fish2@users.noreply.github.com>
2019-06-14 19:26:25 -0700

    build user- and PR-specific Docker images
    * `cloud/build-runtime.sh` cloud-builds the wcm-runtime Docker image with an optional argument so you can put a different tag on the output, e.g. when testing new pips.
    * `cloud/build-wcm.sh` cloud-builds the wcm-code Docker image with optional arguments for the output image tag and the input wcm-runtime image. I don't expect to need a special wcm-runtime image often so it's the second argument. The first argument supplies an ID which defaults to `${USER}` but you could specify a PR number, git hash, nightly build type, whatever. The output image tag will be `${ID}-wcm-code`.
      * We'll run just this step nearly always. It takes 3 secs to make a tarball of the code in the directory, 14 secs to upload it, and about 2 min to built it on a Cloud Build server, including compiling Cython code.
    * `cloud/build.sh` calls the above shell scripts.
    * I commented out the step that builds a `wcm-full` image. That was mainly so WCM agents could load up ready to runSim, but now the Agent project has a cool new strategy to run a modest number of WCMs alongside 30K flyweight in-process surrogates. We can bring back this Docker image (and parameterize it like the others) if needed.

--- cloud/build.sh
+++ cloud/build.sh
@@ -3,26 +3,17 @@
 # them in the Google Container Registry.
 #
 # ASSUMES: The current working dir is the wcEcoli/ project root.
-#
-# TODO: Try using `gcloud builds submit --substitutions TAG_NAME=...` for
-# different developers and CI builds to create images containing different code.
-# That should work for the config file, and use `--build-arg from=xyz` to change
-# the Dockerfile "FROM" base images.
 
 set -eu
 
-# 1. The Python runtime environment.
-# This needs one payload file so copy it in rather than using a config at the
-# project root which would upload the entire project.
-cp requirements.txt cloud/docker/runtime/
-gcloud builds submit --timeout=2h --tag gcr.io/allen-discovery-center-mcovert/wcm-runtime cloud/docker/runtime/
-rm -f cloud/docker/runtime/requirements.txt
+# 1. The Python runtime environment with the default image tag.
+cloud/build-runtime.sh
 
-# 2. The Whole Cell Model code.
-# This needs a config file to identify the project files to upload and the
-# Dockerfile to run.
-gcloud builds submit --timeout=15m --config config-build-2-wcm-code.json
+# 2. The Whole Cell Model code with the default user-specific tag, building
+# FROM the default runtime.
+cloud/build-wcm.sh
 
-# 3. The full WCM with Parca output, ready to run sims, esp. agent sims.
-# This build doesn't need to upload any payload files.
-gcloud builds submit --timeout=90m --tag gcr.io/allen-discovery-center-mcovert/wcm-full cloud/docker/full
+### # Commented out until needed.
+### # 3. The full WCM with Parca output, ready to run sims, esp. agent sims.
+### # This build doesn't need to upload any payload files.
+### gcloud builds submit --timeout=90m --tag gcr.io/allen-discovery-center-mcovert/wcm-full cloud/docker/full
