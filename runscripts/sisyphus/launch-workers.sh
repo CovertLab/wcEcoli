12220630732d2a315338fef34b97b45c3b7577ef
Jerry Morrison <1fish2@users.noreply.github.com>
2019-07-11 20:13:01 -0700

    the Workflow class isn't a runscript
    ... so move it into a utils/ dir.

--- runscripts/sisyphus/launch-workers.sh
+++ runscripts/sisyphus/launch-workers.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# Launch Sisyphus worker nodes with the given names on Google Compute Engine,
+# provisioned with the capacity and service access needed for wcEcoli.
 
 set -eu
 
