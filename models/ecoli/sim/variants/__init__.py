5ef9cc5c64459894cf992434ecf5c57a00a8c24d
tahorst <thorst@stanford.edu>
2019-07-25 09:09:05 -0700

    Add flux_sensitivity variant

--- models/ecoli/sim/variants/__init__.py
+++ models/ecoli/sim/variants/__init__.py
@@ -47,6 +47,9 @@ from models.ecoli.sim.variants.metabolism_kinetic_objective_weight import metabo
 from models.ecoli.sim.variants.param_sensitivity import param_sensitivity
 from models.ecoli.sim.variants.param_sensitivity import param_sensitivity_indices
 
+from models.ecoli.sim.variants.flux_sensitivity import flux_sensitivity
+from models.ecoli.sim.variants.flux_sensitivity import flux_sensitivity_indices
+
 nameToFunctionMapping = {
 	"geneKnockout": geneKnockout,
 	"wildtype": wildtype,
@@ -63,6 +66,7 @@ nameToFunctionMapping = {
 	"meneParams": meneParams,
 	"metabolism_kinetic_objective_weight": metabolism_kinetic_objective_weight,
 	"param_sensitivity": param_sensitivity,
+	"flux_sensitivity": flux_sensitivity,
 }
 
 nameToNumIndicesMapping = {
@@ -81,4 +85,5 @@ nameToNumIndicesMapping = {
 	"meneParams": meneParamsTotalIndices,
 	"metabolism_kinetic_objective_weight": metabolism_kinetic_objective_weight_indices,
 	"param_sensitivity": param_sensitivity_indices,
+	"flux_sensitivity": flux_sensitivity_indices,
 }
