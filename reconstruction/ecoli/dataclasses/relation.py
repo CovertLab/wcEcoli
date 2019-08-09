e8a6f91764b482ef1ada509335d44d7f8eaecafb
Mialy DeFelice <mialymurie@gmail.com>
2019-08-09 11:28:03 -0700

    removed commented sections

--- reconstruction/ecoli/dataclasses/relation.py
+++ reconstruction/ecoli/dataclasses/relation.py
@@ -22,24 +22,18 @@ class Relation(object):
 		self._buildRnaIndexToMonomerMapping(raw_data, sim_data)
 		self._buildMonomerIndexToRnaMapping(raw_data, sim_data)
 		#self._buildRnaIndexToGeneMapping(raw_data, sim_data)
-	'''
-	def make_individual_rna_ids(operon_rna_id):
-			#move this to the top if we keep it
-			SPLIT_DELIMITER = '_'
-			#This solution will work for now, but will need to look for type and add the appropriate suffix
-			#There has to be an easier way, maybe store the first gene in the operon_rnas file.
-			rna_ids = re.split(SPLIT_DELIMITER, operon_rna_id)
-			split_rna_ids = []
-			for rna_id in rna_ids:
-				split_rna_ids.append(rna_id + '_RNA')
-			return split_rna_ids
-	'''
+	
 	def _buildRnaIndexToMonomerMapping(self, raw_data, sim_data):
 		#import pdb; pdb.set_trace()
-		self.rnaIndexToMonomerMapping = np.array([np.where(x == sim_data.process.transcription.rnaData["id"])[0][0] for x in sim_data.process.translation.monomerData["rnaId"]])
+		self.rnaIndexToMonomerMapping = np.array([
+			np.where(x == sim_data.process.transcription.rnaData["id"])[0][0] 
+			for x in sim_data.process.translation.monomerData["rnaId"]])
 
 	def _buildMonomerIndexToRnaMapping(self, raw_data, sim_data):
-		self.monomerIndexToRnaMapping = np.array([np.where(x == sim_data.process.translation.monomerData["rnaId"])[0][0] for x in sim_data.process.transcription.rnaData["id"] if len(np.where(x == sim_data.process.translation.monomerData["rnaId"])[0])])
+		self.monomerIndexToRnaMapping = np.array([
+			np.where(x == sim_data.process.translation.monomerData["rnaId"])[0][0] 
+			for x in sim_data.process.transcription.rnaData["id"] 
+			if len(np.where(x == sim_data.process.translation.monomerData["rnaId"])[0])])
 
 	#def _buildRnaIndexToGeneMapping(self, raw_data, sim_data):
 	#	self.rnaIndexToGeneMapping = np.array([np.where(x + "[c]" == sim_data.process.transcription.rnaData["id"])[0][0] for x in sim_data.process.replication.geneData["rnaId"]])
