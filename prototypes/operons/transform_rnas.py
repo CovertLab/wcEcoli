c5baf1be99b98651dc2b02cb0b61d9c52dcc2321
Mialy DeFelice <mialymurie@gmail.com>
2019-08-09 11:22:41 -0700

    cleaned up transform_rnas.py to get rid of previously commented lines

--- prototypes/operons/transform_rnas.py
+++ prototypes/operons/transform_rnas.py
@@ -1,15 +1,6 @@
 import os
 from functools import partial
-
 from reconstruction import spreadsheets
-
-'''
-There is an error in this file when there is no monomerID the 
-value is not saved.
-
-Fix this
-
-'''
 import csv
 
 DIALECT = "excel-tab"
@@ -22,6 +13,7 @@ RNA_FILE = os.path.join(FLAT_DIR, "rnas.tsv")
 #saving to a new file for now so that all manually input TUs in 
 #operon_rnas.tsv are not overwritten.
 output_file = os.path.join(FLAT_DIR, "operon_rnas_3.tsv")
+#
 
 
 def parse_tsv(tsv_file):
@@ -40,22 +32,27 @@ def parse_tsv(tsv_file):
 
 def make_collection():
 	#pull in rna_info as a list
-	rna_info, fieldnames= parse_tsv(RNA_FILE)
+	rna_info, fieldnames = parse_tsv(RNA_FILE)
+	
+	#Creating a new column for monomerSets. Which are the monomers for given
+	#mRNA transcripts. Right now we are manually adding in multi-gene TUs, 
+	#so Im not going to worry about anything except taking the current rnas.tsv
+	#and creating an output that has a new column putting the monomerID into a list.
+	#for a multigene TU, will have two IDs within this list.
 
 	#look for instances where rnas do not have an assigned monomerId
 	#replace with a empty list, else put monomerId(s) into a list.
-	#Do the same for GeneIDs
+
+	
 
 	for rna_row in rna_info:
 		if not rna_row['monomerId']:
-			rna_row['monomerId'] = []
+			rna_row['monomerSet'] = []
 		else:
-			rna_row['monomerId'] = [rna_row['monomerId']]
+			rna_row['monomerSet'] = [rna_row['monomerId']]
 
-		#if not rna_row['geneId']:
-			#rna_row['geneId'] = []
-		#else:
-			#rna_row['geneId'] = [rna_row['geneId']]
+	#add fieldname for 'monomersets'
+	fieldnames.append('monomerSet')
 
 	with open(output_file, "w") as f:
 		writer = JsonWriter(f, fieldnames)
@@ -66,4 +63,5 @@ def make_collection():
 
 if __name__ == "__main__":
 	make_collection()
+
 
