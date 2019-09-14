5dbac9e3d0b14503097ac9a5f30504fdc8854b3b
Gwanggyu Sun <ggsun@stanford.edu>
2019-09-13 19:02:17 -0700

    Add more data to chromosome json file

--- models/ecoli/analysis/single/chromosome_visualization.py
+++ models/ecoli/analysis/single/chromosome_visualization.py
@@ -37,45 +37,78 @@ class Plot(singleAnalysisPlot.SingleAnalysisPlot):
 		main_reader = TableReader(os.path.join(simOutDir, 'Main'))
 		replication_data_reader = TableReader(
 			os.path.join(simOutDir, "ReplicationData"))
+		rnap_data_reader = TableReader(
+			os.path.join(simOutDir, "RnapData"))
 
 		# Load time data
 		initial_time = main_reader.readAttribute('initialTime')
 		time = main_reader.readColumn('time') - initial_time
+		n_timesteps = len(time)
 
 		# Load replisome attributes
 		fork_coordinates = replication_data_reader.readColumn("fork_coordinates")
-		domain_indexes = replication_data_reader.readColumn("fork_domains")
-		fork_unique_index = replication_data_reader.readColumn("fork_unique_index")
+		fork_domain_indexes = replication_data_reader.readColumn("fork_domains")
+		fork_unique_indexes = replication_data_reader.readColumn("fork_unique_index")
 
-		# Get unique indexes of each fork
-		all_unique_indexes = np.unique(fork_unique_index[~np.isnan(fork_unique_index)])
-		n_replisomes = len(all_unique_indexes)
+		# Load active RNAP attributes
+		rnap_coordinates = rnap_data_reader.readColumn("active_rnap_coordinates")
+		rnap_domain_indexes = rnap_data_reader.readColumn("active_rnap_domain_indexes")
+		rnap_unique_indexes = rnap_data_reader.readColumn("active_rnap_unique_indexes")
 
-		# Parse data such that one column corresponds to one unique replisome
-		fork_coordinates_parsed = np.zeros((fork_coordinates.shape[0], n_replisomes))
-		domain_indexes_parsed = np.zeros(n_replisomes, dtype=np.int64)
+		# Get list of unique indexes of each replisome
+		fork_unique_index_list = np.unique(
+			fork_unique_indexes[~np.isnan(fork_unique_indexes)])
+		n_unique_replisomes = len(fork_unique_index_list)
 
-		for mol_idx, unique_idx in enumerate(all_unique_indexes):
-			rows, cols = np.where(fork_unique_index == unique_idx)
-			fork_coordinates_parsed[rows, mol_idx] = fork_coordinates[rows, cols]
-			domain_indexes_parsed[mol_idx] = domain_indexes[rows[0], cols[0]]
+		# Parse data such that one row of data corresponds to one unique
+		# replisome. The active mask boolean array is set to True if the
+		# corresponding replisome (row) is active at the given timestep
+		# (column).
+		fork_coordinates_parsed = np.zeros((n_unique_replisomes, n_timesteps))
+		fork_active_mask_parsed = np.zeros(
+			(n_unique_replisomes, n_timesteps), dtype=np.bool)
+		fork_domain_indexes_parsed = np.zeros(n_unique_replisomes, dtype=np.int64)
+
+		for mol_idx, unique_idx in enumerate(fork_unique_index_list):
+			time_index, col_index = np.where(fork_unique_indexes == unique_idx)
+			fork_coordinates_parsed[mol_idx, time_index] = fork_coordinates[time_index, col_index]
+			fork_active_mask_parsed[mol_idx, time_index] = True
+
+			# Domain indexes are static - just take the first value
+			fork_domain_indexes_parsed[mol_idx] = fork_domain_indexes[
+				time_index[0], col_index[0]]
+
+		# Crop out full columns of NaNs and replace NaNs to zeros for RNAP data
+		rnap_isnan = np.isnan(rnap_coordinates)
+		n_nan_columns = (rnap_isnan.sum(axis=0) == n_timesteps).sum()
+
+		rnap_coordinates_cropped = np.nan_to_num(rnap_coordinates[:, :-n_nan_columns])
+		rnap_domain_indexes_cropped = np.nan_to_num(rnap_domain_indexes[:, :-n_nan_columns])
+		rnap_unique_indexes_cropped = np.nan_to_num(rnap_unique_indexes[:, :-n_nan_columns])
 
 		# Build dictionary of chromosome data
 		chromosome_data = {
 			"metadata": metadata,
-			"time": time.tolist(),
-			"right_replichore_length": replichore_lengths[0],
-			"left_replichore_length": replichore_lengths[1],
+			"time": [round(t, 2) for t in time],
+			"right_replichore_len": replichore_lengths[0],
+			"left_replichore_len": replichore_lengths[1],
 			"replisomes": {
-					"counts": n_replisomes,
-					"coordinates": fork_coordinates_parsed.tolist(),
-					"domain_indexes": domain_indexes_parsed.tolist()
+				"active": fork_active_mask_parsed.tolist(),
+				"counts": n_unique_replisomes,
+				"coordinates": fork_coordinates_parsed.tolist(),
+				"domain_indexes": fork_domain_indexes_parsed.tolist()
 				},
+			"active_RNAPs": {
+				"active": np.logical_not(rnap_isnan).tolist(),
+				"coordinates": rnap_coordinates_cropped.tolist(),
+				"domain_indexes": rnap_domain_indexes_cropped.tolist(),
+				"unique_indexes": rnap_unique_indexes_cropped.tolist()
+				}
 			}
 
 		# Output dictionary to json file
 		with open(os.path.join(plotOutDir, plotOutFileName + ".json"), 'w') as f:
-			f.write(json.dumps(chromosome_data, indent=4))
+			f.write(json.dumps(chromosome_data))
 
 
 if __name__ == '__main__':
