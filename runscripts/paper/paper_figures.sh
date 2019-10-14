da63efec3e74076e587a3e542d7ddd0113e0a469
Travis Horst <thorst@stanford.edu>
2019-10-14 12:15:37 -0700

    Update plot formatting and paper_figures.sh for fig 5 (#695)

--- runscripts/paper/paper_figures.sh
+++ runscripts/paper/paper_figures.sh
@@ -107,9 +107,12 @@ python models/ecoli/analysis/multigen/pabx_limitations.py \
 
 
 ## Figure 5
-# panel A: made by JC in repo EcoliFoldChanges
+# panel A
+python paper/steadyStateAnalysis/main.py
 
-# panel B: made by JC from his experimental results
+# panel B - experimental results
+
+# panel C - experimental results
 
 
 ## Supplemental figure 1
@@ -174,5 +177,4 @@ python models/ecoli/analysis/variant/subgen_expression.py \
 /scratch/PI/mcovert/wc_ecoli/paper/SET_F/20190918.093118__SET_F_16_gens_8_seeds_9_pabB_expression_values/
 
 
-## Supplemental figure 5
-# made by JC from his experimental results
+## Supplemental figure 5 - experimental results
