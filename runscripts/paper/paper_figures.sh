da75ec718d31782a91f89af2b43e6254ab6ac325
heejochoi <heejochoi@gmail.com>
2019-10-14 12:43:57 -0700

    Describe fig 2 and s2 generation.

--- runscripts/paper/paper_figures.sh
+++ runscripts/paper/paper_figures.sh
@@ -11,32 +11,46 @@ python models/ecoli/analysis/multigen/growth_dynamics_grid.py \
 
 
 ## Figure 2
-# Panel a
+# Panel A
 python models/ecoli/analysis/cohort/doubling_times_histogram_all.py \
-/scratch/PI/mcovert/wc_ecoli/paper/SET_C/20190821.000038__SET_C_4_gens_256_seeds_3_conditions_with_growth_noise_and_D_period/
+/scratch/PI/mcovert/wc_ecoli/paper/SET_L/20190910.155143__SET_L_4_gens_256_seeds_3_conditions_unfit_ribosome_and_rna_poly_expression/
 
-python models/ecoli/analysis/cohort/doubling_times_histogram_all.py \
-/scratch/PI/mcovert/wc_ecoli/paper/SET_D/20190822.092543__SET_D2_4_gens_256_seeds,_unfit_ribosome_expression/
+# Panel B
 
-python models/ecoli/analysis/cohort/doubling_times_histogram_all.py \
-/scratch/PI/mcovert/wc_ecoli/paper/SET_D/20190822.230154__SET_D3_4_gens_256_seeds,_unfit_rna_poly_expression/
 
+# Panel C
 python models/ecoli/analysis/cohort/doubling_times_histogram_all.py \
-/scratch/PI/mcovert/wc_ecoli/paper/SET_D/20190824.144321__SET_D5_4_gens_256_seeds,_unfit_ribosome_and_rna_poly_expression/
-
-# Panel b
-python models/ecoli/analysis/cohort/doubling_times_histogram_all.py \
-/scratch/PI/mcovert/wc_ecoli/paper/SET_C/20190821.000038__SET_C_4_gens_256_seeds_3_conditions_with_growth_noise_and_D_period/ \
---variant_index 1
+/scratch/PI/mcovert/wc_ecoli/paper/SET_D/20190908.203751__SET_D2_4_gens_256_seeds,_unfit_ribosome_expression/
 
 python models/ecoli/analysis/cohort/doubling_times_histogram_all.py \
-/scratch/PI/mcovert/wc_ecoli/paper/SET_C/20190821.000038__SET_C_4_gens_256_seeds_3_conditions_with_growth_noise_and_D_period/ \
---variant_index 2
+/scratch/PI/mcovert/wc_ecoli/paper/SET_D/20190909.171540__SET_D3_4_gens_256_seeds,_unfit_rna_poly_expression/
 
-# Panel c
-python models/ecoli/analysis/variant/growth_condition_comparison_validation.py \
-/scratch/PI/mcovert/wc_ecoli/paper/SET_C/20190821.000038__SET_C_4_gens_256_seeds_3_conditions_with_growth_noise_and_D_period/
+python models/ecoli/analysis/cohort/doubling_times_histogram_all.py \
+/scratch/PI/mcovert/wc_ecoli/paper/SET_D/20190909.224043__SET_D4_4_gens_256_seeds,_unfit_ribosome_and_rna_poly_expression,_adjusted_RNase_expression/
 
+# Panel D
+python models/ecoli/analysis/cohort/doubling_times_histogram_all.py \
+/scratch/PI/mcovert/wc_ecoli/paper/SET_C/20190906.151026__SET_C_4_gens_256_seeds_3_conditions_with_growth_noise_and_D_period/
+
+# Panel E
+# Simulation values were obtained from pausing
+# reconstruction/ecoli/fit_sim_data_1.py after line 381, reading abundances of
+# RNA Polymerase and Ribosome subunits from the bulkContainer object, and
+# calculating the number of full complexes that can be formed from their stoichiometries.
+
+# Panel F
+python paper/figure_s2_investigation/mrna_expression/mrna_expression.py
+
+# Panel G
+python models/ecoli/analysis/cohort/rnap_validation.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_J/20190924.090047__SET_J03_alternate_RNA_seq
+
+python models/ecoli/analysis/cohort/ribosome_validation.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_J/20190924.090047__SET_J03_alternate_RNA_seq
+
+# Panel H
+python models/ecoli/analysis/variant/growth_condition_comparison_validation_parallel.py \
+/scratch/PI/mcovert/wc_ecoli/paper/SET_C/20190906.151026__SET_C_4_gens_256_seeds_3_conditions_with_growth_noise_and_D_period/
 
 ## Figure 3
 # panel A
@@ -128,7 +142,124 @@ python models/ecoli/analysis/multigen/environmental_shift_fluxes.py \
 
 
 ## Supplemental figure 2
-# panel C
+# Panel A
+# Input Comparison
+python paper/figure_s2_investigation/mrna_expression/mrna_expression.py
+python paper/figure_s2_investigation/translation_efficiency/translation_efficiency.py
+python paper/figure_s2_investigation/mrna_half_lives/mrna_half_lives.py
+
+# RNA Polymerase (ordered from column 0 to 11)
+python models/ecoli/analysis/cohort/rnap_validation.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_L/20190910.155143__SET_L_4_gens_256_seeds_3_conditions_unfit_ribosome_and_rna_poly_expression
+
+python models/ecoli/analysis/cohort/rnap_validation.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_J/20190923.193545__SET_J01_alternate_RNA_mass_per_cell
+
+python models/ecoli/analysis/cohort/rnap_validation.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_J/20190923.223401__SET_J02_alternate_mRNA_mass_per_cell
+
+python models/ecoli/analysis/cohort/rnap_validation.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_J/20190924.090047__SET_J03_alternate_RNA_seq
+
+python models/ecoli/analysis/cohort/rnap_validation.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_J/20190924.111334__SET_J04_alternate_translation_efficiency
+
+python models/ecoli/analysis/cohort/rnap_validation.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_J/20190924.134144__SET_J05_alternate_R-protein_half_lives
+
+python models/ecoli/analysis/cohort/rnap_validation.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_J/20190924.180126__SET_J06_alternate_protein_mass_per_cell
+
+python models/ecoli/analysis/cohort/rnap_validation.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_J/20190924.200659__SET_J07_variable_transcription_elongation_rate
+
+python models/ecoli/analysis/cohort/rnap_validation.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_J/20190924.220818__SET_J08_max_RNA_polymerase_activity
+
+python models/ecoli/analysis/cohort/rnap_validation.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_J/20190924.234820__SET_J09_mRNA_half_life_fitting
+
+python models/ecoli/analysis/cohort/rnap_validation.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_J/20190925.014132__SET_J10_variable_translation_elongation_rate
+
+python models/ecoli/analysis/cohort/rnap_validation.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_J/20190925.070638__SET_J11_alternate_ribosome_activity
+
+# Ribosome (ordered from column 0 to 11)
+python models/ecoli/analysis/cohort/ribosome_validation.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_L/20190910.155143__SET_L_4_gens_256_seeds_3_conditions_unfit_ribosome_and_rna_poly_expression
+
+python models/ecoli/analysis/cohort/ribosome_validation.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_J/20190923.193545__SET_J01_alternate_RNA_mass_per_cell
+
+python models/ecoli/analysis/cohort/ribosome_validation.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_J/20190923.223401__SET_J02_alternate_mRNA_mass_per_cell
+
+python models/ecoli/analysis/cohort/ribosome_validation.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_J/20190924.090047__SET_J03_alternate_RNA_seq
+
+python models/ecoli/analysis/cohort/ribosome_validation.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_J/20190924.111334__SET_J04_alternate_translation_efficiency
+
+python models/ecoli/analysis/cohort/ribosome_validation.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_J/20190924.134144__SET_J05_alternate_R-protein_half_lives
+
+python models/ecoli/analysis/cohort/ribosome_validation.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_J/20190924.180126__SET_J06_alternate_protein_mass_per_cell
+
+python models/ecoli/analysis/cohort/ribosome_validation.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_J/20190924.200659__SET_J07_variable_transcription_elongation_rate
+
+python models/ecoli/analysis/cohort/ribosome_validation.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_J/20190924.220818__SET_J08_max_RNA_polymerase_activity
+
+python models/ecoli/analysis/cohort/ribosome_validation.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_J/20190924.234820__SET_J09_mRNA_half_life_fitting
+
+python models/ecoli/analysis/cohort/ribosome_validation.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_J/20190925.014132__SET_J10_variable_translation_elongation_rate
+
+python models/ecoli/analysis/cohort/ribosome_validation.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_J/20190925.070638__SET_J11_alternate_ribosome_activity
+
+# Doubling Time (ordered from column 0 to 11)
+python models/ecoli/analysis/cohort/doubling_times_violin.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_L/20190910.155143__SET_L_4_gens_256_seeds_3_conditions_unfit_ribosome_and_rna_poly_expression/
+
+python models/ecoli/analysis/cohort/doubling_times_violin.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_J/20190923.193545__SET_J01_alternate_RNA_mass_per_cell
+
+python models/ecoli/analysis/cohort/doubling_times_violin.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_J/20190923.223401__SET_J02_alternate_mRNA_mass_per_cell
+
+python models/ecoli/analysis/cohort/doubling_times_violin.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_J/20190924.090047__SET_J03_alternate_RNA_seq
+
+python models/ecoli/analysis/cohort/doubling_times_violin.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_J/20190924.111334__SET_J04_alternate_translation_efficiency
+
+python models/ecoli/analysis/cohort/doubling_times_violin.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_J/20190924.134144__SET_J05_alternate_R-protein_half_lives
+
+python models/ecoli/analysis/cohort/doubling_times_violin.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_J/20190924.180126__SET_J06_alternate_protein_mass_per_cell
+
+python models/ecoli/analysis/cohort/doubling_times_violin.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_J/20190924.200659__SET_J07_variable_transcription_elongation_rate
+
+python models/ecoli/analysis/cohort/doubling_times_violin.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_J/20190924.220818__SET_J08_max_RNA_polymerase_activity
+
+python models/ecoli/analysis/cohort/doubling_times_violin.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_J/20190924.234820__SET_J09_mRNA_half_life_fitting
+
+python models/ecoli/analysis/cohort/doubling_times_violin.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_J/20190925.014132__SET_J10_variable_translation_elongation_rate
+
+python models/ecoli/analysis/cohort/doubling_times_violin.py \
+/scratch/groups/mcovert/wc_ecoli/paper/SET_J/20190925.070638__SET_J11_alternate_ribosome_activity
+
+# Panel C
 python models/ecoli/analysis/cohort/doubling_times_histogram_all.py \
 /scratch/PI/mcovert/wc_ecoli/paper/SET_C/20190906.151026__SET_C_4_gens_256_seeds_3_conditions_with_growth_noise_and_D_period/ \
 --variant_index 1
