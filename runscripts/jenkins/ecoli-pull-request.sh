a3972c948bddbfbec243284fe8807cc28b627e70
tahorst <thorst@stanford.edu>
2020-01-09 10:18:48 -0800

    Update PR script to test seg fault

--- runscripts/jenkins/ecoli-pull-request.sh
+++ runscripts/jenkins/ecoli-pull-request.sh
@@ -23,10 +23,13 @@ sh runscripts/jenkins/fireworks-config.sh $HOST $NAME $PORT $PASSWORD
 
 echo y | lpad reset
 
-PYTHONPATH=$PWD DESC="2 generations completion test." WC_ANALYZE_FAST=1 SINGLE_DAUGHTERS=1 N_GENS=2 MASS_DISTRIBUTION=0 \
-	PARALLEL_PARCA=1 COMPRESS_OUTPUT=0 PLOTS=ACTIVE BUILD_CAUSALITY_NETWORK=1 RAISE_ON_TIME_LIMIT=1 python runscripts/fireworks/fw_queue.py
+PYTHONPATH=$PWD DESC="singleshot" VARIANT="condition" FIRST_VARIANT_INDEX=2 LAST_VARIANT_INDEX=2 SINGLE_DAUGHTERS=1 N_GENS=8 MASS_DISTRIBUTION=0 COMPRESS_OUTPUT=1 PLOTS=ACTIVE RAISE_ON_TIME_LIMIT=1 python runscripts/fireworks/fw_queue.py
+while [ $(lpad get_fws -s READY -d count) -ge 1 ]; do
+  PYTHONPATH=$PWD rlaunch singleshot  # should succeed
+done
 
-PYTHONPATH=$PWD rlaunch rapidfire --nlaunches 0
+PYTHONPATH=$PWD DESC="rapidfire" VARIANT="condition" FIRST_VARIANT_INDEX=2 LAST_VARIANT_INDEX=2 SINGLE_DAUGHTERS=1 N_GENS=8 MASS_DISTRIBUTION=0 COMPRESS_OUTPUT=1 PLOTS=ACTIVE RAISE_ON_TIME_LIMIT=1 python runscripts/fireworks/fw_queue.py
+PYTHONPATH=$PWD rlaunch rapidfire --nlaunches 0  # should seg fault
 
 N_FAILS=$(lpad get_fws -s FIZZLED -d count)
 
