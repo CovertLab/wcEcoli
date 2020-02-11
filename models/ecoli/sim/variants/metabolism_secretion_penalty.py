070f827183987b39d346fec906be604a1bade1df
tahorst <thorst@stanford.edu>
2020-02-10 17:48:38 -0800

    Adjust secretion penalty values in variant

--- models/ecoli/sim/variants/metabolism_secretion_penalty.py
+++ models/ecoli/sim/variants/metabolism_secretion_penalty.py
@@ -1,16 +1,16 @@
 """
 Variant to compare impact of adjusting the secretion penalty in metabolism.
-Penalty varies between 0 and 10 (could go higher if needed).
+Penalty varies between 0 and 0.05 (could go higher if needed).
 
 Modifies:
 	sim_data.process.metabolism.secretion_penalty_coeff
 
 Expected variant indices (dependent on SECRETION_PENALTY):
-	4: control
-	0 (no penalty) - 10 (high penalty)
+	1: control
+	0 (no penalty) - 9 (high penalty)
 """
 
-SECRETION_PENALTY = [0, 1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 0.01, 0.1, 1, 10]
+SECRETION_PENALTY = [0, 1e-5, 1e-4, 5e-4, 1e-3, 2e-3, 5e-3, 0.01, 0.02, 0.05]
 
 
 def metabolism_secretion_penalty(sim_data, index):
