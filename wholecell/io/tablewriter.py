3ff464b4f826210e4b2d2358dd16a3b219055248
Gwanggyu Sun <ggsun@stanford.edu>
2020-02-26 11:37:43 -0800

    Support variable length columns in TableWriter/Reader

--- wholecell/io/tablewriter.py
+++ wholecell/io/tablewriter.py
@@ -29,10 +29,18 @@ FILE_ATTRIBUTES = "attributes.json"
 # Chunk type and size.
 CHUNK_HEADER = struct.Struct('>4s I')
 COLUMN_CHUNK_TYPE = b'COLM'  # column file's header chunk
+VARIABLE_COLUMN_CHUNK_TYPE = b'VCOL'  # variable-length column's header chunk
 BLOCK_CHUNK_TYPE = b'BLOC'   # data block chunk
+OFFSET_CHUNK_TYPE = b'OFST'  # offset block chunk
+
+# Datatype of offset chunks
+OFFSET_CHUNK_DTYPE = np.int64
 
 # Column header struct. See the pack() calls for field details.
 COLUMN_STRUCT = struct.Struct('>2I 2H')
+
+# Variable-length column header struct.
+VARIABLE_COLUMN_STRUCT = struct.Struct('H')
 
 COMPRESSION_TYPE_NONE = 0
 COMPRESSION_TYPE_ZLIB = 1
@@ -121,13 +129,15 @@ class AttributeTypeError(TableWriterError):
 	pass
 
 class VariableEntrySizeError(TableWriterError):
-	"""Error raised on attempt to write an entry that's not the same size as
+	"""
+	An error raised on attempt to write an entry that's not the same size as
 	the previous entries in the same Column.
 	"""
 	pass
 
 class TableExistsError(TableWriterError):
-	"""Error raised on attempt to create a Table in a directory that already
+	"""
+	An error raised on attempt to create a Table in a directory that already
 	has a table (completed or in progress).
 	"""
 	pass
@@ -163,8 +173,8 @@ class _Column(object):
 		as a lightweight alternative to TableWriter in addition to part of
 		TableWriter's internal implementation.
 	"""
-
-	def __init__(self, path, compression_type=COMPRESSION_TYPE_ZLIB):
+	def __init__(self, path, compression_type=COMPRESSION_TYPE_ZLIB,
+			variable_length=False):
 		if compression_type not in (COMPRESSION_TYPE_NONE, COMPRESSION_TYPE_ZLIB):
 			raise ValueError('Unknown compression type {}'.format(compression_type))
 
@@ -175,7 +185,10 @@ class _Column(object):
 		self._bytes_per_entry = None
 		self._elements_per_entry = None  # aka subcolumn count
 		self._compression_type = compression_type
-		self._current_block = []
+		self._variable_length = variable_length
+		self._current_data_block = []
+		self._current_offset_block = []
+		self._remaining_bytes_in_block = BLOCK_BYTES_GOAL
 
 
 	def append(self, value):
@@ -183,75 +196,116 @@ class _Column(object):
 		Appends an array-like entry to the end of a column, converting the
 		value to a 1-D array.
 
-		The first call to this method will define the column's NumPy dtype,
-		element array size (subcolumns), and element size in bytes.
-		Subsequent entries must be consistent.
+		For fixed-length columns, the first call to this method will define the
+		column's NumPy dtype, element array size (subcolumns), and element size
+		in bytes. Subsequent entries must be consistent.
+
+		For variable-length columns, the first call to this method will define
+		the column's NumPy dtype. Subsequent entries can have different
+		lengths, and an "offset" block will be written before any data block
+		to help the reader determine the boundaries of each row.
 
 		Parameters:
 			value (array-like): A NumPy ndarray or anything that can be cast to
 				an array via np.asarray, including a scalar (i.e. 0D array).
 		"""
+		if self._data.closed:
+			raise ValueError('I/O operation on closed file')
 
 		value = np.asarray(value, self._dtype)
-		if value.nbytes == 0:
-			# Replace an empty row with [0] to preserve num_rows without special
-			# cases in the rest of the writer, reader, and downstream code.
+		if not self._variable_length and value.nbytes == 0:
+			# For fixed-length columns, replace an empty row with [0] to
+			# preserve num_rows without special cases in the rest of the
+			# writer, reader, and downstream code.
 			value = np.zeros(1, value.dtype)
 		data_bytes = value.tobytes()
 
 		# First entry: Write the column header.
 		if self._dtype is None:
 			self._dtype = value.dtype
-			self._bytes_per_entry = value.nbytes
-			self._entries_per_block = int(np.ceil(BLOCK_BYTES_GOAL / value.nbytes))
-			self._elements_per_entry = value.size
-
 			descr = self._dtype.descr
 			if len(descr) == 1 and descr[0][0] == "":
 				descr = descr[0][1]
 			descr_json = json.dumps(descr, separators=(',', ':'))
 
-			chunk_header = CHUNK_HEADER.pack(
-				COLUMN_CHUNK_TYPE,
-				COLUMN_STRUCT.size + len(descr_json),
-				)
-			column_struct = COLUMN_STRUCT.pack(
-				self._bytes_per_entry,     # I: bytes/entry, before packing, compression, and CHUNK_HEADER
-				self._elements_per_entry,  # I: subcolumns
-				self._entries_per_block,   # H: packed entries/block; the last block may be smaller
-				self._compression_type,    # H: compression type code
-				)
+			# Variable-length columns
+			if self._variable_length:
+				chunk_header = CHUNK_HEADER.pack(
+					VARIABLE_COLUMN_CHUNK_TYPE,
+					VARIABLE_COLUMN_STRUCT.size + len(descr_json)
+					)
+				column_struct = VARIABLE_COLUMN_STRUCT.pack(
+					self._compression_type,  # H: compression type code
+					)
+
+			# Fixed-length columns
+			else:
+				self._bytes_per_entry = value.nbytes
+				self._entries_per_block = int(np.ceil(BLOCK_BYTES_GOAL / value.nbytes))
+				self._elements_per_entry = value.size
+
+				chunk_header = CHUNK_HEADER.pack(
+					COLUMN_CHUNK_TYPE,
+					COLUMN_STRUCT.size + len(descr_json),
+					)
+				column_struct = COLUMN_STRUCT.pack(
+					self._bytes_per_entry,     # I: bytes/entry, before packing, compression, and CHUNK_HEADER
+					self._elements_per_entry,  # I: subcolumns
+					self._entries_per_block,   # H: packed entries/block; the last block may be smaller
+					self._compression_type,    # H: compression type code
+					)
+
 			self._data.write(chunk_header + column_struct + descr_json)
 
-		# Later entry: Check consistency.
-		elif self._bytes_per_entry != len(data_bytes):
+		# Later entry: Check size consistency for fixed-length columns.
+		elif not self._variable_length and self._bytes_per_entry != len(data_bytes):
 			raise VariableEntrySizeError(
 				'Entry size in bytes, elements {} is inconsistent with {}'
-				' for Table column {}'.format(
+				' for Table column {} which is not set up for variable lengths.'.format(
 					(len(data_bytes), value.size),
 					(self._bytes_per_entry, self._elements_per_entry),
 					self._path))
 
 		# Collect up an I/O block. Compress and write it when it's full.
-		self._current_block.append(data_bytes)
-		if len(self._current_block) >= self._entries_per_block:
+		self._current_data_block.append(data_bytes)
+
+		if self._variable_length:
+			self._current_offset_block.append(value.size)
+			self._remaining_bytes_in_block -= value.nbytes
+
+			if self._remaining_bytes_in_block <= 0:
+				self._write_block()
+
+		# Fixed-length columns
+		elif len(self._current_data_block) >= self._entries_per_block:
 			self._write_block()
-		elif self._data.closed:
-			raise ValueError('I/O operation on closed file')
 
 
 	def _write_block(self):
 		'''Compress and write the current block, if any.'''
-		if self._current_block:
-			block_data = b''.join(self._current_block)
+		if self._current_data_block:
+			block_data = b''.join(self._current_data_block)
 
 			if self._compression_type == COMPRESSION_TYPE_ZLIB:
 				block_data = zlib.compress(block_data, ZLIB_LEVEL)
 
 			block_header = CHUNK_HEADER.pack(BLOCK_CHUNK_TYPE, len(block_data))
-			self._data.write(block_header + block_data)
 
-			del self._current_block[:]
+			# Write the offset block first for variable-length columns
+			if self._variable_length:
+				offset_data = np.array(
+					self._current_offset_block, OFFSET_CHUNK_DTYPE).tobytes()
+				offset_header = CHUNK_HEADER.pack(
+					OFFSET_CHUNK_TYPE, len(offset_data))
+
+				self._data.write(offset_header + offset_data + block_header + block_data)
+
+				del self._current_offset_block[:]
+				self._remaining_bytes_in_block = BLOCK_BYTES_GOAL
+			else:
+				self._data.write(block_header + block_data)
+
+			del self._current_data_block[:]
 
 
 	def close(self):
@@ -320,10 +374,12 @@ class TableWriter(object):
 	or table.
 
 	Each append() operation adds a new 'row' to the table containing one
-	'entry' (or table cell) per 'column'.  Each 'column' corresponds to a
-	'field' and holds a fixed array size and data type for all of its entries.
-	append() will convert scalar values and higher dimension arrays to 1D
-	arrays.  The 1D array elements are called 'subcolumns'.
+	'entry' (or table cell) per column.  Each 'column' corresponds to a 'field'
+	and holds a fixed data type for all of its entries. append() will convert
+	scalar values and higher dimension arrays to 1D arrays. For fixed-length
+	columns, each 1D array must be of fixed size, and each of the elements are
+	called 'subcolumns'. For variable-length columns, the 1D arrays given for
+	each row may have different sizes.
 
 	Both simple and structured ndarray dtypes are supported.  Structured arrays
 	are a good way to work around the dimensional limitations.
@@ -347,6 +403,7 @@ class TableWriter(object):
 	def __init__(self, path):
 		self._path = filepath.makedirs(path)
 		self._columns = None
+		self._variable_length_columns = set()
 
 		self._attributes = {}
 		self._attributes_filename = os.path.join(path, FILE_ATTRIBUTES)
@@ -377,13 +434,15 @@ class TableWriter(object):
 		-----
 		All fields must be provided every time this method is called.
 		"""
-
+		# First call - instantiate all columns
 		if self._columns is None:
 			self._columns = {
-				name:_Column(os.path.join(self._path, name))
+				name: _Column(os.path.join(self._path, name),
+					variable_length=(name in self._variable_length_columns))
 				for name in namesAndValues.viewkeys()
 				}
 
+		# Later calls - check for missing or unrecognized fields
 		else:
 			missingFields = self._columns.viewkeys() - namesAndValues.viewkeys()
 			unrecognizedFields = namesAndValues.viewkeys() - self._columns.viewkeys()
@@ -452,6 +511,19 @@ class TableWriter(object):
 		filepath.write_json_file(self._attributes_filename, self._attributes, indent=1)
 
 
+	def set_variable_length_columns(self, *column_names):
+		"""
+		Sets the names of columns that should have entries with variable
+		lengths. This must be set before any values are appended to the column.
+
+		Args:
+			*column_names (tuple[str]): Names of columns that should have
+			entries with variable lengths.
+		"""
+		for name in column_names:
+			self._variable_length_columns.add(name)
+
+
 	def close(self):
 		"""
 		Close the output files (columns).
@@ -461,7 +533,6 @@ class TableWriter(object):
 		Trying to append after closing will raise an error.
 
 		"""
-
 		if self._columns is not None:
 			for column in self._columns.viewvalues():
 				column.close()
