7ece0a9a21d6f59bc9f1fc3aa91c970ac10b52a1
tahorst <thorst@stanford.edu>
2020-05-22 14:53:48 -0700

    Add parcaAnalysis task and manual script

---
+++ models/ecoli/analysis/parca/template.py
@@ -0,0 +1,43 @@
+"""
+Template for parca analysis plots
+
+@organization: Covert Lab, Department of Bioengineering, Stanford University
+@date: Created 5/22/20
+"""
+
+from __future__ import absolute_import, division, print_function
+
+import cPickle
+import os
+
+from matplotlib import pyplot as plt
+import numpy as np
+
+from models.ecoli.analysis import variantAnalysisPlot
+from wholecell.analysis.analysis_tools import exportFigure
+from wholecell.utils import filepath
+
+
+class Plot(variantAnalysisPlot.VariantAnalysisPlot):
+	def do_plot(self, inputDir, plotOutDir, plotOutFileName, simDataFile, validationDataFile, metadata):
+		if not os.path.isdir(inputDir):
+			raise Exception('inputDir does not currently exist as a directory')
+
+		filepath.makedirs(plotOutDir)
+
+		with open(simDataFile, 'rb') as f:
+			sim_data = cPickle.load(f)
+		with open(validationDataFile, 'rb') as f:
+			validation_data = cPickle.load(f)
+
+		plt.figure()
+
+		### Create Plot ###
+
+		plt.tight_layout()
+		exportFigure(plt, plotOutDir, plotOutFileName, metadata)
+		plt.close('all')
+
+
+if __name__ == "__main__":
+	Plot().cli()
---
+++ runscripts/manual/analysisParca.py
@@ -0,0 +1,41 @@
+"""
+Runs all parca analysis plots for a given sim.
+
+Run with '-h' for command line help.
+"""
+
+from __future__ import absolute_import, division, print_function
+
+import os
+
+from runscripts.manual.analysisBase import AnalysisBase
+from wholecell.fireworks.firetasks.analysisParca import AnalysisParcaTask
+from wholecell.fireworks.firetasks.parca import ParcaTask
+from wholecell.utils import constants
+
+
+class AnalysisParca(AnalysisBase):
+	"""Runs some or all the ACTIVE parca analysis plots for a given sim."""
+
+	def run(self, args):
+		# TODO: where to put output?
+		# Does kb/plotOut make sense? - Is this compatible with cloud runs?
+		output_dir = os.path.join(args.sim_path, constants.KB_OUTPUT_DIR)
+		input_sim_data = os.path.join(args.sim_path,
+			ParcaTask.OUTPUT_SUBDIR, constants.SERIALIZED_SIM_DATA_FILENAME)
+
+		task = AnalysisParcaTask(
+			input_directory=args.sim_path,
+			input_sim_data=input_sim_data,
+			input_validation_data=args.input_validation_data,
+			output_plots_directory=output_dir,
+			metadata=args.metadata,
+			output_filename_prefix=args.output_prefix,
+			**self.select_analysis_keys(args)
+			)
+		task.run_task({})
+
+
+if __name__ == '__main__':
+	analysis = AnalysisParca()
+	analysis.cli()
