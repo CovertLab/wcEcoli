a63bc1b93be8ca41ae2a88902b16daefe18256ec
tahorst <thorst@stanford.edu>
2020-05-23 10:09:54 -0700

    Add metabolite_concentrations parca analysis

--- models/ecoli/analysis/parca/__init__.py
+++ models/ecoli/analysis/parca/__init__.py
@@ -1,10 +1,12 @@
 # Active analysis modules to run, in this order.
 # Tip: Edit this during development to run ones you're working on first.
 ACTIVE = [
+	'metabolite_concentrations.py',
 ]
 
 TAGS = {
 	'ACTIVE': ACTIVE,   # all active analyses in this category
 	'CORE': [           # the default list to run in development
+		'metabolite_concentrations.py',
 		],
 	}
