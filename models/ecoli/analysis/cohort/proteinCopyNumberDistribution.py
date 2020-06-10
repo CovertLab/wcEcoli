e86b3ee9493d65cdd04aa4c8d486621f772d4e0d
Jerry Morrison <1fish2@users.noreply.github.com>
2020-06-09 22:24:20 -0700

    comment a MatplotlibDeprecationWarning

--- models/ecoli/analysis/cohort/proteinCopyNumberDistribution.py
+++ models/ecoli/analysis/cohort/proteinCopyNumberDistribution.py
@@ -169,6 +169,9 @@ class Plot(cohortAnalysisPlot.CohortAnalysisPlot):
 
 			# Go back and reset y axis upper limit
 			for gen_idx in range(n_generation):
+				# TODO: MatplotlibDeprecationWarning: Adding an axes using the same arguments as a previous axes currently reuses the earlier instance.
+				#  In a future version, a new instance will always be created and returned.  Meanwhile, this warning can be suppressed, and the
+				#  future behavior ensured, by passing a unique label to each axes instance.
 				ax = plt.subplot(gs[i + 1, gen_idx])
 				ax.set_ylim([0, 1.1*max_bin_prob])
 
