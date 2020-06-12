40b5ad5c4686ec15e0de8a76043a44916b19f656
tahorst <thorst@stanford.edu>
2020-06-12 15:56:54 -0700

    Add additional intepolation functions

--- models/ecoli/analysis/parca/interpolation.py
+++ models/ecoli/analysis/parca/interpolation.py
@@ -7,11 +7,11 @@ Plots for interpolation functions.
 
 from __future__ import absolute_import, division, print_function
 
-import cPickle
 import os
 
 from matplotlib import pyplot as plt
 from matplotlib import gridspec
+from six.moves import cPickle
 import numpy as np
 
 from models.ecoli.analysis import parcaAnalysisPlot
@@ -29,6 +29,7 @@ def get_raw(data, x_col, y_col, factor=1):
 			y = y.asNumber()
 		ys.append(y)
 	return xs, ys
+
 
 class Plot(parcaAnalysisPlot.ParcaAnalysisPlot):
 	def do_plot(self, input_dir, plot_out_dir, plot_out_filename, sim_data_file, validation_data_file, metadata):
@@ -48,9 +49,20 @@ class Plot(parcaAnalysisPlot.ParcaAnalysisPlot):
 			growth.getRnapElongationRate: get_raw(raw_data.growthRateDependentParameters, 'doublingTime', 'rnaPolymeraseElongationRate'),
 			mass.get_dna_critical_mass: None,
 			mass.getAvgCellDryMass: get_raw(raw_data.dryMassComposition, 'doublingTime', 'averageDryMass'),
-			# growth.getFractionMass,
-			# growth.getTrnaDistribution,
 			}
+
+		# Interpolation functions that return values in a dictionary
+		indexed_functions = {
+			(mass.getMassFraction, fraction): get_raw(raw_data.dryMassComposition, 'doublingTime', '{}MassFraction'.format(fraction))
+			for fraction in mass.getMassFraction(45*units.min)
+			}
+		indexed_functions.update({
+			(mass.getFractionMass, fraction): None
+			for fraction in mass.getFractionMass(45*units.min)
+			})
+
+		# TODO: handle getTrnaDistribution and all 86 outputs from 'molar_ratio_to_16SrRNA'
+		# mass.getTrnaDistribution(45.*units.min)['molar_ratio_to_16SrRNA']
 
 		# Doubling times to show on plot (extended range including all conditions)
 		doubling_times = np.unique([
@@ -60,12 +72,14 @@ class Plot(parcaAnalysisPlot.ParcaAnalysisPlot):
 		doubling_time_range = np.arange(0.5 * doubling_times.min(), 1.2 * doubling_times.max())
 
 		# Create Plot
-		plt.figure(figsize=(10, 20))
-		gs = gridspec.GridSpec(len(interpolation_functions), 1)
+		plt.figure(figsize=(20, 20))
+		n_plots = len(interpolation_functions) + len(indexed_functions)
+		cols = 5
+		gs = gridspec.GridSpec(int(np.ceil(n_plots / cols)), cols)
 
 		## Plot values for each function
 		for i, (fun, data) in enumerate(sorted(interpolation_functions.items())):
-			ax = plt.subplot(gs[i, 0])
+			ax = plt.subplot(gs[i // cols, i % cols])
 
 			# Get interpolation values and handle units
 			y_interp = np.array([fun(dt) for dt in units.min * doubling_time_range])
@@ -80,6 +94,40 @@ class Plot(parcaAnalysisPlot.ParcaAnalysisPlot):
 				ax.plot(data[0], data[1], 'or')
 			for dt in doubling_times:
 				ax.axvline(dt, linestyle='--', color='k', linewidth=1)
+
+			# Formatting
+			ax.spines['top'].set_visible(False)
+			ax.spines['right'].set_visible(False)
+			ax.set_ylabel(label)
+
+		row = len(interpolation_functions)
+		for (fun, key), data in sorted(indexed_functions.items()):
+			ax = plt.subplot(gs[row // cols, row % cols])
+			row += 1
+
+			# Get interpolation values and handle units
+			values = []
+			unit = None
+			for dt in units.min * doubling_time_range:
+				try:
+					value = fun(dt)[key]
+					if units.hasUnit(value):
+						unit = str(units.getUnit(value)).strip('1 []')
+						value = value.asNumber()
+				except Exception as e:
+					value = np.nan
+				values.append(value)
+			y_interp = np.array(values)
+			label = '{}\n{}'.format(fun.__name__, key)
+			if unit:
+				label += '\n({})'.format(unit)
+
+			# Plot data
+			ax.plot(doubling_time_range, y_interp)
+			if data:
+				ax.plot(data[0], data[1], 'or')
+			for dt in doubling_times:
+				ax.axvline(dt, linestyle='--', color='k', linewidth=0.5)
 
 			# Formatting
 			ax.spines['top'].set_visible(False)
