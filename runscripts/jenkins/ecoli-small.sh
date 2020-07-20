85f2cf3c148280fd154ee5d127800f3a065f198a
tahorst <thorst@stanford.edu>
2020-07-20 15:01:29 -0700

    Update Jenkins scripts for py3

--- runscripts/jenkins/ecoli-small.sh
+++ runscripts/jenkins/ecoli-small.sh
@@ -1,8 +1,8 @@
 set -e
 
-module load wcEcoli/sherlock2
+module load wcEcoli/python3
 
-WCECOLI_PYENV=wcEcoli2
+WCECOLI_PYENV=wcEcoli3
 pyenv local ${WCECOLI_PYENV}
 
 make clean compile
@@ -10,4 +10,4 @@ make clean compile
 PYTHONPATH=$PWD:$PYTHONPATH pytest --cov=wholecell --cov-report xml \
     --junitxml=unittests.xml
 
-(export PYENV_VERSION="mypy:${WCECOLI_PYENV}"; echo ---Running mypy---; mypy --py2)
+(export PYENV_VERSION="mypy:${WCECOLI_PYENV}"; echo ---Running mypy---; mypy)
