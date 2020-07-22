061d5fa4f223a083837b687d460d586b91aebd4d
tahorst <thorst@stanford.edu>
2020-07-21 17:13:25 -0700

    Update Jenkins scripts for mypy

--- runscripts/jenkins/ecoli-small.sh
+++ runscripts/jenkins/ecoli-small.sh
@@ -1,13 +1,11 @@
 set -e
 
 module load wcEcoli/python3
-
-WCECOLI_PYENV=wcEcoli3
-pyenv local ${WCECOLI_PYENV}
+pyenv local wcEcoli3
 
 make clean compile
 
 PYTHONPATH=$PWD:$PYTHONPATH pytest --cov=wholecell --cov-report xml \
     --junitxml=unittests.xml
 
-(export PYENV_VERSION="mypy:${WCECOLI_PYENV}"; echo ---Running mypy---; mypy)
+runscripts/debug/mypy.sh
