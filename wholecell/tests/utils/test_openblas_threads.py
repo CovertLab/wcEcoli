f2204838b0c6a2336d657c5031a1bbeb6de6ba9a
Jerry Morrison <1fish2@users.noreply.github.com>
2020-08-05 00:01:03 -0700

    discovery needs a TestCase subclass

--- wholecell/tests/utils/test_openblas_threads.py
+++ wholecell/tests/utils/test_openblas_threads.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 """
-Any of these ways works to run this test:
+Any of these ways works to run this test. The first one is quiet if the test
+passes. The others always show stdout:
 	pytest wholecell/tests/utils/test_openblas_threads.py
 	python -m wholecell.tests.utils.test_openblas_threads
 	wholecell/tests/utils/test_openblas_threads.py
@@ -27,27 +28,28 @@ def dot_product():
 	return diff.dot(mass)
 
 
-def test_openblas():
-	"""Compare a dot product running with various numbers of OpenBLAS threads."""
-	products = []
-	print(f'{"THREADS":>7} {"DOT PRODUCT":>25} {"DIFF":>25}')
+class Test_openblas_threads(unittest.TestCase):
+	def test_openblas(self):
+		"""Compare a dot product running with various numbers of OpenBLAS threads."""
+		products = []
+		print(f'{"THREADS":>7} {"DOT PRODUCT":>25} {"DIFF":>25}')
 
-	for num_threads in range(1, 13):
-		env = dict(os.environ, OPENBLAS_NUM_THREADS=str(num_threads))
-		command = 'python -m wholecell.tests.utils.test_openblas_threads DOT'.split()
-		dot = float(fp.run_cmd(command, env=env))
+		for num_threads in range(1, 13):
+			env = dict(os.environ, OPENBLAS_NUM_THREADS=str(num_threads))
+			command = 'python -m wholecell.tests.utils.test_openblas_threads DOT'.split()
+			dot = float(fp.run_cmd(command, env=env))
 
-		products.append(dot)
-		diff = dot - products[0]
-		print(f'{num_threads:7} {dot:25} {diff:25}')
+			products.append(dot)
+			diff = dot - products[0]
+			print(f'{num_threads:7} {dot:25} {diff:25}')
 
-	np_products = np.array(products)
-	reference = np.full(len(products), products[0])
-	np.testing.assert_allclose(np_products, reference, rtol=1e-17)
+		np_products = np.array(products)
+		reference = np.full(len(products), products[0])
+		np.testing.assert_allclose(np_products, reference, rtol=1e-17)
 
 
 if __name__ == '__main__':
 	if sys.argv[1:] == ['DOT']:
 		print(dot_product())
 	else:
-		unittest.main(defaultTest='test_openblas')  # Why won't it auto-discover test_openblas?
+		unittest.main()
