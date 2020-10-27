5416a127d51e3623f487d5de7809792ee932b04a
Taryn Gillies <tgillies@stanford.edu>
2020-10-26 23:24:12 -0700

    TU to operons

--- prototypes/operons/TU_to_operons.py
+++ prototypes/operons/TU_to_operons.py
@@ -1,11 +1,40 @@
 from reconstruction.spreadsheets import tsv_reader
+from collections import defaultdict
 
 
-def TU_to_operons(polycistron_file):
-    with tsv_reader(polycistron_file) as reader:
+def TU_to_operons(operon_rnas):
+    with tsv_reader(operon_rnas) as reader:
         tsv_fieldnames = reader.fieldnames
-        polycistrons = list(reader)
+        tsv_data = list(reader)
 
+
+    geneid_to_rna_names = defaultdict(list)
+    rna_name_to_geneids = defaultdict(list)
+    polycistron_list = []
+
+    for rna in tsv_data:
+        if rna['id'].count('_') > 1:
+            polycistron_list.append(rna['id'])
+        rna_name_to_geneids[rna['id']] = rna['gene_set']
+        for gene in rna['gene_set']:
+            geneid_to_rna_names[gene].append(rna['id'])
+
+
+    polycistron_list = sorted(polycistron_list, key=lambda x: x.count("_"), reverse=True)
+    rna_names_list = sorted(list(rna_name_to_geneids.keys()), key=len, reverse=True)
+
+    operons = defaultdict(list)
+    while len(polycistron_list) > 0:
+        for pc in polycistron_list:
+            for gene in rna_name_to_geneids[pc]:
+                for rna in geneid_to_rna_names[gene]:
+                    if rna not in operons[pc]:
+                        operons[pc].append(rna)
+                    if rna in polycistron_list:
+                        polycistron_list.remove(rna)
+
+
+    
 
 
 
