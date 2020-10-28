3b3870c09ad73ba4eccccc4724f799b6787de4ca
cs <cs.temporary@icloud.com>
2020-10-28 17:39:04 +0000

    Calculate mmol_to_counts and periplasm_volume in derive_shape

--- colony/processes/wcecoli_derive_shape.py
+++ colony/processes/wcecoli_derive_shape.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import, division, print_function
 
 import copy
 
+from scipy import constants
+
 from vivarium.core.process import Deriver
 from vivarium.library.dict_utils import deep_merge
 from vivarium.library.units import units
@@ -11,10 +13,18 @@ from vivarium_cell.processes.derive_globals import (
 )
 
 
+AVOGADRO = constants.N_A * 1 / units.mmol
+
+
 class WcEcoliDeriveShape(Deriver):
 
 	defaults = {
 		'width': 1,  # um
+		# Source: Wülfing, C., & Plückthun, A. (1994). Protein folding
+		# in the periplasm of Escherichia coli. Molecular Microbiology,
+		# 12(5), 685–692.
+		# https://doi.org/10.1111/j.1365-2958.1994.tb01056.x
+		'periplasm_volume_fraction': 0.3,
 	}
 	name = "wcEcoliDeriveShape"
 
@@ -43,10 +53,12 @@ class WcEcoliDeriveShape(Deriver):
 	def ports_schema(self):
 		default_state = {
 			'global': {
-				'volume': 0.0 * units.fL,
+				'volume': 0 * units.fL,
 				'width': self.parameters['width'],
-				'length': 0.0,
-				'surface_area': 0.0,
+				'length': 0,
+				'surface_area': 0,
+				'mmol_to_counts': 0,
+				'periplasm_volume': 0,
 			}
 		}
 
@@ -71,10 +83,15 @@ class WcEcoliDeriveShape(Deriver):
 
 		length = length_from_volume(volume.magnitude, width)
 		surface_area = surface_area_from_length(length, width)
+		mmol_to_counts = (AVOGADRO * volume).to('L/mmol')
+		periplasm_volume = volume * self.parameters[
+			'periplasm_volume_fraction']
 
 		return {
 			'global': {
 				'length': length,
 				'surface_area': surface_area,
+				'mmol_to_counts': mmol_to_counts,
+				'periplasm_volume': periplasm_volume,
 			},
 		}
