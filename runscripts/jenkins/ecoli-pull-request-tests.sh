117f38808a0288ca1eafe3231757927c7e3170a5
tahorst <thorst@stanford.edu>
2020-11-23 11:57:14 -0800

    Add new PR script for pytest only

---
+++ runscripts/jenkins/ecoli-pull-request-tests.sh
@@ -0,0 +1,14 @@
+set -e
+
+module load wcEcoli/python3
+
+### -------------------------------------------------------------------
+### Edit this line to make the PR build use another pyenv like wcEcoli3-staging.
+### Revert it to `wcEcoli3` before merging the PR into master.
+### -------------------------------------------------------------------
+pyenv local wcEcoli3
+
+make clean compile
+
+PYTHONPATH=$PWD:$PYTHONPATH pytest --cov=wholecell --cov-report xml \
+    --junitxml=unittests.xml
