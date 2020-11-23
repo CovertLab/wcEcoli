3e0c0ebe83deda4c766bde58ef25e39857565ed2
tahorst <thorst@stanford.edu>
2020-11-23 11:57:36 -0800

    Add new PR script for mypy type checking only

---
+++ runscripts/jenkins/ecoli-pull-request-types.sh
@@ -0,0 +1,12 @@
+set -e
+
+module load wcEcoli/python3
+
+### -------------------------------------------------------------------
+### Edit this line to make the PR build use another pyenv like wcEcoli3-staging.
+### Revert it to `wcEcoli3` before merging the PR into master.
+### -------------------------------------------------------------------
+pyenv local wcEcoli3
+
+# Run mypy type checking
+runscripts/debug/mypy.sh
