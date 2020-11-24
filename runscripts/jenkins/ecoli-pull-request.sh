ed0fea56705aceb40883ea9aa1e794dcaaa76913
tahorst <thorst@stanford.edu>
2020-11-24 09:50:23 -0800

    Clarify pyenv for PR builds

--- runscripts/jenkins/ecoli-pull-request.sh
+++ runscripts/jenkins/ecoli-pull-request.sh
@@ -5,6 +5,11 @@ PASSWORD=$4
 
 set -e
 
+### ---------------------------------------------------------------------------
+### Edit the pyenv in setup-environment.sh to make the PR build temporarily use
+### another pyenv for testing (eg. wcEcoli3-staging). Revert the change before
+### merging the PR into master to prevent changing it for other Jenkins builds.
+### ---------------------------------------------------------------------------
 source runscripts/jenkins/setup-environment.sh
 sh runscripts/jenkins/fireworks-config.sh $HOST $NAME $PORT $PASSWORD
 
--- runscripts/jenkins/setup-environment.sh
+++ runscripts/jenkins/setup-environment.sh
@@ -3,10 +3,7 @@ set -e
 export PYTHONPATH=$PWD
 module load wcEcoli/python3
 
-### -------------------------------------------------------------------
-### Edit this line to make the PR build use another pyenv like wcEcoli3-staging.
-### Revert it to `wcEcoli3` before merging the PR into master.
-### -------------------------------------------------------------------
+### Edit this line to make this branch use another pyenv like wcEcoli3-staging
 pyenv local wcEcoli3
 
 make clean compile
