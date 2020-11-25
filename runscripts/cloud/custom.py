65a836efd82b41309ce5c7861374aa989b56284f
Jerry Morrison <1fish2@users.noreply.github.com>
2020-11-24 16:07:06 -0800

    CustomWorkflow CustomWorkflow more

--- runscripts/cloud/custom.py
+++ runscripts/cloud/custom.py
@@ -4,10 +4,23 @@
 Compute Engine (GCE), with access to the Google Cloud Storage (GCS) files from a
 WCM workflow run.
 
-Use this as as starting point for experimental analyses and other custom code.
+Purpose: Use this as as starting point for exploratory analyses and other custom
+code that you want to iterate on or use temporarily rather than adding it to the
+normal wcm.py workflow. Tweak it to fetch the relevant simOut/ files, e.g.
+specific tables.
+
+Alternative: An easier approach is to use gcsfuse to mount the storage bucket on
+your development computer then run the analysis there. gcsfuse will fetch files
+on demand. This might run slower since gcsfuse takes a bunch of network requests
+to enumerate directories, fetch files, and check if cached files are up to date.
+Also, fetching files incurs cloud data egress costs at $0.12 per GB.
+
+Alternative: Run lots of `gsutil cp -m ...` commands to download the needed
+simOut/ files to local directories, then process them locally. This would save
+download time once you write the download code.
 
 Prerequisite: Run `cloud/build-wcm.sh` to build a Docker Image containing the
-custom code you want the GCE workers to run. The code needn't be checked in to
+custom code you want the GCE workers to run. That code needn't be checked in to
 git. It just needs to be in the wcEcoli/ directory (and not listed in
 `.gcloudignore` or `.dockerignore`).
 
@@ -15,9 +28,26 @@ For the custom code to read input files from a WCM workflow run, the owner_id,
 timestamp arg, and description CLI args must match those of the previous run,
 and `add_analysis_task()` must list the needed input directories and files.
 
-You can iterate on the code by rebuilding the Image then re-running this
-workflow builder script, or (if the same Firetask definition is fine), just use
-`lpad rerun_fws -i <N>` to rerun the Firetask.
+This CustomWorkflow builder runs on your development computer so you can change
+it without rebuilding the Docker Image. When you run it again, if your
+fireworkers are still running in GCE, pass `-w0` so it won't try to recreate
+them with clashing names. Or don't sweat it -- this uploads a workflow and then
+tries to create fireworkers; if the second part fails, your running fireworkers
+will still run the newly uploaded Firetasks.
+
+You can create additional fireworker VMs in GCE like this:
+
+	gce --base 20 --count 3 fireworker-$USER
+
+to create workers named "fireworker-$USER-20", "fireworker-$USER-21", and
+"fireworker-$USER-22". Here, `--base 20` sets the base index to 20 so the new
+names won't collide with running fireworkers "fireworker-$USER-0" through
+"fireworker-$USER-19".
+
+You can iterate on the custom code by rebuilding the Docker Image then
+re-running this CustomWorkflow builder or (if the same Firetask definition is
+fine), just use `lpad rerun_fws -i <N>` to rerun a Firetask. Use the `gce`
+command to launch a fireworker VM if they all timed out.
 """
 
 import os
