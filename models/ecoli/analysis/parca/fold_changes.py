13b0a0edeb80e344d888d90e61ac154392a1e52f
tahorst <thorst@stanford.edu>
2020-11-24 18:11:19 -0800

    Plot histogram of fold change datasets

--- models/ecoli/analysis/parca/fold_changes.py
+++ models/ecoli/analysis/parca/fold_changes.py
@@ -5,6 +5,7 @@ Compare fold changes from different sources.
 from six.moves import cPickle
 import os
 
+from matplotlib import gridspec
 from matplotlib import pyplot as plt
 import numpy as np
 from scipy import stats
@@ -43,31 +44,70 @@ class Plot(parcaAnalysisPlot.ParcaAnalysisPlot):
 		nca = np.array(nca)
 		pearson = stats.pearsonr(original, nca)
 
+		# Get data unique to each dataset
+		original_unique_pairs = set(original_fold_changes) - set(nca_fold_changes)
+		nca_unique_pairs = set(nca_fold_changes) - set(original_fold_changes)
+		original_unique = np.array([original_fold_changes[pair] for pair in original_unique_pairs])
+		nca_unique = np.array([nca_fold_changes[pair] for pair in nca_unique_pairs])
+
 		# Get range for y=x line
 		min_val = min(original.min(), nca.min())
 		max_val = max(original.max(), nca.max())
+		unique_min = min(original_unique.min(), nca_unique.min())
+		unique_max = max(original_unique.max(), nca_unique.max())
 
-		plt.figure()
+		plt.figure(figsize=(5, 15))
+		gs = gridspec.GridSpec(3, 1)
+		scatter_ax = plt.subplot(gs[0, 0])
+		shared_hist_ax = plt.subplot(gs[1, 0])
+		unique_hist_ax = plt.subplot(gs[2, 0])
 
-		# Plot scatter
-		plt.plot(original, nca, 'x', label='log2 fold changes')
-		xlim = plt.xlim()
-		ylim = plt.ylim()
+		# Scatter plot
+		## Plot comparison
+		scatter_ax.plot(original, nca, 'x', label='log2 fold changes')
+		xlim = scatter_ax.get_xlim()
+		ylim = scatter_ax.get_ylim()
 
-		# Plot y=x and revert to previous limits
-		plt.plot([min_val, max_val], [min_val, max_val], 'k--', label='y=x')
-		plt.xlim(xlim)
-		plt.ylim(ylim)
+		## Plot y=x and revert to previous limits
+		scatter_ax.plot([min_val, max_val], [min_val, max_val], 'k--', label='y=x')
+		scatter_ax.set_xlim(xlim)
+		scatter_ax.set_ylim(ylim)
 
-		# Format axes
-		plt.xlabel('Original fold change')
-		plt.ylabel('NCA predicted fold change')
-		plt.gca().spines['top'].set_visible(False)
-		plt.gca().spines['right'].set_visible(False)
-		plt.legend(fontsize=8, frameon=False)
+		## Format axes
+		scatter_ax.set_xlabel('Original fold change')
+		scatter_ax.set_ylabel('NCA predicted fold change')
+		scatter_ax.spines['top'].set_visible(False)
+		scatter_ax.spines['right'].set_visible(False)
+		scatter_ax.legend(fontsize=8, frameon=False)
 
-		# Display stats
-		plt.title(f'Pearson r={pearson[0]:.3f} (p={pearson[1]:.0e}, n={len(original)})', fontsize=10)
+		## Display stats
+		scatter_ax.set_title(f'Pearson r={pearson[0]:.3f} (p={pearson[1]:.0e}, n={len(original)})', fontsize=10)
+
+		# Histogram for shared fold change pairs
+		## Plot data
+		shared_hist_ax.hist(original, range=(min_val, max_val), bins=20, alpha=0.5, label='Shared original fold changes')
+		shared_hist_ax.hist(nca, range=(min_val, max_val), bins=20, alpha=0.5, label='Shared NCA fold changes')
+
+		## Format axes
+		shared_hist_ax.set_xlabel('Fold change (shared regulation in datasets)')
+		shared_hist_ax.set_ylabel('# of regulatory pairs')
+		shared_hist_ax.spines['top'].set_visible(False)
+		shared_hist_ax.spines['right'].set_visible(False)
+		shared_hist_ax.legend(fontsize=8, frameon=False)
+
+		# Histogram for shared fold change pairs
+		## Plot data
+		unique_hist_ax.hist(original_unique, range=(unique_min, unique_max),
+			bins=20, alpha=0.5, label=f'Unique original fold changes (N={len(original_unique)})')
+		unique_hist_ax.hist(nca_unique, range=(unique_min, unique_max),
+			bins=20, alpha=0.5, label=f'Unique NCA fold changes (N={len(nca_unique)})')
+
+		## Format axes
+		unique_hist_ax.set_xlabel('Fold change (unique regulation in datasets)')
+		unique_hist_ax.set_ylabel('# of regulatory pairs')
+		unique_hist_ax.spines['top'].set_visible(False)
+		unique_hist_ax.spines['right'].set_visible(False)
+		unique_hist_ax.legend(fontsize=8, frameon=False)
 
 		# Save figure
 		plt.tight_layout()
