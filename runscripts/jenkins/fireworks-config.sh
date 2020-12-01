6dd7e89b33aebb72f0afab230bc1365c8c447cf0
tahorst <thorst@stanford.edu>
2020-11-30 17:14:53 -0800

    Update fireworks-config.sh for atlas

--- runscripts/jenkins/fireworks-config.sh
+++ runscripts/jenkins/fireworks-config.sh
@@ -1,21 +1,24 @@
-HOST=$1
-NAME=$2
-PORT=$3
-PASSWORD=$4
+#! /usr/bin/env sh
 
-set -e
+set -eu
+
+WC_MONGO_DB=$1
 
 mkdir -p /scratch/PI/mcovert/jenkins/fireworks/logs/launchpad
+echo "authsource: admin" > my_launchpad.yaml
+echo "host: mongodb+srv://${WC_MONGO_USER}:${WC_MONGO_PW}@${WC_MONGO_CLUSTER}/${WC_MONGO_DB}?retryWrites=true&w=majority" >> my_launchpad.yaml
 echo "logdir: /scratch/PI/mcovert/jenkins/fireworks/logs/launchpad" >> my_launchpad.yaml
-echo "host: $HOST" >> my_launchpad.yaml
-echo "name: $NAME" >> my_launchpad.yaml
-echo "username: fireworks" >> my_launchpad.yaml
-echo "password: $PASSWORD" >> my_launchpad.yaml
-echo "port: $PORT" >> my_launchpad.yaml
+echo "mongoclient_kwargs: {}" >> my_launchpad.yaml
+echo "name: null" >> my_launchpad.yaml
+echo "password: null" >> my_launchpad.yaml
+echo "port: null" >> my_launchpad.yaml
+echo "ssl: false" >> my_launchpad.yaml
+echo "ssl_ca_certs: null" >> my_launchpad.yaml
+echo "ssl_certfile: null" >> my_launchpad.yaml
+echo "ssl_keyfile: null" >> my_launchpad.yaml
+echo "ssl_pem_passphrase: null" >> my_launchpad.yaml
 echo "strm_lvl: INFO" >> my_launchpad.yaml
+echo "uri_mode: true" >> my_launchpad.yaml
 echo "user_indices: []" >> my_launchpad.yaml
+echo "username: null" >> my_launchpad.yaml
 echo "wf_user_indices: []" >> my_launchpad.yaml
-
-# So pymongo 3.9.0+ will work with older MongoDB servers:
-echo "mongoclient_kwargs:" >> my_launchpad.yaml
-echo "  retryWrites: false" >> my_launchpad.yaml
