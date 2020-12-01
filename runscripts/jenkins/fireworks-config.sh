3f57aa7142c1140e15495ed53959d8afc8a6236b
tahorst <thorst@stanford.edu>
2020-11-30 17:32:06 -0800

    Add text description to fireworks-config.sh

--- runscripts/jenkins/fireworks-config.sh
+++ runscripts/jenkins/fireworks-config.sh
@@ -1,5 +1,12 @@
 #! /usr/bin/env sh
 
+# Create a new launchpad to allow fireworks to access a database to run a workflow.
+# $WC_MONGO_USER, $WC_MONGO_PW, $WC_MONGO_CLUSTER should be set by Jenkins.
+# Passing in a new database name as the first arg to this script will create
+# a new database in the cluster for easy expansion for new Jenkins jobs.
+# Be sure not to reuse a database name for another job or else jobs may wipe
+# other workflows.
+
 set -eu
 
 WC_MONGO_DB=$1
