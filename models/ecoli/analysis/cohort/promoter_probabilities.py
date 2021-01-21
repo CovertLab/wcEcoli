f3033cabc6f354256123d21cf1a7bd1525434171
tahorst <thorst@stanford.edu>
2021-01-21 09:37:53 -0800

    Add fraction limited to promoter probability

--- models/ecoli/analysis/cohort/promoter_probabilities.py
+++ models/ecoli/analysis/cohort/promoter_probabilities.py
@@ -9,7 +9,7 @@ import numpy as np
 
 from models.ecoli.analysis import cohortAnalysisPlot
 from models.ecoli.analysis.AnalysisPaths import AnalysisPaths
-from wholecell.analysis.analysis_tools import exportFigure
+from wholecell.analysis.analysis_tools import exportFigure, read_bulk_molecule_counts
 from wholecell.io.tablereader import TableReader
 
 
@@ -19,6 +19,7 @@ class Plot(cohortAnalysisPlot.CohortAnalysisPlot):
 
 		expected = []
 		bound = []
+		active_tfs = []
 		promoters = []
 		for sim_dir in ap.get_cells():
 			simOutDir = os.path.join(sim_dir, 'simOut')
@@ -31,21 +32,34 @@ class Plot(cohortAnalysisPlot.CohortAnalysisPlot):
 			bound.append(rna_synth_reader.readColumn('nActualBound'))
 			promoters.append(rna_synth_reader.readColumn('n_available_promoters'))
 
+			tf_ids = [f'{tf}[c]' for tf in rna_synth_reader.readAttribute('tf_ids')]
+			active_counts, = read_bulk_molecule_counts(simOutDir, (tf_ids,))
+			active_tfs.append(active_counts)
+
 		expected = np.vstack(expected).mean(0)
 		bound = np.vstack(bound)
+		active_tfs = np.vstack(active_tfs)
+
+		# Calculate actual average promoter bound probabilities
 		promoter_prob = (bound / np.vstack(promoters))
 		promoter_prob[bound == 0] = 0
 		actual = promoter_prob.mean(0)
 
+		# Calculate how often the promoter binding was limited by the number of
+		# active transcription factors
+		active_limited_timesteps = (active_tfs == 0) & (bound > 0)
+		frac_limited = active_limited_timesteps.sum(0) / active_limited_timesteps.shape[0]
+
 		min_prob = min(expected[expected > 0].min(), actual[actual > 0].min())
 		max_prob = np.max((expected, actual))
 
-		plt.figure()
+		fig = plt.figure()
 		ax = plt.gca()
 
 		# Plot data
 		ax.plot([0, max_prob], [0, max_prob], '--k')
-		ax.plot(expected, actual, 'o', alpha=0.5)
+		scatter = ax.scatter(expected, actual, c=frac_limited, alpha=0.5, cmap='hot', edgecolors='k')
+		fig.colorbar(scatter)
 		ax.set_xscale('symlog', linthreshx=min_prob)
 		ax.set_yscale('symlog', linthreshy=min_prob)
 
@@ -54,6 +68,8 @@ class Plot(cohortAnalysisPlot.CohortAnalysisPlot):
 		ax.spines['top'].set_visible(False)
 		ax.set_xlabel(f'Expected promoter bound probability')
 		ax.set_ylabel(f'Observed promoter bound probability')
+		ax.set_title('Promoter binding probabilities for each transcription factor\n'
+			'Color indicates fraction of time limited by the number of active TFs', fontsize=12)
 
 		plt.tight_layout()
 		exportFigure(plt, plotOutDir, plotOutFileName, metadata)
