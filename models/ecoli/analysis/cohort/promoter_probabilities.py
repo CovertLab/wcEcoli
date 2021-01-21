8bd95d2c354b2b701d3f6bb5e550f9e67804eddf
tahorst <thorst@stanford.edu>
2021-01-20 21:57:28 -0800

    Remove other probability plots

--- models/ecoli/analysis/cohort/promoter_probabilities.py
+++ models/ecoli/analysis/cohort/promoter_probabilities.py
@@ -1,5 +1,5 @@
 """
-Compare expected TF binding probabilities with observed probabilities.
+Compare expected promoter binding probabilities with observed probabilities.
 """
 
 import os
@@ -13,29 +13,12 @@ from wholecell.analysis.analysis_tools import exportFigure
 from wholecell.io.tablereader import TableReader
 
 
-def plot(ax, expected, actual, label):
-	min_prob = min(expected[expected > 0].min(), actual[actual > 0].min())
-	max_prob = np.max((expected, actual))
-
-	# Plot data
-	ax.loglog([min_prob, max_prob], [min_prob, max_prob], '--k')
-	ax.loglog(expected, actual, 'o', alpha=0.5)
-
-	# Axes formatting
-	ax.spines['right'].set_visible(False)
-	ax.spines['top'].set_visible(False)
-	ax.set_xlabel(f'Expected probability')
-	ax.set_ylabel(f'Observed {label} probability')
-
-
 class Plot(cohortAnalysisPlot.CohortAnalysisPlot):
 	def do_plot(self, variantDir, plotOutDir, plotOutFileName, simDataFile, validationDataFile, metadata):
 		ap = AnalysisPaths(variantDir, cohort_plot=True)
 
 		expected = []
 		bound = []
-		active_tfs = []
-		total_tfs = []
 		promoters = []
 		for sim_dir in ap.get_cells():
 			simOutDir = os.path.join(sim_dir, 'simOut')
@@ -46,26 +29,31 @@ class Plot(cohortAnalysisPlot.CohortAnalysisPlot):
 			# Load data
 			expected.append(rna_synth_reader.readColumn('pPromoterBound'))
 			bound.append(rna_synth_reader.readColumn('nActualBound'))
-			active_tfs.append(rna_synth_reader.readColumn('n_active_tfs'))
-			total_tfs.append(rna_synth_reader.readColumn('total_tf_counts'))
 			promoters.append(rna_synth_reader.readColumn('n_available_promoters'))
+
 		expected = np.vstack(expected).mean(0)
 		bound = np.vstack(bound)
-		active_tf_prob = (bound / np.vstack(active_tfs))
-		active_tf_prob[bound == 0] = 0
-		active_tf_prob = active_tf_prob.mean(0)
-		total_tf_prob = (bound / np.vstack(total_tfs))
-		total_tf_prob[bound == 0] = 0
-		total_tf_prob = total_tf_prob.mean(0)
 		promoter_prob = (bound / np.vstack(promoters))
 		promoter_prob[bound == 0] = 0
-		promoter_prob = promoter_prob.mean(0)
+		actual = promoter_prob.mean(0)
 
-		plt.figure(figsize=(5, 12))
+		min_prob = min(expected[expected > 0].min(), actual[actual > 0].min())
+		max_prob = np.max((expected, actual))
 
-		plot(plt.subplot(3, 1, 1), expected, active_tf_prob, 'active TF')
-		plot(plt.subplot(3, 1, 2), expected, total_tf_prob, 'total TF')
-		plot(plt.subplot(3, 1, 3), expected, promoter_prob, 'promoter')
+		plt.figure()
+		ax = plt.gca()
+
+		# Plot data
+		ax.plot([0, max_prob], [0, max_prob], '--k')
+		ax.plot(expected, actual, 'o', alpha=0.5)
+		ax.set_xscale('symlog', linthreshx=min_prob)
+		ax.set_yscale('symlog', linthreshy=min_prob)
+
+		# Axes formatting
+		ax.spines['right'].set_visible(False)
+		ax.spines['top'].set_visible(False)
+		ax.set_xlabel(f'Expected promoter bound probability')
+		ax.set_ylabel(f'Observed promoter bound probability')
 
 		plt.tight_layout()
 		exportFigure(plt, plotOutDir, plotOutFileName, metadata)
