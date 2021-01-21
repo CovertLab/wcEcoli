a788670a0bd67235845cd3425f51f3e7c17d0652
tahorst <thorst@stanford.edu>
2021-01-21 10:19:13 -0800

    Add TF labels for outliers

--- models/ecoli/analysis/cohort/promoter_probabilities.py
+++ models/ecoli/analysis/cohort/promoter_probabilities.py
@@ -3,6 +3,7 @@ Compare expected promoter binding probabilities with observed probabilities.
 """
 
 import os
+import pickle
 
 from matplotlib import pyplot as plt
 import numpy as np
@@ -15,6 +16,10 @@ from wholecell.io.tablereader import TableReader
 
 class Plot(cohortAnalysisPlot.CohortAnalysisPlot):
 	def do_plot(self, variantDir, plotOutDir, plotOutFileName, simDataFile, validationDataFile, metadata):
+		with open(simDataFile, 'rb') as f:
+			sim_data = pickle.load(f)
+		tf_to_gene_id = sim_data.process.transcription_regulation.tf_to_gene_id
+
 		ap = AnalysisPaths(variantDir, cohort_plot=True)
 
 		expected = []
@@ -32,10 +37,12 @@ class Plot(cohortAnalysisPlot.CohortAnalysisPlot):
 			bound.append(rna_synth_reader.readColumn('nActualBound'))
 			promoters.append(rna_synth_reader.readColumn('n_available_promoters'))
 
-			tf_ids = [f'{tf}[c]' for tf in rna_synth_reader.readAttribute('tf_ids')]
-			active_counts, = read_bulk_molecule_counts(simOutDir, (tf_ids,))
+			tf_ids = rna_synth_reader.readAttribute('tf_ids')
+			tf_ids_with_location = [f'{tf}[c]' for tf in tf_ids]
+			active_counts, = read_bulk_molecule_counts(simOutDir, (tf_ids_with_location,))
 			active_tfs.append(active_counts)
 
+		# Turn lists into array for easier processing
 		expected = np.vstack(expected).mean(0)
 		bound = np.vstack(bound)
 		active_tfs = np.vstack(active_tfs)
@@ -50,9 +57,13 @@ class Plot(cohortAnalysisPlot.CohortAnalysisPlot):
 		active_limited_timesteps = (active_tfs == 0) & (bound > 0)
 		frac_limited = active_limited_timesteps.sum(0) / active_limited_timesteps.shape[0]
 
+		# Gene labels for limited points
+		gene_ids = [tf_to_gene_id[tf] for tf in tf_ids]
+
 		min_prob = min(expected[expected > 0].min(), actual[actual > 0].min())
 		max_prob = np.max((expected, actual))
 
+		# Create figure
 		fig = plt.figure()
 		ax = plt.gca()
 
@@ -62,6 +73,14 @@ class Plot(cohortAnalysisPlot.CohortAnalysisPlot):
 		fig.colorbar(scatter)
 		ax.set_xscale('symlog', linthreshx=min_prob)
 		ax.set_yscale('symlog', linthreshy=min_prob)
+
+		# Add text labels
+		limited_cutoff = 0.05
+		for tf, frac, x, y in zip(gene_ids, frac_limited, expected, actual):
+			if frac < limited_cutoff:
+				continue
+			ax.text(x, 0, tf, ha='center', rotation=90, fontsize=6)
+			ax.text(0, y, tf, va='center', fontsize=6)
 
 		# Axes formatting
 		ax.spines['right'].set_visible(False)
