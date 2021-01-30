92a3b5e1f2733e293bce4f596f78143106f0e386
cs <cs.temporary@icloud.com>
2021-01-30 00:00:56 +0000

    Add antibiotics parameter scan

---
+++ colony/experiments/antibiotics_scan.py
@@ -0,0 +1,234 @@
+'''Simulate a colony of E. coli
+
+Each E. coli cell is modeled with wcEcoli. They are placed into a shared
+environment using Vivarium.
+'''
+
+import argparse
+import datetime
+import json
+import sys
+from typing import List, Tuple
+
+from matplotlib import pyplot as plt
+import numpy as np
+from vivarium.core.composition import (
+	agent_environment_experiment,
+	simulate_experiment,
+)
+from vivarium.core.experiment import timestamp, get_in
+from vivarium.library.units import units
+from vivarium_cell.composites.lattice import Lattice
+from vivarium_cell.processes.diffusion_field import make_gradient
+
+from colony.compartments.antibiotics_simple import (
+	ANTIBIOTIC_KEY,
+	INITIAL_EXTERNAL_ANTIBIOTIC,
+	SimpleAntibioticsCell,
+)
+from wholecell.utils import filepath as fp
+
+
+BOUNDS = (50, 50)
+N_BINS = (10, 10)
+NUM_EMISSIONS = 100
+AGENT_NAME = 'simple_antibiotic_cell'
+PUMP_CONCENTRATIONS = np.linspace(0, 0.000175, 10)
+BETA_LACTAMASE_CONCENTRATIONS = np.linspace(0, 0.0003, 10)
+ANTIBIOTIC_THRESHOLD = 0.02
+SIMULATION_TIME = 48 * 60
+PULSE_CONCENTRATION = 0.1239
+
+
+def get_antibiotics_timeline(
+	n_bins,  # type: Tuple[int, int]
+	size,  # type: Tuple[int, int]
+	pulses,  # type: List[Tuple[int, int, float]]
+	end_time,  # type: int
+):
+	'''Get a timeline for antibiotic pulses.
+
+	Arguments:
+		n_bins: Number of bins in x and y directions.
+		size: Size of environment in x and y directions.
+		pulses: List of tuples, each of which describes a pulse.
+			Each tuple has the form (start_time, duration,
+			concentration).
+		end_time: The length of the experiment
+	Returns:
+		list: A timeline that implements the described pulses.
+	'''
+	timeline = []
+	get_empty_field = lambda: make_gradient(
+		{
+			'type': 'uniform',
+			'molecules': {
+				ANTIBIOTIC_KEY: '0',
+			},
+		},
+		n_bins,
+		size,
+	)[ANTIBIOTIC_KEY]
+	for start_time, duration, concentration in pulses:
+		start_field = np.full_like(get_empty_field(), concentration)
+		timeline.append((
+			start_time,
+			{('fields', ANTIBIOTIC_KEY): start_field},
+		))
+		end_field = get_empty_field()
+		timeline.append((
+			start_time + duration,
+			{('fields', ANTIBIOTIC_KEY): end_field},
+		))
+	timeline.append((end_time, {}))
+	return timeline
+
+
+def simulate_one(
+	simulation_time, pulse_concentration, antibiotic_threshold,
+	pump_concentration, beta_lactamase_concentration,
+):
+	recipe = {ANTIBIOTIC_KEY: INITIAL_EXTERNAL_ANTIBIOTIC}
+
+	antibiotic_pulse = (
+		simulation_time * 0.5,
+		simulation_time * 0.5,
+		pulse_concentration,
+	)
+	timeline_config = {
+		'timeline': get_antibiotics_timeline(
+			N_BINS, BOUNDS, [antibiotic_pulse], simulation_time),
+	}
+	process_config = {
+		'_parallel': False,
+		'death': {
+			'detectors': {
+				'antibiotic': {
+					'antibiotic_threshold': antibiotic_threshold,
+				},
+			},
+		},
+		'antibiotic_transport': {
+			'initial_pump': pump_concentration,
+		},
+		'hydrolysis': {
+			'initial_catalyst': beta_lactamase_concentration,
+		},
+	}
+	agent_ids = [AGENT_NAME]
+
+	# These come from the start of a wcEcoli simulation with seed 1.
+	# Specifically, they come from experiment 20210125.180927 at time 2.
+	initial_agent_state = {
+		'boundary': {
+			'dry_mass': 351.4542634162847 * units.fg,
+			'mass': 1171.8377856780676 * units.fg,
+			'volume': 1.0653070778891522 * units.fL,
+			'width': 1,
+		},
+	}
+	settings = {
+		'emitter': {'type': 'timeseries'},
+		'emit_step': max(simulation_time // NUM_EMISSIONS, 1),
+		'timeline': timeline_config,
+		'experiment_id': timestamp(datetime.datetime.utcnow()),
+		'display_info': False,
+		'progress_bar': False,
+	}
+	agents_config = {
+		'type': SimpleAntibioticsCell,
+		'ids': agent_ids,
+		'config': process_config,
+	}
+	environment_config = {
+		'type': Lattice,
+		'config': {
+			'multibody': {
+				'bounds': BOUNDS,
+				'jitter_force': 1e-6,
+			},
+			'diffusion': {
+				'bounds': BOUNDS,
+				'n_bins': N_BINS,
+				'molecules': list(recipe.keys()),
+				'depth': 1000,  # Deep to avoid depleting local molecules
+				'diffusion': 5e-1,
+				'gradient': {
+					'type': 'linear',
+					'molecules': {
+						molecule: {
+							'center': (0, 0),
+							'base': concentration,
+							'slope': 0,
+						}
+						for molecule, concentration in recipe.items()
+					},
+				},
+			},
+		},
+	}
+	experiment = agent_environment_experiment(
+		agents_config=agents_config,
+		environment_config=environment_config,
+		initial_state={},
+		initial_agent_state=initial_agent_state,
+		settings=settings,
+	)
+	settings = {
+		'timestep': 1.0,
+		'return_raw_data': True,
+		'timeline': timeline_config,
+	}
+	raw_data = simulate_experiment(experiment, settings)
+	experiment.end()
+	return raw_data
+
+
+def scan(pump_concentrations, beta_lactamase_concentrations):
+	raw_datasets = []  # List of 3-tuples: (pump, beta lactamase, data)
+	for pump in pump_concentrations:
+		for beta_lactamase in beta_lactamase_concentrations:
+			raw_data = simulate_one(
+				simulation_time=SIMULATION_TIME,
+				pulse_concentration=PULSE_CONCENTRATION,
+				antibiotic_threshold=ANTIBIOTIC_THRESHOLD,
+				pump_concentration=pump,
+				beta_lactamase_concentration=beta_lactamase,
+			)
+			raw_datasets.append((pump, beta_lactamase, raw_data))
+	return raw_datasets
+
+
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument(
+		'-o', '--out',
+		type=str,
+		default='antibiotics_scan_data.json',
+		help='Output file for raw scan data.',
+	)
+	args = parser.parse_args()
+	results = scan(PUMP_CONCENTRATIONS, BETA_LACTAMASE_CONCENTRATIONS)
+	output = {
+		'metadata': {
+			'python': sys.version.splitlines()[0],
+			'git_hash': fp.run_cmdline('git rev-parse HEAD'),
+			'git_branch': fp.run_cmdline('git symbolic-ref --short HEAD'),
+			'git_status': fp.run_cmdline(
+                'git status --porcelain').split('\n'),
+            'time': datetime.datetime.utcnow().isoformat() + '+00:00',
+		},
+		'data': results,
+		'parameters': {
+			'pump_concentrations': PUMP_CONCENTRATIONS.tolist(),
+			'beta_lactamase_concentrations': (
+				BETA_LACTAMASE_CONCENTRATIONS.tolist()),
+            'agent_name': AGENT_NAME,
+		},
+	}
+	with open(args.out, 'w') as f:
+		json.dump(output, f, indent=4)
+
+
+if __name__ == '__main__':
+	main()
