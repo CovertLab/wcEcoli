a26f194298b22702848b3e444dfdc020eb1cbf5e
Jerry Morrison <1fish2@users.noreply.github.com>
2021-05-13 15:06:33 -0700

    revert from wcEcoli3-staging to wcEcoli3

--- runscripts/jenkins/setup-environment.sh
+++ runscripts/jenkins/setup-environment.sh
@@ -4,6 +4,6 @@ export PYTHONPATH=$PWD
 module load wcEcoli/python3
 
 ### Edit this line to make this branch use another pyenv like wcEcoli3-staging
-pyenv local wcEcoli3-staging
+pyenv local wcEcoli3
 
 make clean compile
