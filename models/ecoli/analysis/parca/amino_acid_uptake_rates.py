cd0e165a7fbd35d51868ff23da5b9b39fd3dc35c
tahorst <thorst@stanford.edu>
2021-05-26 14:28:08 -0400

    Add comparison to translation supply

--- models/ecoli/analysis/parca/amino_acid_uptake_rates.py
+++ models/ecoli/analysis/parca/amino_acid_uptake_rates.py
@@ -18,12 +18,39 @@ from wholecell.utils import units
 FLUX_UNITS = units.mmol / units.g / units.h
 
 
+def subplot(x, y, x_err, labels, y_label, title):
+	# Statistics
+	r, p = pearsonr(np.log10(x), np.log10(y))
+	n = len(labels)
+
+	## Plot data
+	min_rate = min(x.min(), y.min())
+	max_rate = max(x.max(), y.max())
+	plt.errorbar(x, y, xerr=x_err, fmt='o')
+	plt.plot([min_rate, max_rate], [min_rate, max_rate], '--k')
+
+	## Show point labels
+	for aa, x, y in zip(labels, x, y):
+		plt.text(x, 1.1 * y, aa, ha='center', fontsize=6)
+
+	## Format axes
+	plt.xlabel('Zampieri et al. max uptake flux\n(mmol/g DCW/hr)')
+	plt.ylabel(y_label)
+	plt.xscale('log')
+	plt.yscale('log')
+	plt.title(f'{title}\nr={r:.2f} (p={p:.2g}, n={n})', fontsize=8)
+
 class Plot(parcaAnalysisPlot.ParcaAnalysisPlot):
 	def do_plot(self, input_dir, plot_out_dir, plot_out_filename, sim_data_file, validation_data_file, metadata):
 		with open(sim_data_file, 'rb') as f:
 			sim_data = pickle.load(f)
 		with open(validation_data_file, 'rb') as f:
 			validation_data = pickle.load(f)
+
+		# Attributes from sim_data
+		wcm_aa_ids = sim_data.molecule_groups.amino_acids
+		specific_import_rates = sim_data.process.metabolism.specific_import_rates
+		aa_supply_rates = sim_data.translation_supply_rate['minimal_plus_amino_acids']
 
 		# Load validation data
 		aa_ids = []
@@ -40,38 +67,34 @@ class Plot(parcaAnalysisPlot.ParcaAnalysisPlot):
 
 		# Load WCM calculated data
 		wcm_rates = []
+		wcm_supply = []
 		wcm_data = {
 			aa[:-3]: rates
-			for aa, rates in zip(sim_data.molecule_groups.amino_acids, sim_data.process.metabolism.specific_import_rates)
+			for aa, rates in zip(wcm_aa_ids, specific_import_rates)
+			}
+		supply_data = {
+			aa[:-3]: rates
+			for aa, rates in zip(wcm_aa_ids, aa_supply_rates)
 			}
 		wcm_units = units.count / DRY_MASS_UNITS * K_CAT_UNITS  # TODO: store this in sim_data?
 		for aa in aa_ids:
 			wcm_rates.append((wcm_units * wcm_data[aa]).asNumber(FLUX_UNITS))
+			wcm_supply.append(supply_data[aa].asNumber(FLUX_UNITS))
 		wcm_rates = np.array(wcm_rates)
-
-		# Calculate statistics
-		r, p = pearsonr(np.log10(val_rates), np.log10(wcm_rates))
-		n = len(aa_ids)
+		wcm_supply = np.array(wcm_supply)
 
 		# Create plot
-		plt.figure()
+		plt.figure(figsize=(5, 10))
 
-		## Plot data
-		min_rate = min(val_rates.min(), wcm_rates.min())
-		max_rate = max(val_rates.max(), wcm_rates.max())
-		plt.errorbar(val_rates, wcm_rates, xerr=val_error, fmt='o')
-		plt.plot([min_rate, max_rate], [min_rate, max_rate], '--k')
+		plt.subplot(2, 1, 1)
+		subplot(val_rates, wcm_rates, val_error, aa_ids,
+			'WCM uptake flux\n(mmol/g DCW/hr)',
+			'Amino acid import rate validation')
 
-		## Show point labels
-		for aa, x, y in zip(aa_ids, val_rates, wcm_rates):
-			plt.text(x, 1.1*y, aa, ha='center', fontsize=6)
-
-		## Format axes
-		plt.xlabel('Zampieri et al. max uptake flux\n(mmol/g DCW/hr)')
-		plt.ylabel('WCM uptake flux\n(mmol/g DCW/hr)')
-		plt.xscale('log')
-		plt.yscale('log')
-		plt.title(f'Amino acid import rate validation\nr={r:.2f} (p={p:.3f}, n={n})', fontsize=8)
+		plt.subplot(2, 1, 2)
+		subplot(val_rates, wcm_supply, val_error, aa_ids,
+			'WCM translation supply in rich media\n(mmol/g DCW/hr)',
+			'Translation vs import comparison')
 
 		plt.tight_layout()
 		exportFigure(plt, plot_out_dir, plot_out_filename, metadata)
