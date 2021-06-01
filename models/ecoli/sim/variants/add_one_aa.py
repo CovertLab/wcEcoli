a2ffa204888fb30ec033a1617f52b98f7e03e993
tahorst <thorst@stanford.edu>
2021-06-01 12:09:31 -0400

    Cleanup remove_one_aa variant

--- models/ecoli/sim/variants/add_one_aa.py
+++ models/ecoli/sim/variants/add_one_aa.py
@@ -16,13 +16,15 @@ import numpy as np
 
 
 def add_one_aa(sim_data, index):
+	# Add one amino acid to the media
 	aa = sim_data.molecule_groups.amino_acids[index][:-3]
 	sim_data.external_state.saved_media['minimal'][aa] = np.inf
 
+	# Descriptions of variant
 	name = '{}_added'.format(aa)
 	desc = 'Add {} to minimal media.'.format(aa)
 
-	# Use index as a control because Sel needs to be in the media
+	# Use index as a control because Sel needs to be in the media so it is already at inf
 	if aa == 'L-SELENOCYSTEINE':
 		name = 'control'
 		desc = 'Minimal media control'
--- models/ecoli/sim/variants/remove_one_aa.py
+++ models/ecoli/sim/variants/remove_one_aa.py
@@ -2,6 +2,9 @@
 Remove one amino acid from the minimal media plus amino acids condition.
 
 Modifies:
+	sim_data.condition
+	sim_data.external_state.current_timeline_id
+	sim_data.external_state.saved_timelines
 	sim_data.external_state.saved_media
 
 Expected variant indices (dependent on order of sim_data.moleculeGroups.aaIDs):
@@ -13,17 +16,23 @@ TODO:
 """
 
 def remove_one_aa(sim_data, index):
-	condition_label = sim_data.ordered_conditions[2]
+	# Set condition to be minimal with amino acids
+	condition_label = 'with_aa'
 	sim_data.condition = condition_label
 	sim_data.external_state.current_timeline_id = condition_label
 	sim_data.external_state.saved_timelines[condition_label] = [
 		(0, sim_data.conditions[condition_label]["nutrients"])
 	]
-	aa = sim_data.molecule_groups.amino_acids[index][:-3]
-	if index != 19:
-		sim_data.external_state.saved_media['minimal_plus_amino_acids'][aa] = 0
 
-	return dict(
-		shortName = "{}_removed".format(aa),
-		desc = "Remove {} from rich media.".format(aa)
-		), sim_data
+	aa = sim_data.molecule_groups.amino_acids[index][:-3]
+	# Use index as a control because Sel needs to be in the media
+	if aa == 'L-SELENOCYSTEINE':
+		name = 'control'
+		desc = 'Minimal media with amino acids control'
+	# Remove one amino acid from the media
+	else:
+		sim_data.external_state.saved_media['minimal_plus_amino_acids'][aa] = 0
+		name = '{}_removed'.format(aa)
+		desc = 'Remove {} from rich media.'.format(aa)
+
+	return dict(shortName=name, desc=desc), sim_data
