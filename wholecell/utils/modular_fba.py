34f2cda2b412457fce89aea75478ec189a4f8eaf
tahorst <thorst@stanford.edu>
2021-06-17 12:21:37 -0400

    Add docstring for setExternalMoleculeLevels

--- wholecell/utils/modular_fba.py
+++ wholecell/utils/modular_fba.py
@@ -1064,6 +1064,19 @@ class FluxBalanceAnalysis(object):
 
 
 	def setExternalMoleculeLevels(self, levels, molecules=None, force=False):
+		"""
+		Sets levels of external molecules that the cell can access. A positive
+		level indicates molcules that are available for uptake while a negative
+		level indicates secretion.
+
+		Args:
+			levels: limits corresponding to each molecule or to all molecules if
+				a singleton is passed
+			molecules: the molecule IDs to assign limits to
+			force: if True, will force exchange by setting both the upper and
+				lower bounds to the levels provided
+		"""
+
 		if molecules is None:
 			molecules = self._externalMoleculeIDs
 		elif isinstance(molecules, str):
