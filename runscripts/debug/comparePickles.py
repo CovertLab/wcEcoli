76282d11399fc202634fe7173f926e4409a027b8
Jerry Morrison <1fish2@users.noreply.github.com>
2021-06-22 23:22:58 -0700

    comparePickles tool to compare (dirs of) pickle files
    Bug: Parca is creating different output every time it runs. (A problem using random numbers?)
    
    To help debug it, this tool compares any pair of pickle files or a pair of directories of pickle files -- such as the `--intermediates-directory` containing output from all the stages of `runParca`.
    
    It shows that the output has 9841 lines of differences in the very first stage, `sim_data_initialize.cPickle`, and the same number of differences carries through to the other stages.

---
+++ runscripts/debug/comparePickles.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+"""
+Compare all the .cPickle files in a pair of directories. Show any differences.
+
+Usage (DIR is a path like 'out/manual/intermediates'):
+	runscripts/debug/compareParca.py DIR1 DIR2
+"""
+
+import argparse
+import os
+import sys
+
+from runscripts.reflect.object_tree import diff_dirs, diff_files
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(
+		description="Compare two .cPickle files"
+					" or all the .cPickle files in two directories."
+					" Print a count and optionally a summary of the differences.")
+	parser.add_argument('-c', '--count', action='store_true',
+		help="Print just the diff line count for each file, skipping the"
+			 " detailed diff lines.")
+	parser.add_argument('path', metavar='PATH', nargs=2,
+		help="The two pickle files or directories to compare.")
+
+	args = parser.parse_args()
+	path1, path2 = args.path
+
+	if os.path.isfile(path1):
+		diff_count = diff_files(path1, path2, print_diff_lines=not args.count)
+	else:
+		diff_count = diff_dirs(path1, path2, print_diff_lines=not args.count)
+
+	sys.exit(3 if diff_count else 0)
