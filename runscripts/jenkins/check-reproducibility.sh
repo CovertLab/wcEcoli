092ef9e84a33ed67bb00a195f1efa6cd5f6d29f5
tahorst <thorst@stanford.edu>
2021-06-23 20:48:41 -0400

    Add comments

--- runscripts/jenkins/check-reproducibility.sh
+++ runscripts/jenkins/check-reproducibility.sh
@@ -1,5 +1,10 @@
 #! /usr/bin/env bash
 
+# Runs the parca and sims twice to compare output to ensure both
+# are reproducible from run to run. Will exit with an error code
+# if either the parca or sim runs produce different output from
+# each other.
+
 set -eu
 
 script_dir=$(dirname $(dirname $(realpath $0)))
@@ -9,8 +14,8 @@ dir2="check-reproducibility-2"
 sim_out_dir="wildtype_000000/000000/generation_000001/000000/simOut"
 
 # Run parca twice and check that output is consistent from run to run
-python $script_dir/manual/runParca.py -c4 $dir1 --save
-python $script_dir/manual/runParca.py -c4 $dir2 --save
+python $script_dir/manual/runParca.py -c4 $dir1 --save-intermediates
+python $script_dir/manual/runParca.py -c4 $dir2 --save-intermediates
 $script_dir/debug/comparePickles.py out/$dir1/kb out/$dir2/kb
 
 # Run entire simulation for each parca output
