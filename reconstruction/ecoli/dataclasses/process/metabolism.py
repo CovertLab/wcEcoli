d55e822af997c8c22bed2c47a50f4c72a818d98b
tahorst <thorst@stanford.edu>
2021-06-30 15:30:18 -0400

    Revert "Adjust Glt deg KM for positive import"
    This reverts commit 48668b81f39db3b16fd8714e0a6144397aaa5309.

--- reconstruction/ecoli/dataclasses/process/metabolism.py
+++ reconstruction/ecoli/dataclasses/process/metabolism.py
@@ -785,8 +785,6 @@ class Metabolism(object):
 			if amino_acid == 'MET[c]':
 				km_degradation *= 10
 				ki *= 5
-			if amino_acid == 'GLT[c]':
-				km_degradation *= 0.1
 
 			# Calculate kcat value to ensure sufficient supply to double
 			kcat = total_supply / (enzyme_counts * (1 / (1 + aa_conc / ki) * np.prod(1 / (1 + kms / km_conc)) - 1 / (1 + km_reverse / aa_conc) - 1 / (1 + km_degradation / aa_conc)))
