12403d779ed5d2118c427f52a3ee57781fd2c2bb
tahorst <thorst@stanford.edu>
2021-07-22 19:31:56 -0400

    Update pPromoterBound for combined conditions

--- reconstruction/ecoli/fit_sim_data_1.py
+++ reconstruction/ecoli/fit_sim_data_1.py
@@ -2615,6 +2615,22 @@ def fitPromoterBoundProbability(sim_data, cell_specs):
 				pInitI.append(H_col_name_to_index[col_name])
 				pInitV.append(1.)
 
+		for condition, tfs in sim_data.condition_active_tfs.items():
+			if condition == 'basal':
+				continue
+
+			for tf in tfs:
+				col_name = f'{tf}__{tf}__active'
+				pPromoterBoundIdxs[condition][tf] = H_col_name_to_index[col_name]
+
+		for condition, tfs in sim_data.condition_inactive_tfs.items():
+			if condition == 'basal':
+				continue
+
+			for tf in tfs:
+				col_name = f'{tf}__{tf}__inactive'
+				pPromoterBoundIdxs[condition][tf] = H_col_name_to_index[col_name]
+
 		# Build vector pInit and matrix H
 		pInit = np.zeros(len(set(pInitI)))
 		pInit[pInitI] = pInitV
