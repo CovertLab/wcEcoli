f84372aaa08870aab12ed8f637d227c94cf7ff96
tahorst <thorst@stanford.edu>
2021-07-28 15:23:32 -0400

    Sort order of amino acids

--- models/ecoli/analysis/variant/aa_uptake_sensitivity.py
+++ models/ecoli/analysis/variant/aa_uptake_sensitivity.py
@@ -16,7 +16,8 @@ from wholecell.analysis.analysis_tools import exportFigure, read_stacked_columns
 from wholecell.utils import units
 
 
-CONTROL_LABEL = 'L-SELENOCYSTEINE'  # control because SEL is already included for uptake in minimal media
+CONTROL_AA = 'L-SELENOCYSTEINE'  # control because SEL is already included for uptake in minimal media
+CONTROL_LABEL = 'Control'
 
 
 class Plot(variantAnalysisPlot.VariantAnalysisPlot):
@@ -31,7 +32,7 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 
 		aa_ids = sim_data.molecule_groups.amino_acids
 		aa_idx = {aa[:-3]: i for i, aa in enumerate(aa_ids)}
-		aa_labels = ['Control' if aa == CONTROL_LABEL + '[c]' else aa[:-3] for aa in aa_ids]
+		aa_labels = [CONTROL_LABEL if aa == CONTROL_AA + '[c]' else aa[:-3] for aa in aa_ids]
 
 		x_scatter = []
 		growth_rates = []
@@ -45,7 +46,7 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 				remove_first=True, ignore_exception=True).mean()
 			index = get_aa_index(variant)
 			aa_id = aa_ids[index][:-3]
-			if aa_id == CONTROL_LABEL:
+			if aa_id == CONTROL_AA:
 				control_rates.append(growth_rate)
 			factor = get_factor(variant)
 
@@ -65,13 +66,24 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 
 		# Load validation growth rates
 		val_control = validation_data.amino_acid_growth_rates['minimal']['mean']
-		val_aa_idx = []
+		val_aa = []
 		val_normalized_growth_rates = []
 		for media, rates in validation_data.amino_acid_growth_rates.items():
 			aa_id = media.split('_')[-1]
 			if aa_id in aa_idx:
-				val_aa_idx.append(aa_idx[aa_id])
+				val_aa.append(aa_id)
 				val_normalized_growth_rates.append(units.strip_empty_units(rates['mean'] / val_control))
+
+		# Sort order of amino acids by the validation growth rate expected
+		sorted_order = (
+				[CONTROL_AA]  # Control first
+				+ list(np.array(val_aa)[np.argsort(val_normalized_growth_rates)])  # Sorted validation growth rate
+				+ [aa for aa in aa_labels if aa not in val_aa and aa != CONTROL_LABEL]  # Amino acids without data
+		)
+		sorted_idx = {aa: i for i, aa in enumerate(sorted_order)}
+		val_aa_idx = [sorted_idx[aa] for aa in val_aa]
+		x_scatter = [sorted_idx[aa_ids[i][:-3]] for i in x_scatter]
+		sorted_order[0] = CONTROL_LABEL
 
 		# Plot data
 		plt.figure()
@@ -80,8 +92,10 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 		plt.colorbar(scatter)
 
 		# Plot formatting
-		plt.xticks(range(len(aa_labels)), aa_labels, rotation=45, fontsize=6, ha='right')
-		plt.ylabel('Growth rate\n(Normalized to minimal media)')
+		self.remove_border()
+		plt.xticks(range(len(sorted_order)), sorted_order, rotation=45, fontsize=6, ha='right')
+		plt.yticks(fontsize=6)
+		plt.ylabel('Growth rate\n(Normalized to minimal media)', fontsize=6)
 		plt.title('Effect of scaling uptake rates by a factor\n'
 			'Color represents uptake rate scale factor\n'
 			'Horizontal bars are expected growth rates', fontsize=6)
