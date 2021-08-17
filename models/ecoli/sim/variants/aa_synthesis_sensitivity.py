6f8b91b1f7aa7ccddccc0b630afef39c5d580a09
tahorst <thorst@stanford.edu>
2021-08-16 23:55:06 -0400

    Update params for variant

--- models/ecoli/sim/variants/aa_synthesis_sensitivity.py
+++ models/ecoli/sim/variants/aa_synthesis_sensitivity.py
@@ -5,10 +5,10 @@ Modifies:
 	TODO
 
 Expected variant indices (dependent on length of FACTORS and sim_data.molecule_groups.amino_acids):
-	0-7: range of values for first parameter, first amino acid, first media condition
-	0-39: range of parameters and values for first amino acid, first media condition
-	0-839: range of parameters and values over all amino acids for first media condition
-	0-1679: all changes
+	0-6: range of values for first parameter, first amino acid, first media condition
+	0-34: range of parameters and values for first amino acid, first media condition
+	0-734: range of parameters and values over all amino acids for first media condition
+	0-1469: all changes
 """
 
 from .add_one_aa import add_one_aa
@@ -16,7 +16,7 @@ from .add_one_aa import add_one_aa
 import numpy as np
 
 
-FACTORS = [0, 0.1, 0.5, 1, 1.5, 2, 5, 10]  # TODO: skip factor of 1 - only run once for each media condition
+FACTORS = [0, 0.1, 0.5, 1.5, 2, 5, 10]  # TODO: run factor of 1 once for each media condition
 PARAMETERS = ['aa_kcats', 'aa_kis', 'aa_upstream_kms', 'aa_reverse_kms', 'aa_degradation_kms']
 N_PARAM_VALUES = len(FACTORS) * len(PARAMETERS)
 MEDIA_IDS = [5, 19]  # Glt and control for now - TODO: run this for all AA additions?
@@ -27,7 +27,7 @@ def get_media_index(index, n_aas):
 
 def get_aa_index(index, n_aas):
 	sub_index = index % (N_PARAM_VALUES * n_aas)
-	return 5  #sub_index // N_PARAM_VALUES
+	return sub_index // N_PARAM_VALUES
 
 def get_adjustment(index):
 	sub_index = index % N_PARAM_VALUES
@@ -36,7 +36,7 @@ def get_adjustment(index):
 	return PARAMETERS[param_index], FACTORS[factor_index]
 
 def aa_synthesis_sensitivity(sim_data, index):
-	n_aas = 1  #len(sim_data.molecule_groups.amino_acids)
+	n_aas = len(sim_data.molecule_groups.amino_acids)
 
 	# Use add_one_aa variant to add a specific amino acid to the media
 	media_idx = get_media_index(index, n_aas)
