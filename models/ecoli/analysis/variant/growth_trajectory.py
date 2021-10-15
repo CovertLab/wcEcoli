6673d7b7fd5ed9d846564d6ad1c4d36afb72bec0
tahorst <thorst@stanford.edu>
2021-10-15 16:58:06 -0400

    Plot mass fraction growth rates

--- models/ecoli/analysis/variant/growth_trajectory.py
+++ models/ecoli/analysis/variant/growth_trajectory.py
@@ -12,7 +12,7 @@ from wholecell.analysis.analysis_tools import exportFigure, read_stacked_columns
 
 def plot(ax, x, y, xlabel=None, ylabel=None, label=None, background=False):
 	if background:
-		kwargs = {'alpha': 0.3, 'linewidth': 0.8, 'color': 'black'}
+		kwargs = {'alpha': 0.2, 'linewidth': 0.6, 'color': 'black'}
 	else:
 		kwargs = {'alpha': 0.7, 'label': label}
 		ax.plot(x[0], y[0], 'og', markersize=3)
@@ -31,21 +31,29 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 		variants = ap.get_variants()
 
 		# Create plot
-		_, axes = plt.subplots(3, 1, figsize=(4, 12))
+		_, axes = plt.subplots(3, 2, figsize=(8, 12))
 
+		growth_function = lambda x: np.diff(x, axis=0) / x[:-1]
 		for variant in variants:
 			all_mass_means = []
 			all_growth_means = []
 			all_ratio_means = []
 			all_ratio_ma = []
 			all_growth_ma = []
+			all_protein_growth_ma = []
+			all_rna_growth_ma = []
+			all_small_mol_growth_ma = []
 			for seed in ap.get_seeds(variant):
 				cell_paths = ap.get_cells(variant=[variant], seed=[seed])
 
 				# Load data
+				time_step = read_stacked_columns(cell_paths, 'Main', 'timeStepSec', remove_first=True).squeeze()
 				growth = read_stacked_columns(cell_paths, 'Mass', 'instantaneous_growth_rate', remove_first=True).squeeze()
 				protein = read_stacked_columns(cell_paths, 'Mass', 'proteinMass', remove_first=True).squeeze()
 				rna = read_stacked_columns(cell_paths, 'Mass', 'rnaMass', remove_first=True).squeeze()
+				protein_growth = read_stacked_columns(cell_paths, 'Mass', 'proteinMass', fun=growth_function).squeeze() / time_step
+				rna_growth = read_stacked_columns(cell_paths, 'Mass', 'rnaMass', fun=growth_function).squeeze() / time_step
+				small_mol_growth = read_stacked_columns(cell_paths, 'Mass', 'smallMoleculeMass', fun=growth_function).squeeze() / time_step
 				growth_means = read_stacked_columns(cell_paths, 'Mass', 'instantaneous_growth_rate', remove_first=True, fun=np.mean).squeeze()
 				protein_means = read_stacked_columns(cell_paths, 'Mass', 'proteinMass', remove_first=True, fun=np.mean).squeeze()
 				rna_means = read_stacked_columns(cell_paths, 'Mass', 'rnaMass', remove_first=True, fun=np.mean).squeeze()
@@ -56,10 +64,16 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 				ratio_means = rna_means / protein_means
 				growth_ma = np.convolve(growth, np.ones(200) / 200, mode='valid')
 				ratio_ma = np.convolve(ratio, np.ones(200) / 200, mode='valid')
+				protein_growth_ma = np.convolve(protein_growth, np.ones(200) / 200, mode='valid')
+				rna_growth_ma = np.convolve(rna_growth, np.ones(200) / 200, mode='valid')
+				small_mol_growth_ma = np.convolve(small_mol_growth, np.ones(200) / 200, mode='valid')
 
-				plot(axes[0], mass_means, growth_means, background=True)
-				plot(axes[1], ratio_means, growth_means, background=True)
-				plot(axes[2], ratio_ma, growth_ma, background=True)
+				plot(axes[0, 0], mass_means, growth_means, background=True)
+				plot(axes[1, 0], ratio_means, growth_means, background=True)
+				plot(axes[2, 0], ratio_ma, growth_ma, background=True)
+				plot(axes[0, 1], ratio_ma, protein_growth_ma, background=True)
+				plot(axes[1, 1], ratio_ma, rna_growth_ma, background=True)
+				plot(axes[2, 1], ratio_ma, small_mol_growth_ma, background=True)
 				# plot(axes[2], ratio, growth, background=True)  # Include this?  Should plot on new axes
 
 				all_mass_means.append(mass_means)
@@ -67,6 +81,9 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 				all_ratio_means.append(ratio_means)
 				all_ratio_ma.append(ratio_ma)
 				all_growth_ma.append(growth_ma)
+				all_protein_growth_ma.append(protein_growth_ma)
+				all_rna_growth_ma.append(rna_growth_ma)
+				all_small_mol_growth_ma.append(small_mol_growth_ma)
 
 			min_length = min([len(data) for data in all_growth_means])
 			stacked_mass_means = np.vstack([data[:min_length] for data in all_mass_means]).mean(0)
@@ -76,15 +93,24 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 			min_length_ma = min([len(data) for data in all_growth_ma])
 			stacked_ratio_ma = np.vstack([data[:min_length_ma] for data in all_ratio_ma]).mean(0)
 			stacked_growth_ma = np.vstack([data[:min_length_ma] for data in all_growth_ma]).mean(0)
+			stacked_protein_growth_ma = np.vstack([data[:min_length_ma] for data in all_protein_growth_ma]).mean(0)
+			stacked_rna_growth_ma = np.vstack([data[:min_length_ma] for data in all_rna_growth_ma]).mean(0)
+			stacked_small_mol_growth_ma = np.vstack([data[:min_length_ma] for data in all_small_mol_growth_ma]).mean(0)
 
-			plot(axes[0], stacked_mass_means, stacked_growth_means,
-				 xlabel='Cell cycle mass', ylabel='Cell cycle growth', label=variant)
-			plot(axes[1], stacked_ratio_means, stacked_growth_means,
-				 xlabel='Cell cycle RNA/protein', ylabel='Cell cycle growth', label=variant)
-			plot(axes[2], stacked_ratio_ma, stacked_growth_ma,
-				 xlabel='RNA/protein', ylabel='Growth', label=variant)
+			plot(axes[0, 0], stacked_mass_means, stacked_growth_means,
+				xlabel='Cell cycle mass', ylabel='Cell cycle growth', label=variant)
+			plot(axes[1, 0], stacked_ratio_means, stacked_growth_means,
+				xlabel='Cell cycle RNA/protein', ylabel='Cell cycle growth', label=variant)
+			plot(axes[2, 0], stacked_ratio_ma, stacked_growth_ma,
+				xlabel='RNA/protein', ylabel='Growth', label=variant)
+			plot(axes[0, 1], stacked_ratio_ma, stacked_protein_growth_ma,
+				xlabel='RNA/protein', ylabel='Protein growth', label=variant)
+			plot(axes[1, 1], stacked_ratio_ma, stacked_rna_growth_ma,
+				xlabel='RNA/protein', ylabel='RNA growth', label=variant)
+			plot(axes[2, 1], stacked_ratio_ma, stacked_small_mol_growth_ma,
+				xlabel='RNA/protein', ylabel='Small molecule growth', label=variant)
 
-		for ax in axes:
+		for ax in axes.reshape(-1):
 			self.remove_border(ax)
 			if len(variants) > 1:
 				ax.legend(fontsize=6)
