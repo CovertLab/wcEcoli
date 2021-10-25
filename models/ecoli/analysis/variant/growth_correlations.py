845a32fc5adeafa76286c8e1a30021aba1aacd5c
tahorst <thorst@stanford.edu>
2021-10-25 15:04:28 -0400

    Add legend

--- models/ecoli/analysis/variant/growth_correlations.py
+++ models/ecoli/analysis/variant/growth_correlations.py
@@ -27,7 +27,7 @@ MA_WINDOWS = np.array([10, 30, 60, 100, 300, 600, 1000, 3000]) + 1
 
 
 class Plot(variantAnalysisPlot.VariantAnalysisPlot):
-	def plot_bar(self, ax, cell_property, growth_rates, xlabel, tick_labels):
+	def plot_bar(self, ax, cell_property, growth_rates, xlabel, tick_labels, growth_labels=None):
 		n_subproperties = cell_property.shape[0]
 		n_growth_rates = len(growth_rates)
 		all_r = np.zeros((n_subproperties, n_growth_rates))
@@ -38,14 +38,17 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 		width = 0.8 / n_growth_rates
 		offsets = np.arange(n_growth_rates) * width - 0.4 + width / 2
 		x = np.arange(n_subproperties)
-		for r, offset in zip(all_r.T, offsets):
-			ax.bar(x + offset, r, width)
+		for i, (r, offset) in enumerate(zip(all_r.T, offsets)):
+			label = None if growth_labels is None else growth_labels[i]
+			ax.bar(x + offset, r, width, label=label)
 
 		self.remove_border(ax)
 		ax.tick_params(labelsize=6)
 		ax.set_xticks(x)
 		ax.set_xticklabels(tick_labels, fontsize=6, rotation=45, ha='right')
 		ax.set_xlabel(xlabel, fontsize=8)
+
+		return ax.get_legend_handles_labels()
 
 	def do_plot(self, inputDir, plotOutDir, plotOutFileName, simDataFile, validationDataFile, metadata):
 		with open(simDataFile, 'rb') as f:
@@ -58,7 +61,7 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 
 		# TODO: add plots for ppGpp, ribosomes, RNAP etc
 		scaling = 3
-		n_cols = 8
+		n_cols = 9
 		plt.figure(figsize=(scaling*n_cols, scaling*n_variants))
 		gs = gridspec.GridSpec(nrows=n_variants, ncols=n_cols)
 
@@ -72,6 +75,8 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 			ribosome_idx = unique_molecule_ids.index('active_ribosome')
 
 			# Load data
+			time_step = read_stacked_columns(cell_paths, 'Main', 'timeStepSec',
+				remove_first=True).squeeze()
 			sim_time = read_stacked_columns(cell_paths, 'Main', 'time',
 				remove_first=True).squeeze()
 			counts_to_molar = read_stacked_columns(cell_paths, 'EnzymeKinetics', 'countsToMolar',
@@ -105,13 +110,22 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 				np.convolve(growth, np.ones(window) / window, mode='valid')
 				for window in MA_WINDOWS
 				]
+			ma_labels = ['Current growth'] + [
+				f'{np.convolve(time_step, np.ones(window-1), mode="valid").mean():.0f} sec window'
+				for window in MA_WINDOWS
+				]
 
 			# Create subplots for this variant
 			combined_data = np.vstack((ppgpp_conc, active_ribosome_conc, active_rnap_conc, sim_time))
 			combined_labels = ['ppGpp', 'Ribosome', 'RNAP', 'Time']
 			ax = plt.subplot(gs[row, 0])
-			self.plot_bar(ax, combined_data, all_growth, 'Other conc', combined_labels)
+			handles, labels = self.plot_bar(ax, combined_data, all_growth,
+				'Other conc', combined_labels, growth_labels=ma_labels)
 			ax.set_ylabel('Growth correlation', fontsize=8)
+
+			ax = plt.subplot(gs[row, -1])
+			ax.axis('off')
+			ax.legend(handles, labels, loc='center', frameon=False, fontsize=8)
 
 			plot_data = [
 				(aa_conc, 'Amino acid conc'),
