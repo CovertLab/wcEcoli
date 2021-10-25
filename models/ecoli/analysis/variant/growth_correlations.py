cde52a213707d9493d4e46a3d963e3a59a40afa4
tahorst <thorst@stanford.edu>
2021-10-25 14:06:57 -0400

    Show correlation to multiple moving averages of the growth

--- models/ecoli/analysis/variant/growth_correlations.py
+++ models/ecoli/analysis/variant/growth_correlations.py
@@ -20,18 +20,24 @@ from models.ecoli.analysis.AnalysisPaths import AnalysisPaths
 from wholecell.analysis.analysis_tools import exportFigure, read_stacked_columns
 
 
-MA_WINDOW = 151  # 5 min moving average if timestep is 2 sec
+# +1 to make the window balanced on either side of the point of interest
+MA_WINDOWS = np.array([10, 30, 60, 100, 300, 600, 1000, 3000]) + 1
 
 
 class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 	def plot_bar(self, ax, cell_property, growth_rates, xlabel, tick_labels):
-		r = np.zeros((cell_property.shape[0], len(growth_rates)))
+		n_subproperties = cell_property.shape[0]
+		n_growth_rates = len(growth_rates)
+		all_r = np.zeros((n_subproperties, n_growth_rates))
 		for i, property in enumerate(cell_property):
 			for j, growth in enumerate(growth_rates):
-				r[i, j] = stats.pearsonr(property[:len(growth)], growth)[0]
+				all_r[i, j] = stats.pearsonr(property[:len(growth)], growth)[0]
 
-		x = np.arange(r.shape[0])
-		ax.bar(x, r[:, 0])
+		width = 0.8 / n_growth_rates
+		offsets = np.arange(n_growth_rates) * width - 0.4 + width / 2
+		x = np.arange(n_subproperties)
+		for r, offset in zip(all_r.T, offsets):
+			ax.bar(x + offset, r, width)
 
 		self.remove_border(ax)
 		ax.tick_params(labelsize=6)
@@ -44,15 +50,12 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 		with open(simDataFile, 'rb') as f:
 			sim_data = pickle.load(f)
 		aa_ids = [aa[:-3] for aa in sim_data.molecule_groups.amino_acids]
-		n_aas = len(aa_ids)
 
 		ap = AnalysisPaths(inputDir, variant_plot=True)
 		variants = ap.get_variants()
 		n_variants = len(variants)
 
-		n_bar = 1
-		n_others = 0  # TODO: add plots for ppGpp, ribosomes, RNAP etc
-		n_cols = n_aas + n_others + n_bar
+		# TODO: add plots for ppGpp, ribosomes, RNAP etc
 		scaling = 3
 		n_cols = 7
 		plt.figure(figsize=(scaling*n_cols, scaling*n_variants))
@@ -62,7 +65,6 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 			cell_paths = ap.get_cells(variant=[variant])
 
 			# Load data
-			# TODO: load AA, synthesis enzymes, synthetases and overlay on same plots?
 			counts_to_molar = read_stacked_columns(cell_paths, 'EnzymeKinetics', 'countsToMolar',
 				remove_first=True).squeeze()
 			growth = read_stacked_columns(cell_paths, 'Mass', 'instantaneous_growth_rate',
@@ -80,11 +82,16 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 			aa_supply_enzymes_rev = read_stacked_columns(cell_paths, 'GrowthLimits', 'aa_supply_enzymes_rev',
 				remove_first=True).T * counts_to_molar
 
-			growth_ma = np.convolve(growth, np.ones(MA_WINDOW) / MA_WINDOW, mode='valid')
+			# Derived values
 			fraction_charged = charged_trna / (uncharged_trna + charged_trna)
 
-			all_growth = [growth, growth_ma]
+			# Apply moving average to growth to calculate correlation between current value and future growth
+			all_growth = [growth] + [
+				np.convolve(growth, np.ones(window) / window, mode='valid')
+				for window in MA_WINDOWS
+				]
 
+			# Create subplots for this variant
 			plot_data = [
 				(aa_conc, 'Amino acid conc'),
 				(uncharged_trna, 'Uncharged tRNA conc'),
