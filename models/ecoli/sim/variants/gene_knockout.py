299d0bb4f139cd00cf99eab1d7ac7b0f3430a0e4
tahorst <thorst@stanford.edu>
2021-11-16 13:00:56 -0500

    Update gene_knockout variant for ppGpp

--- models/ecoli/sim/variants/gene_knockout.py
+++ models/ecoli/sim/variants/gene_knockout.py
@@ -2,28 +2,32 @@
 Knockout expression of a gene
 
 Modifies:
-	sim_data.process.transcription.rnaSynthProb
-	sim_data.process.transcription.rnaExpression
-	sim_data.process.transcription_regulation.recruitmentData
+	sim_data.process.transcription.rna_synth_prob
+	sim_data.process.transcription.rna_expression
+	sim_data.process.transcription.exp_free
+	sim_data.process.transcription.exp_ppgpp
+	sim_data.process.transcription_regulation.basal_prob
+	sim_data.process.transcription_regulation.delta_prob
 
-Expected variant indices (depends on length of sim_data.process.transcription.rnaData):
+Expected variant indices (depends on length of sim_data.process.transcription.rna_data):
 	0: control
-	1-4558: gene index to knockout
+	1-4692: gene index to knockout
 """
 
-from __future__ import absolute_import, division, print_function
-
 import numpy as np
+
 
 CONTROL_OUTPUT = dict(
 	shortName = "control",
 	desc = "Control simulation"
 	)
 
-def gene_knockout(sim_data, index):
-	# Knocks-out genes in order
 
-	nGenes = sim_data.process.transcription.rna_data.fullArray().size
+def gene_knockout(sim_data, index):
+	transcription = sim_data.process.transcription
+	transcription_regulation = sim_data.process.transcription_regulation
+
+	nGenes = len(transcription.rna_data)
 	nConditions = nGenes + 1
 
 	if index % nConditions == 0:
@@ -33,21 +37,25 @@ def gene_knockout(sim_data, index):
 
 	factor = 0  # Knockout expression
 	recruitment_mask = np.array([i == geneIndex
-		for i in sim_data.process.transcription_regulation.delta_prob['deltaI']])
-	for synth_prob in sim_data.process.transcription.rna_synth_prob.values():
+		for i in transcription_regulation.delta_prob['deltaI']])
+	for synth_prob in transcription.rna_synth_prob.values():
 		synth_prob[geneIndex] *= factor
-	for exp in sim_data.process.transcription.rna_expression.values():
+	for exp in transcription.rna_expression.values():
 		exp[geneIndex] *= factor
-	sim_data.process.transcription_regulation.basal_prob[geneIndex] *= factor
-	sim_data.process.transcription_regulation.delta_prob['deltaV'][recruitment_mask] *= factor
+	transcription.exp_free[geneIndex] *= factor
+	transcription.exp_ppgpp[geneIndex] *= factor
+	transcription_regulation.basal_prob[geneIndex] *= factor
+	transcription_regulation.delta_prob['deltaV'][recruitment_mask] *= factor
 
 	# Renormalize parameters
-	for synth_prob in sim_data.process.transcription.rna_synth_prob.values():
+	for synth_prob in transcription.rna_synth_prob.values():
 		synth_prob /= synth_prob.sum()
-	for exp in sim_data.process.transcription.rna_expression.values():
+	for exp in transcription.rna_expression.values():
 		exp /= exp.sum()
+	transcription.exp_free /= transcription.exp_free.sum()
+	transcription.exp_ppgpp /= transcription.exp_ppgpp.sum()
 
-	geneID = sim_data.process.transcription.rna_data["id"][geneIndex]
+	geneID = transcription.rna_data["id"][geneIndex]
 
 	return dict(
 		shortName = "{}_KO".format(geneID),
