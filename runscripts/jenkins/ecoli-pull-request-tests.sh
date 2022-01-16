2f95cb7fb690d73a19bc72bbb5d72d742808d450
Jerry Morrison <1fish2@users.noreply.github.com>
2022-01-15 21:50:05 -0800

    experiment with ecoli-pull-request-tests.sh
    ... which seems able to run pyenv shims.

--- runscripts/jenkins/ecoli-pull-request-tests.sh
+++ runscripts/jenkins/ecoli-pull-request-tests.sh
@@ -6,4 +6,13 @@ set -e
 ### merging the PR into master to prevent changing it for other Jenkins builds.
 ### ---------------------------------------------------------------------------
 source runscripts/jenkins/setup-environment.sh
+
+echo pyenv version: $(pyenv version)
+echo pyenv root: $(pyenv root)
+
 pytest --cov=wholecell --cov-report xml --junitxml=unittests.xml
+
+# Debugging experiment:
+echo mypy: $(which mypy)
+mypy -V
+mypy
