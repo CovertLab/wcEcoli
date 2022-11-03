47fe4a9565115f4edb5e7edf77b9eb5d62bc5e84
alberthqzhang <ahzhang@stanford.edu>
2022-11-02 20:32:10 -0700

    Format and finalize current plotted variables

--- models/ecoli/analysis/single/rRNA_operon_expression.py
+++ models/ecoli/analysis/single/rRNA_operon_expression.py
@@ -14,9 +14,15 @@ from wholecell.io.tablereader import TableReader
 from wholecell.analysis.analysis_tools import exportFigure, read_bulk_molecule_counts
 from models.ecoli.analysis import singleAnalysisPlot
 
-MA_WINDOWS = [200, 400] # timestep to plot timeseries, in timesteps
+MA_WINDOWS = [200, 400] # moving average window to plot timeseries, in timesteps
 COLORS = ['r', 'b']
+LABELPAD = 10
+FONTSIZE = 15
+LEGEND_FONT = 10
+
 class Plot(singleAnalysisPlot.SingleAnalysisPlot):
+	# TODO: add listener that tracks partial ribosome formation, in order to get
+	#  how many RNAPs are on rRNAs at this time point
 	def make_plot(self, axs, time, var, titles, ma, ylim=None):
 		if ma:
 			var = self.moving_average(var)
@@ -24,30 +30,34 @@ class Plot(singleAnalysisPlot.SingleAnalysisPlot):
 			for (i, mv_avg) in enumerate(var):
 				if len(np.shape(mv_avg)) > 1:
 					for (j, subvar) in enumerate(mv_avg):
-						axs[j].plot(time[i], subvar, c=COLORS[i])
+						axs[j].plot(time[i], subvar, c=COLORS[i], label="Mov avg window %f timesteps" % MA_WINDOWS[i])
 						if i==0 and titles:
-							axs[j].set_title(titles[j])
+							axs[j].set_title(titles[j], pad=LABELPAD, fontsize=FONTSIZE)
 						if ylim:
 							axs[j].set_ylim(ylim)
+						if i == len(MA_WINDOWS) - 1:
+							axs[j].legend(fontsize=LEGEND_FONT)
 				else:
-					axs.plot(time[i], mv_avg, c=COLORS[i])
+					axs.plot(time[i], mv_avg, c=COLORS[i], label="Mov avg window %f timesteps" % MA_WINDOWS[i])
 					if titles:
-						axs.set_title(titles)
+						axs.set_title(titles, pad=LABELPAD, fontsize=FONTSIZE)
 					if ylim:
 						axs.set_ylim(ylim)
+					if i == len(MA_WINDOWS) - 1:
+						axs.legend(fontsize=LEGEND_FONT)
 		else:
 			if len(np.shape(var)) > 1:
 				var = var.T
 				for (j, subvar) in enumerate(var):
 					axs[j].plot(time, subvar, c=COLORS[0])
 					if titles:
-						axs[j].set_title(titles[j])
+						axs[j].set_title(titles[j], pad=LABELPAD, fontsize=FONTSIZE)
 					if ylim:
 						axs.set_ylim(ylim)
 			else:
 				axs.plot(time, var, c=COLORS[0])
 				if titles:
-					axs.set_title(titles)
+					axs.set_title(titles, pad=LABELPAD, fontsize=FONTSIZE)
 				if ylim:
 					axs.set_ylim(ylim)
 
@@ -69,6 +79,7 @@ class Plot(singleAnalysisPlot.SingleAnalysisPlot):
 		sim_data = pickle.load(open(simDataFile, "rb"))
 		transcription = sim_data.process.transcription
 		rRNA_ids = transcription.rna_data['id'][transcription.rna_data['is_rRNA']]
+		num_rRNAs = len(rRNA_ids)
 
 		# Time
 		main_reader = TableReader(os.path.join(simOutDir, "Main"))
@@ -78,9 +89,9 @@ class Plot(singleAnalysisPlot.SingleAnalysisPlot):
 		# Transcription initiation events
 		rnap_data = TableReader(os.path.join(simOutDir, "RnapData"))
 		rna_init_event = rnap_data.readColumn("rnaInitEvent")
-		rna_ids = rnap_data.readAttribute("rnaIds")
-		is_rRNA = [rna_id in rRNA_ids for rna_id in rna_ids]
-		rRNA_init_events = rna_init_event[:, is_rRNA]
+		rna_ids = np.array(rnap_data.readAttribute("rnaIds"))
+		rRNA_idxs = [np.where(rna_ids == rRNA_id)[0][0] for rRNA_id in rRNA_ids]
+		rRNA_init_events = rna_init_event[:, rRNA_idxs]
 
 		ribosome_data = TableReader(os.path.join(simOutDir, "RibosomeData"))
 		total_rRNA_initiated = ribosome_data.readColumn("total_rRNA_initiated", squeeze=True)
@@ -126,43 +137,45 @@ class Plot(singleAnalysisPlot.SingleAnalysisPlot):
 		total_30S_counts = ribosome_30S_counts + active_ribosome
 		total_50S_counts = ribosome_50S_counts + active_ribosome
 
-		#mRNA_counts = TableReader(os.path.join(simOutDir, "mRnaCounts"))
-		#partial_mRNA_counts = mRNA_counts.readColumn("partial_mRNA_counts")
-		#mRNA_ids = mRNA_counts.readAttribute("mRNA_ids")
-
 		## Make figure
-		fig, axs = plt.subplots(nrows=4, ncols=11, figsize=(55, 20))
+		# Ncols is number of rRNA TUs, 1 for total, and 3 for 5S/16S/23S
+		ncols = num_rRNAs + 1 + 3
+		nrows=4
+
+		fig, axs = plt.subplots(nrows=nrows, ncols=ncols, figsize=(5 * ncols, 5 * nrows))
 		# Plot operon-level rRNA initiation events
-		axs[0, 0].set_ylabel("Initiations per second")
-		self.make_plot(axs=[axs[0, i] for i in range(7)], time=time, var=rRNA_init_events,
+		axs[0, 0].set_ylabel("Initiations per second", labelpad=LABELPAD)
+		self.make_plot(axs=[axs[0, i] for i in range(num_rRNAs)], time=time, var=rRNA_init_events,
 					   titles=[rRNA for rRNA in rRNA_ids], ma=True, ylim=[0, 2])
-		self.make_plot(axs=axs[0, 7], time=time, var=total_rRNA_initiated,
+		self.make_plot(axs=axs[0, num_rRNAs], time=time, var=total_rRNA_initiated,
 					   titles="Total rRNAs", ma=True, ylim=[4, 8])
+		for i in range(ncols):
+			axs[0, i].set_xlabel("Time (min)", labelpad=LABELPAD, fontsize=FONTSIZE)
 
 		# Plot gene dosage of each operon
-		axs[1, 0].set_ylabel("Gene dosage")
-		self.make_plot(axs=[axs[1, i] for i in range(7)], time=time, var=rRNA_gene_dosage,
+		axs[1, 0].set_ylabel("Gene dosage", labelpad=LABELPAD, fontsize=FONTSIZE)
+		self.make_plot(axs=[axs[1, i] for i in range(num_rRNAs)], time=time, var=rRNA_gene_dosage,
 					   titles=None, ma=False)
-		self.make_plot(axs=axs[1, 7], time=time, var=total_rRNA_gene_dosage,
+		self.make_plot(axs=axs[1, num_rRNAs], time=time, var=total_rRNA_gene_dosage,
 					   titles=None, ma=False)
 
 		# Plot the rRNA initiation events per gene dosage
-		axs[2, 0].set_ylabel("Initiations per second per gene dosage")
-		self.make_plot(axs=[axs[2,i] for i in range(7)], time=time, var=rRNA_initiation_per_gene_dosage,
+		axs[2, 0].set_ylabel("Initiations per second per gene dosage", labelpad=LABELPAD, fontsize=FONTSIZE)
+		self.make_plot(axs=[axs[2,i] for i in range(num_rRNAs)], time=time, var=rRNA_initiation_per_gene_dosage,
 					   titles=None, ma=True, ylim=[0, 1])
-		self.make_plot(axs=axs[2, 7], time=time, var=total_rRNA_initiation_per_gene_dosage,
+		self.make_plot(axs=axs[2, num_rRNAs], time=time, var=total_rRNA_initiation_per_gene_dosage,
 					   titles=None, ma=True, ylim=[0, 1])
 
 		# Plot effective initiation events of 5S, 16S, and 23S rRNA
-		self.make_plot(axs=axs[0, 8], time=time, var=rRNA5S_initiated,
+		self.make_plot(axs=axs[0, num_rRNAs+1], time=time, var=rRNA5S_initiated,
 					   titles="5S rRNA inits", ma=True, ylim=[4, 8])
-		self.make_plot(axs=axs[0, 9], time=time, var=rRNA16S_initiated,
+		self.make_plot(axs=axs[0, num_rRNAs+2], time=time, var=rRNA16S_initiated,
 					   titles="16S rRNA inits", ma=True, ylim=[4, 8])
-		self.make_plot(axs=axs[0, 10], time=time, var=rRNA23S_initiated,
+		self.make_plot(axs=axs[0, num_rRNAs+3], time=time, var=rRNA23S_initiated,
 					   titles="23S rRNA inits", ma=True, ylim=[4, 8])
 
 		# Plot the amount of (active or inactive) ribosomal subunits
-		axs[3, 0].set_ylabel("Counts")
+		axs[3, 0].set_ylabel("Counts", labelpad=LABELPAD, fontsize=FONTSIZE)
 		self.make_plot(axs=axs[3, 0], time=time, var=total_30S_counts,
 					   titles="Total 30S", ma=True)
 		self.make_plot(axs=axs[3, 1], time=time, var=total_50S_counts,
@@ -176,34 +189,9 @@ class Plot(singleAnalysisPlot.SingleAnalysisPlot):
 		self.make_plot(axs=axs[3, 4], time=time, var=rRNA_23S_counts,
 					   titles="Free 23S rRNA", ma=True)
 
-		# Plot the amount of
-
-
-		# Plot the absolute number of RNAPs on each ribosomal operon,
-		# and the absolute number of RNAPs on total ribosomal operons.
-		# Make listener that's the number of RNAPs on each gene. And you
-		# divide by the total to get fractions. And get inactive RNAPs from
-		# bulkMoleculeCounts, then u get the fraction of total RNAPs.
-
-
-		# Plot 1: the seven operons, time series of how much transcr initiation, also total
-		# Plot 2: the seven operons, time series of how much transcr initiation per gene dosage
-		# Plot 3: just gene dosage
-		# Plot 4: time series of how much 5S, 23S, 16S made
-		# Plot 5: time series of ribosome subunit counts (accumulating)
-		# Plot 6: time series of 5S, 23S, 16S rRNA at each time step (how many there r)
-		# we need: transcr init at each time step (possible w gene correction),
-		# how much 5S, etc. r made at each time step,
-		# Absolute number of RNAPs on ribosomal genes
-
-		# Ribosome protein limiting vs rrna limiting
-		# each type of rRNA accumulation
-		# subunit counts of each ribosome
-
 		plt.tight_layout()
 		exportFigure(plt, plotOutDir, plotOutFileName, metadata)
 		plt.close("all")
-		import ipdb; ipdb.set_trace()
 
 if __name__ == "__main__":
 	Plot().cli()
