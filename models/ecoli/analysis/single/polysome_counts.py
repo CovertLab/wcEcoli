9fccda348c0a3521c3b4756b063f532b4163ee95
Heena <hsaqibo1@stanford.edu>
2022-11-10 15:59:17 -0800

    merge updated polysome_mw.py into polysome_counts.py

--- models/ecoli/analysis/single/polysome_counts.py
+++ models/ecoli/analysis/single/polysome_counts.py
@@ -9,23 +9,38 @@ import os
 
 from matplotlib import pyplot as plt
 import numpy as np
+import pickle
 
 from wholecell.io.tablereader import TableReader
+from wholecell.utils import units
 from wholecell.analysis.analysis_tools import exportFigure
 from models.ecoli.analysis import singleAnalysisPlot
 
+BIN_WIDTH = 2E-3 # Bin width to the polysome mw histogram in fg
 
 class Plot(singleAnalysisPlot.SingleAnalysisPlot):
     def do_plot(self, simOutDir, plotOutDir, plotOutFileName, simDataFile, validationDataFile, metadata):
+        with open(simDataFile, 'rb') as f:
+            sim_data = pickle.load(f)
 
         # Listeners used
         ribosome_reader = TableReader(os.path.join(simOutDir, 'RibosomeData'))
 
         # Load data
+        mRNA_TU_index = ribosome_reader.readColumn('mRNA_TU_index')
         n_ribosome_on_each_mRNA = ribosome_reader.readColumn('n_ribosomes_on_each_mRNA')
 
 
         # Initialize variables
+        rna_mw = (sim_data.process.transcription.rna_data['mw']/sim_data.constants.n_avogadro).asNumber(units.fg)
+        ribosome_30S_mass = sim_data.getter.get_mass(sim_data.molecule_ids.s30_full_complex)
+        ribosome_50S_mass = sim_data.getter.get_mass(sim_data.molecule_ids.s50_full_complex)
+        ribosome_mass = ((ribosome_30S_mass + ribosome_50S_mass)/sim_data.constants.n_avogadro).asNumber(units.fg)
+
+        mRNA_TU_index_flattened = mRNA_TU_index.ravel()
+        mRNA_TU_index_flattened_clean = mRNA_TU_index_flattened[
+                                        np.logical_not(np.isnan(mRNA_TU_index_flattened))].astype(int)
+
         n_ribosome_on_each_mRNA_flattened = n_ribosome_on_each_mRNA.ravel()
         n_ribosome_on_each_mRNA_flattened_clean = n_ribosome_on_each_mRNA_flattened[
                                                   np.logical_not(np.isnan(n_ribosome_on_each_mRNA_flattened))].astype(int)
@@ -36,15 +51,32 @@ class Plot(singleAnalysisPlot.SingleAnalysisPlot):
         bincount_ribosomes_all_time_step = np.bincount(n_ribosome_on_each_mRNA_flattened_clean)
         average_polysome_count = bincount_ribosomes_all_time_step/n_ribosome_on_each_mRNA.shape[0]
 
+        # Calculate polysomes mw at all times steps and take average
+        polysome_mw_all_time_step = rna_mw[
+                                        mRNA_TU_index_flattened_clean] + ribosome_mass * n_ribosome_on_each_mRNA_flattened_clean
+        polysome_mw_hist, bin_edges = np.histogram(polysome_mw_all_time_step,
+                                                   bins=np.arange(min(polysome_mw_all_time_step),
+                                                                  max(polysome_mw_all_time_step) + BIN_WIDTH,
+                                                                  BIN_WIDTH))
+        average_polysome_mw = polysome_mw_hist / mRNA_TU_index.shape[0]
 
         fig = plt.figure(figsize = (8, 10))
+        # Plot polysome count
+        polysome_count_ax = plt.subplot(2,1,1)
+        polysome_count_ax.hist(np.arange(highest_ribosome_count+1), # add 1 to include the highest ribosome count
+                               weights = average_polysome_count,
+                               bins = highest_ribosome_count,
+                               align = 'left')
+        polysome_count_ax.set_xlabel("Count of Ribosomes Attached to Individual mRNA")
+        polysome_count_ax.set_ylabel("Count of mRNA")
 
-        plt.hist(np.arange(highest_ribosome_count+1), # add 1 to include the highest ribosome count
-                 weights = average_polysome_count,
-                 bins = highest_ribosome_count,
-                 align = 'left')
-        plt.xlabel("Count of Ribosomes Attached to Individual mRNA")
-        plt.ylabel("Count of mRNA")
+        # Plot polysome mw
+        polysome_mw_ax = plt.subplot(2, 1, 2)
+        polysome_mw_ax.hist(np.linspace(min(polysome_mw_all_time_step), max(polysome_mw_all_time_step), len(average_polysome_mw)),
+                            weights=average_polysome_mw,
+                            bins=bin_edges)
+        polysome_mw_ax.set_xlabel("Polysome Molecular Weight [fg]")
+        polysome_mw_ax.set_ylabel("Count of polysome")
 
         fig.tight_layout()
 
