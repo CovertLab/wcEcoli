d34a1dab7f764caab51800c0608f638b3aebc1ad
rjuenemann13 <rjuene@stanford.edu>
2022-11-11 14:03:05 -0800

    more generalizable new gene single analysis script

---
+++ models/ecoli/analysis/multigen/newGeneCounts.py
@@ -0,0 +1,55 @@
+"""
+Template for multigen analysis plots
+"""
+
+import pickle
+import os
+
+from matplotlib import pyplot as plt
+# noinspection PyUnresolvedReferences
+import numpy as np
+
+from models.ecoli.analysis import multigenAnalysisPlot
+from wholecell.analysis.analysis_tools import (exportFigure,
+	read_bulk_molecule_counts, read_stacked_bulk_molecules, read_stacked_columns)
+from wholecell.io.tablereader import TableReader
+
+
+class Plot(multigenAnalysisPlot.MultigenAnalysisPlot):
+	def do_plot(self, seedOutDir, plotOutDir, plotOutFileName, simDataFile, validationDataFile, metadata):
+		with open(simDataFile, 'rb') as f:
+			sim_data = pickle.load(f)
+		with open(validationDataFile, 'rb') as f:
+			validation_data = pickle.load(f)
+
+		cell_paths = self.ap.get_cells()
+
+		# Load data
+		## Simple stacking functions for data from all cells
+		names = ['ATP[c]']  # Replace with desired list of names
+		time = read_stacked_columns(cell_paths, 'Main', 'time')
+		(counts,) = read_stacked_bulk_molecules(cell_paths, (names,))
+
+		## Or iterate on each cell if additional processing is needed
+		for sim_dir in cell_paths:
+			simOutDir = os.path.join(sim_dir, 'simOut')
+
+			# Listeners used
+			main_reader = TableReader(os.path.join(simOutDir, 'Main'))
+
+			# Load data
+			time = main_reader.readColumn('time')
+
+			(counts,) = read_bulk_molecule_counts(simOutDir, (names,))
+
+		plt.figure()
+
+		### Create Plot ###
+
+		plt.tight_layout()
+		exportFigure(plt, plotOutDir, plotOutFileName, metadata)
+		plt.close('all')
+
+
+if __name__ == '__main__':
+	Plot().cli()
--- models/ecoli/analysis/single/newGeneCounts.py
+++ models/ecoli/analysis/single/newGeneCounts.py
@@ -12,29 +12,31 @@ from matplotlib import pyplot as plt
 from wholecell.io.tablereader import TableReader
 from wholecell.analysis.analysis_tools import exportFigure, read_bulk_molecule_counts
 from models.ecoli.analysis import singleAnalysisPlot
-from six.moves import range
+from six.moves import cPickle,range
 
 
 class Plot(singleAnalysisPlot.SingleAnalysisPlot):
 	def do_plot(self, simOutDir, plotOutDir, plotOutFileName, simDataFile, validationDataFile, metadata):
-		### TODO: CHECK THAT THE NEW GENES VARIANT WAS ON FOR THIS SIM
-
 		# Extract mRNA counts for each new gene
 		mRNA_counts_reader = TableReader(os.path.join(simOutDir, 'mRNACounts'))
 		mRNA_counts = mRNA_counts_reader.readColumn('mRNA_counts')
 		mRNA_idx = {rna: i for i, rna in enumerate(mRNA_counts_reader.readAttribute('mRNA_ids'))}
-		### TODO: FIND THESE NEW GENE IDS USING NAMING CONVENTION NGXXX
-		new_gene_mRNA_IDS = ['NG001_RNA[c]']
-		new_gene_mrna_indexes = np.array([mRNA_idx[newGeneId] for newGeneId in new_gene_mRNA_IDS], int)
+
+		new_gene_mRNA_IDS  = [k for k,v in mRNA_idx.items() if k.startswith('NG')]
+		new_gene_mrna_indexes = [v for k, v in mRNA_idx.items() if k.startswith('NG')]
+
+		assert len(new_gene_mRNA_IDS) != 0, 'no new gene mRNAs found'
 		new_gene_mrna_counts = mRNA_counts[:, new_gene_mrna_indexes]
 
 		# Extract protein counts for each new gene
 		monomer_counts_reader = TableReader(os.path.join(simOutDir, "MonomerCounts"))
 		monomer_counts = monomer_counts_reader.readColumn('monomerCounts')
 		monomer_idx = {monomer: i for i, monomer in enumerate(monomer_counts_reader.readAttribute('monomerIds'))}
-		### TODO: FIND THESE NEW GENE IDS USING NAMING CONVENTION NGXXX
-		new_gene_monomer_IDS = ['GFP-MONOMER[m]']
-		new_gene_monomer_indexes = np.array([monomer_idx[newGeneId] for newGeneId in new_gene_monomer_IDS], int)
+
+		new_gene_monomer_IDS = [k for k, v in monomer_idx.items() if k.startswith('NG')]
+		new_gene_monomer_indexes = [v for k, v in monomer_idx.items() if k.startswith('NG')]
+
+		assert len(new_gene_monomer_IDS) != 0, 'no new gene proteins found'
 		new_gene_monomer_counts = monomer_counts[:, new_gene_monomer_indexes]
 
 		main_reader = TableReader(os.path.join(simOutDir, "Main"))
@@ -43,21 +45,24 @@ class Plot(singleAnalysisPlot.SingleAnalysisPlot):
 
 		# Plotting
 		plt.figure(figsize = (8.5, 11))
-		### TODO: MAKE THESE PLOTS LOOP THROUGH ALL NEW GENES
 
 		# Protein Counts
 		plt.subplot(2, 1, 1)
-		plt.plot(time / 60., new_gene_monomer_counts)
+		for m in range(len(new_gene_monomer_IDS)):
+			plt.plot(time / 60., new_gene_monomer_counts[:,m], label = new_gene_monomer_IDS[m])
 		plt.xlabel("Time (min)")
 		plt.ylabel("Protein Counts")
 		plt.title("New Gene Protein Counts")
+		plt.legend()
 
 		# mRNA Counts
 		plt.subplot(2, 1, 2)
-		plt.plot(time / 60., new_gene_mrna_counts)
+		for r in range(len(new_gene_mRNA_IDS)):
+			plt.plot(time / 60., new_gene_mrna_counts[:,r], label = new_gene_mRNA_IDS[r])
 		plt.xlabel("Time (min)")
 		plt.ylabel("mRNA Counts")
 		plt.title("New Gene mRNA Counts")
+		plt.legend()
 
 		plt.subplots_adjust(hspace = 0.5, top = 0.95, bottom = 0.05)
 		exportFigure(plt, plotOutDir, plotOutFileName, metadata)
