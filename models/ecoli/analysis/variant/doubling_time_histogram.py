2c88fcfd3fe69d4f52e9d77df779277578142e73
rjuenemann13 <rjuene@stanford.edu>
2022-11-27 15:33:04 -0800

    modify doubling time histogram analysis to include early vs late gens

--- models/ecoli/analysis/variant/doubling_time_histogram.py
+++ models/ecoli/analysis/variant/doubling_time_histogram.py
@@ -1,3 +1,7 @@
+"""
+Plot doubling time and instantaneous growth rate for all generations, early generations, and late (i.e. not early) generations
+"""
+
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -8,6 +12,7 @@ from wholecell.analysis.plotting_tools import COLORS_COLORBLIND as COLORS
 
 FONT_SIZE=9
 MAX_CELL_LENGTH = 180  # filter sims that reach the max time of 180 min
+MIN_LATE_CELL_INDEX = 4 # generations before this may not be representative of dynamics due to how they are initialized
 
 
 class Plot(variantAnalysisPlot.VariantAnalysisPlot):
@@ -30,7 +35,12 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 
 	def do_plot(self, inputDir, plotOutDir, plotOutFileName, simDataFile, validationDataFile, metadata):
 		doubling_times = {}
+		doubling_times_early_gens = {}
+		doubling_times_late_gens = {}
+
 		growth_rates = {}
+		growth_rates_early_gens = {}
+		growth_rates_late_gens = {}
 
 		def downsample(x):
 			"""Average every n_downsample points to one value to smooth and downsample"""
@@ -50,17 +60,61 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 			growth_rates[variant] = read_stacked_columns(all_cells[dt < MAX_CELL_LENGTH], 'Mass', 'instantaneous_growth_rate',
 				remove_first=True, fun=downsample).squeeze() * 3600.
 
-		_, axes = plt.subplots(2, 1, figsize=(10, 10))
+			if len(all_cells) >= MIN_LATE_CELL_INDEX:
+				early_cell_index = list(range(MIN_LATE_CELL_INDEX))
+				late_cell_index = list(range(MIN_LATE_CELL_INDEX, len(all_cells)))
 
+				early_cells = all_cells[early_cell_index]
+				late_cells = all_cells[late_cell_index]
+
+				dt_early_cells = read_stacked_columns(early_cells, 'Main', 'time',
+										  fun=lambda x: (x[-1] - x[0]) / 60.).squeeze()
+				dt_late_cells = read_stacked_columns(late_cells, 'Main', 'time',
+										  fun=lambda x: (x[-1] - x[0]) / 60.).squeeze()
+
+				doubling_times_early_gens[variant] = doubling_times[variant][early_cell_index]
+				doubling_times_late_gens[variant] = doubling_times[variant][late_cell_index]
+
+				growth_rates_early_gens[variant] = read_stacked_columns(early_cells[dt_early_cells < MAX_CELL_LENGTH], 'Mass',
+															 'instantaneous_growth_rate',
+															 remove_first=True, fun=downsample).squeeze() * 3600.
+
+				growth_rates_late_gens[variant] = read_stacked_columns(late_cells[dt_late_cells < MAX_CELL_LENGTH], 'Mass',
+																		'instantaneous_growth_rate',
+																		remove_first=True,
+																		fun=downsample).squeeze() * 3600.
+		# LATE GENS
+		_, axes = plt.subplots(2, 1, figsize=(10, 10))
+		self.hist(axes[0], doubling_times_late_gens, 'Doubling Time (min)')
+		self.hist(axes[1], growth_rates_late_gens, 'Growth rates (1/hr)', bin_width=0.05, sf=2)
+		plt.tight_layout()
+		exportFigure(plt, plotOutDir, plotOutFileName+'_lategens', metadata)
+
+		axes[0].set_xlim([30, 90])
+		axes[1].set_xlim([0, 2.5])
+		exportFigure(plt, plotOutDir, plotOutFileName + '_lategens_trimmed', metadata)
+
+		# EARLY GENS
+		_, axes = plt.subplots(2, 1, figsize=(10, 10))
+		self.hist(axes[0], doubling_times_early_gens, 'Doubling Time (min)')
+		self.hist(axes[1], growth_rates_early_gens, 'Growth rates (1/hr)', bin_width=0.05, sf=2)
+		plt.tight_layout()
+		exportFigure(plt, plotOutDir, plotOutFileName + '_earlygens', metadata)
+
+		axes[0].set_xlim([30, 90])
+		axes[1].set_xlim([0, 2.5])
+		exportFigure(plt, plotOutDir, plotOutFileName + '_earlygens_trimmed', metadata)
+
+		# ALL GENS
+		_, axes = plt.subplots(2, 1, figsize=(10, 10))
 		self.hist(axes[0], doubling_times, 'Doubling Time (min)')
 		self.hist(axes[1], growth_rates, 'Growth rates (1/hr)', bin_width=0.05, sf=2)
-
 		plt.tight_layout()
-		exportFigure(plt, plotOutDir, plotOutFileName, metadata)
+		exportFigure(plt, plotOutDir, plotOutFileName+'_allgens', metadata)
 
-		axes[0].set_xlim([15, 90])
+		axes[0].set_xlim([30, 90])
 		axes[1].set_xlim([0, 2.5])
-		exportFigure(plt, plotOutDir, plotOutFileName + '_trimmed', metadata)
+		exportFigure(plt, plotOutDir, plotOutFileName + '_allgens_trimmed', metadata)
 
 
 if __name__ == "__main__":
