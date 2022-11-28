33d63aec1a8d2849c9d0781739cee89b9fe17056
rjuenemann13 <rjuene@stanford.edu>
2022-11-27 20:31:09 -0800

    use average value for generation in new_gene_histogram

--- models/ecoli/analysis/variant/new_gene_histogram.py
+++ models/ecoli/analysis/variant/new_gene_histogram.py
@@ -1,5 +1,5 @@
 """
-Plot mRNA and protein counts for new genes
+Plot histogram of mRNA and protein counts for new genes, colored by variant
 """
 
 import numpy as np
@@ -14,13 +14,18 @@ from wholecell.analysis.plotting_tools import DEFAULT_MATPLOTLIB_COLORS as COLOR
 
 FONT_SIZE=9
 MAX_CELL_LENGTH = 180  # filter sims that reach the max time of 180 min
+MIN_LATE_CELL_INDEX = 4 # generations before this may not be representative of dynamics due to how they are initialized
 
 
 class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 	def hist(self, ax, data, xlabel, bin_width=1., xlim=None, sf=1):
+		if xlim:
+			bins = np.histogram(range(xlim[0],xlim[1]+1), bins=int(np.ceil((xlim[1]-xlim[0])/bin_width)))[1]
+
 		for variant, variant_data in data.items():
 			color = COLORS[variant % len(COLORS)]
-			bins = max(1, int(np.ceil((variant_data.max() - variant_data.min()) / bin_width)))
+			if not xlim:
+				bins = max(1, int(np.ceil((variant_data.max() - variant_data.min()) / bin_width)))
 			mean = variant_data.mean()
 			std = variant_data.std()
 			ax.hist(variant_data, bins, color=color, alpha=0.5,
@@ -35,17 +40,8 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 		ax.legend()
 
 	def do_plot(self, inputDir, plotOutDir, plotOutFileName, simDataFile, validationDataFile, metadata):
-		doubling_times = {}
-		growth_rates = {}
-
-		def downsample(x):
-			"""Average every n_downsample points to one value to smooth and downsample"""
-			n_downsample = 100
-			if (extra_points := x.shape[0] % n_downsample) != 0:
-				x = x[:-extra_points]
-			return x.reshape(-1, n_downsample).mean(1).reshape(-1, 1)
-
 		# Data extraction
+		print("---Data Extraction---")
 		variants = self.ap.get_variants()
 		min_variant = min(variants)
 		for variant in variants:
@@ -53,14 +49,10 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 			if len(all_cells) == 0:
 				continue
 
-			dt = read_stacked_columns(all_cells, 'Main', 'time',
-				fun=lambda x: (x[-1] - x[0]) / 60.).squeeze()
-			doubling_times[variant] = dt[dt < MAX_CELL_LENGTH]
-			growth_rates[variant] = read_stacked_columns(all_cells[dt < MAX_CELL_LENGTH], 'Mass', 'instantaneous_growth_rate',
-				remove_first=True, fun=downsample).squeeze() * 3600.
+			if len(all_cells) >= MIN_LATE_CELL_INDEX:
+				early_cell_index = list(range(MIN_LATE_CELL_INDEX))
+				late_cell_index = list(range(MIN_LATE_CELL_INDEX, len(all_cells)))
 
-			all_mRNA_counts = read_stacked_columns(all_cells[dt < MAX_CELL_LENGTH], 'mRNACounts', 'mRNA_counts')
-			all_monomer_counts = read_stacked_columns(all_cells[dt < MAX_CELL_LENGTH], 'MonomerCounts', 'monomerCounts')
 			if variant == min_variant: ### TODO flag new gene mRNAs and proteins more efficiently
 				# Extract mRNA indexes for each new gene
 				sim_dir = all_cells[0]
@@ -84,28 +76,73 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 				new_gene_mRNA_counts = [{} for id in new_gene_mRNA_ids]
 				new_gene_monomer_counts = [{} for id in new_gene_monomer_ids]
 
-			for i in range(len(new_gene_mRNA_ids)):
-				new_gene_mRNA_counts_var = all_mRNA_counts[:,new_gene_mRNA_indexes[i]]
-				new_gene_mRNA_counts_var = downsample(new_gene_mRNA_counts_var)
-				new_gene_mRNA_counts_var[new_gene_mRNA_counts_var == 0] = 10**(-10)
-				new_gene_mRNA_counts[i][variant] = np.log10(new_gene_mRNA_counts_var)
+				new_gene_mRNA_counts_early_gens = [{} for id in new_gene_mRNA_ids]
+				new_gene_monomer_counts_early_gens = [{} for id in new_gene_monomer_ids]
 
-				new_gene_monomer_counts_var = all_monomer_counts[:, new_gene_monomer_indexes[i]]
-				new_gene_monomer_counts_var = downsample(new_gene_monomer_counts_var)
-				new_gene_monomer_counts_var[new_gene_monomer_counts_var == 0] = 10 ** (-10)
-				new_gene_monomer_counts[i][variant] = np.log10(new_gene_monomer_counts_var)
+				new_gene_mRNA_counts_late_gens = [{} for id in new_gene_mRNA_ids]
+				new_gene_monomer_counts_late_gens = [{} for id in new_gene_monomer_ids]
+
+			print("Variant: ",variant)
+			for i in range(len(new_gene_mRNA_ids)):
+				new_gene_mRNA_counts_var = np.ones(len(all_cells))
+				new_gene_monomer_counts_var = np.ones(len(all_cells))
+				for gen in range(len(all_cells)):
+					sim_dir = all_cells[gen]
+					simOutDir = os.path.join(sim_dir, 'simOut')
+					mRNA_counts_reader = TableReader(os.path.join(simOutDir, 'mRNACounts'))
+					monomer_counts_reader = TableReader(os.path.join(simOutDir, "MonomerCounts"))
+
+					mRNA_counts_gen = mRNA_counts_reader.readColumn('mRNA_counts')
+					new_gene_mRNA_counts_gen = mRNA_counts_gen[:, new_gene_mRNA_indexes[i]]
+					new_gene_mRNA_counts_var[gen] = np.mean(new_gene_mRNA_counts_gen)
+
+					monomer_counts_gen = monomer_counts_reader.readColumn('monomerCounts')
+					new_gene_monomer_counts_gen = monomer_counts_gen[:, new_gene_monomer_indexes[i]]
+					new_gene_monomer_counts_var[gen] = np.mean(new_gene_monomer_counts_gen)
+
+				new_gene_mRNA_counts[i][variant] = np.log10(new_gene_mRNA_counts_var + 1)
+				new_gene_monomer_counts[i][variant] = np.log10(new_gene_monomer_counts_var + 1)
+
+				if len(all_cells) >= MIN_LATE_CELL_INDEX:
+					new_gene_mRNA_counts_early_gens[i][variant] = new_gene_mRNA_counts[i][variant][early_cell_index]
+					new_gene_monomer_counts_early_gens[i][variant] = new_gene_monomer_counts[i][variant][early_cell_index]
+
+					new_gene_mRNA_counts_late_gens[i][variant] = new_gene_mRNA_counts[i][variant][late_cell_index]
+					new_gene_monomer_counts_late_gens[i][variant] = new_gene_monomer_counts[i][variant][late_cell_index]
 
 		# Plotting
+		print("---Plotting---")
+		# ALL GENS
 		for i in range(len(new_gene_mRNA_ids)):
 			_, axes = plt.subplots(2, 1, figsize=(10, 10))
-			self.hist(axes[0], new_gene_monomer_counts[i], 'Log10(' + new_gene_monomer_ids[i][:-3] +' Counts)')
-			self.hist(axes[1], new_gene_mRNA_counts[i], 'Log10(' + new_gene_mRNA_ids[i][:-3] +' Counts)')
+			self.hist(axes[0], new_gene_monomer_counts[i], 'Log10(' + new_gene_monomer_ids[i][:-3] +' Counts + 1)', bin_width=0.25, sf=2,xlim=[-1,8])
+			self.hist(axes[1], new_gene_mRNA_counts[i], 'Log10(' + new_gene_mRNA_ids[i][:-3] +' Counts + 1)', bin_width=0.25, sf=2,xlim=[-1,8])
 			plt.tight_layout()
-			exportFigure(plt, plotOutDir, plotOutFileName+'_'+new_gene_monomer_ids[i][:-3], metadata)
+			exportFigure(plt, plotOutDir, plotOutFileName+'_all_gens_'+new_gene_monomer_ids[i][:-3], metadata)
 
-			axes[0].set_xlim([-11, 8])
-			axes[1].set_xlim([-11, 8])
-			exportFigure(plt, plotOutDir, plotOutFileName+'_'+new_gene_monomer_ids[i][:-3] +'_trimmed', metadata)
+		if len(all_cells) >= MIN_LATE_CELL_INDEX:
+			# EARLY GENS
+			for i in range(len(new_gene_mRNA_ids)):
+				_, axes = plt.subplots(2, 1, figsize=(10, 10))
+				self.hist(axes[0], new_gene_monomer_counts_early_gens[i], 'Log10(' + new_gene_monomer_ids[i][:-3] + ' Counts + 1)',
+						  bin_width=0.25, sf=2, xlim=[-1, 8])
+				self.hist(axes[1], new_gene_mRNA_counts_early_gens[i], 'Log10(' + new_gene_mRNA_ids[i][:-3] + ' Counts + 1)',
+						  bin_width=0.25, sf=2, xlim=[-1, 8])
+				plt.tight_layout()
+				exportFigure(plt, plotOutDir, plotOutFileName + '_early_gens_' + new_gene_monomer_ids[i][:-3], metadata)
+
+			# LATE GENS
+			for i in range(len(new_gene_mRNA_ids)):
+				_, axes = plt.subplots(2, 1, figsize=(10, 10))
+				self.hist(axes[0], new_gene_monomer_counts_late_gens[i],
+						  'Log10(' + new_gene_monomer_ids[i][:-3] + ' Counts + 1)',
+						  bin_width=0.25, sf=2, xlim=[-1, 8])
+				self.hist(axes[1], new_gene_mRNA_counts_late_gens[i],
+						  'Log10(' + new_gene_mRNA_ids[i][:-3] + ' Counts + 1)',
+						  bin_width=0.25, sf=2, xlim=[-1, 8])
+				plt.tight_layout()
+				exportFigure(plt, plotOutDir, plotOutFileName + '_late_gens_' + new_gene_monomer_ids[i][:-3], metadata)
+
 		plt.close("all")
 
 
