e01b05d6851a6cc3370c38d217eaef31028da4bc
Riley Juenemann <rjuene@stanford.edu>
2023-04-22 15:43:01 -0700

    update ribosome_crowding gene list to filter early and late gens

--- models/ecoli/analysis/variant/ribosome_crowding.py
+++ models/ecoli/analysis/variant/ribosome_crowding.py
@@ -1,12 +1,12 @@
 """
-Produces a txt file containing a list of genes and the index of variants
-where those genes were overcrowded by ribosomes. Here, overcrowded by
-ribosomes is defined as the actual probability of translation per
-transcript being less than the target probability of translation per
-transcript on average for at least one generation in at least one seed for
-that variant index.
+Produces a txt file for all, early, and late generations containing a list of
+genes and the index of variants where those genes were overcrowded by
+ribosomes. Here, overcrowded by ribosomes is defined as the actual
+probability of translation per transcript being less than the target
+probability of translation per transcript on average for at least one
+generation in at least one seed for that variant index.
 
-TODO: Filter early vs late gens, sims that times out
+TODO: Filter sims that timed out
 """
 
 import numpy as np
@@ -16,13 +16,30 @@ import pickle
 import os
 from wholecell.io.tablereader import TableReader
 from models.ecoli.analysis import variantAnalysisPlot
-from wholecell.analysis.analysis_tools import exportFigure,\
-	read_stacked_columns, stacked_cell_threshold_mask
-from wholecell.analysis.plotting_tools import DEFAULT_MATPLOTLIB_COLORS\
-	as COLORS, labeled_indexable_hist, labeled_indexable_scatter
+from wholecell.analysis.analysis_tools import read_stacked_columns, \
+	stacked_cell_threshold_mask
+
+"""
+1 to plot early (before MIN_LATE_CELL_INDEX), and late generations in
+addition to all generations
+"""
+exclude_early_gens = 1
 
 FONT_SIZE=9
 MAX_VARIANT = 43 # do not include any variant >= this index
+MAX_CELL_INDEX = 16 # do not include any generation >= this index
+
+"""
+generations before this may not be representative of dynamics 
+due to how they are initialized
+"""
+MIN_LATE_CELL_INDEX = 4
+
+"""
+Count number of sims that reach this generation (remember index 7 
+corresponds to generation 8)
+"""
+COUNT_INDEX = 15
 
 class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 	def do_plot(self, inputDir, plotOutDir, plotOutFileName, simDataFile,
@@ -30,7 +47,10 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 
 		# Data extraction
 		print("---Data Extraction---")
-		overcrowded_monomer_index_dict = {}
+		overcrowded_monomer_index_dicts = [{}]
+		if exclude_early_gens == 1:
+			overcrowded_monomer_index_dicts.append({}) # Early gens
+			overcrowded_monomer_index_dicts.append({}) # Late gens
 
 		variants = self.ap.get_variants()
 		min_variant = min(variants)
@@ -65,16 +85,27 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 
 			# Get indexes of proteins corresponding to mRNAs that on
 			# average were overcrowded in any generation for any seed
-			avg_overcrowded_monomer_indexes = np.where(
-				sum(avg_actual_prob_translation_per_transcript <
-				avg_target_prob_translation_per_transcript) > 0)[0]
+			overcrowded_gens, avg_overcrowded_monomer_indexes = np.where(
+				avg_actual_prob_translation_per_transcript <
+					avg_target_prob_translation_per_transcript)
 
 			# Record that these genes were overcrowded this variant
-			for monomer_index in avg_overcrowded_monomer_indexes:
-				if monomer_index not in overcrowded_monomer_index_dict:
-					overcrowded_monomer_index_dict[monomer_index] = []
+			for i in range(len(avg_overcrowded_monomer_indexes)):
+				gen = overcrowded_gens[i]
+				monomer_index = avg_overcrowded_monomer_indexes[i]
+				if monomer_index not in overcrowded_monomer_index_dicts[0]:
+					for j in range(len(overcrowded_monomer_index_dicts)):
+						overcrowded_monomer_index_dicts[j][monomer_index] = []
 
-				overcrowded_monomer_index_dict[monomer_index].append(variant)
+				for j in range(len(overcrowded_monomer_index_dicts)):
+					if j == 1 and gen >= MIN_LATE_CELL_INDEX:
+						continue
+					if j == 2 and gen < MIN_LATE_CELL_INDEX:
+						continue
+					if variant not in overcrowded_monomer_index_dicts[j][
+						monomer_index]:
+						overcrowded_monomer_index_dicts[j][monomer_index].append(
+							variant)
 
 		# Determine the gene ids corresponding to these proteins
 		with open(simDataFile, 'rb') as f:
@@ -87,7 +118,7 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 		mRNA_to_gene_id_dict = dict(zip(mRNA_sim_data['id'],
 										mRNA_sim_data['gene_id']))
 
-		overcrowded_monomer_indexes = overcrowded_monomer_index_dict.keys()
+		overcrowded_monomer_indexes = overcrowded_monomer_index_dicts[0].keys()
 		overcrowded_gene_indexes_to_ids_dict = {monomer_index:
 			mRNA_to_gene_id_dict.get(monomer_to_mRNA_id_dict.get(
 				monomer_ids[monomer_index])) for monomer_index in
@@ -95,16 +126,18 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 
 		# Output
 		print("---Writing Output---")
+		output_descr = ["_all_gens"]
+		if exclude_early_gens == 1:
+			output_descr += ["_early_gens", "_late_gens"]
 
-		print(os.path.join(plotOutDir, plotOutFileName + ".txt"))
-
-		with open(os.path.join(plotOutDir, plotOutFileName + ".txt"),'w') as f:
-			for monomer_id in overcrowded_monomer_index_dict:
-				overcrowded_variant_string = ", ".join(map(str,
-					overcrowded_monomer_index_dict[
-					monomer_id]))
-				f.write("" + overcrowded_gene_indexes_to_ids_dict[
-					monomer_id] + ": " + overcrowded_variant_string + "\n")
+		for j in range(len(overcrowded_monomer_index_dicts)):
+			with open(os.path.join(plotOutDir, plotOutFileName +
+				output_descr[j] + ".txt"), 'w') as f:
+				for monomer_id in overcrowded_monomer_index_dicts[j]:
+					overcrowded_variant_string = ", ".join(map(str,
+						overcrowded_monomer_index_dicts[j][monomer_id]))
+					f.write("" + overcrowded_gene_indexes_to_ids_dict[
+						monomer_id] + ": " + overcrowded_variant_string + "\n")
 
 if __name__ == "__main__":
 	Plot().cli()
