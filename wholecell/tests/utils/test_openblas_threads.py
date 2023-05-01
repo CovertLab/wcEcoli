d04260bd524093bdca02e53648cdf9867196d13b
Jerry Morrison <1fish2@users.noreply.github.com>
2023-04-30 23:32:15 -0700

    measure timing in test_openblas_threads
    This will give a quick reading on BLAS performance, in particular whether we've succeeded in getting Apple's Accelerate library to outperform OpenBLAS. Much quicker than running WCM.

--- wholecell/tests/utils/test_openblas_threads.py
+++ wholecell/tests/utils/test_openblas_threads.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 """
-Test numpy dot products with different numbers of OpenBLAS threads.
+Test numpy dot products with different numbers of OpenBLAS threads, as
+controlled via the OPENBLAS_NUM_THREADS environment variable.
 
 Check that the result using 1 thread is as expected to test if the wcEcoli
 simulation results might be consistent across platforms.
+
+Actually, if Numpy is installed to use a different BLAS library than OpenBLAS,
+this will test that library, and OPENBLAS_NUM_THREADS is unlikely to matter.
+
+The number of OpenBLAS threads can change the dot product computation,
+presumably by changing evaluation order and thus floating point rounding.
 
 Any of these ways works to run this test. The first one is quiet if the test
 passes. The others always show stdout:
@@ -14,6 +21,7 @@ passes. The others always show stdout:
 
 import os
 import sys
+import time
 import unittest
 import warnings
 
@@ -26,31 +34,42 @@ from wholecell.utils import parallelization
 THIS_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
 
-def dot_product():
-	"""Return a dot product of 2 data arrays. In some installations, the result
-	varies with the number of OpenBLAS threads!
+def dot_product() -> tuple[float, float]:
+	"""Return (a dot product of 2 data arrays, the time in ns to compute it).
 	"""
 	diff = np.load(os.path.join(THIS_DIR_PATH, 'diff.npy'))
 	mass = np.load(os.path.join(THIS_DIR_PATH, 'mass.npy'))
-	return diff.dot(mass)
+
+	elapsed_start = time.monotonic_ns()
+	dot = diff.dot(mass)
+	elapsed_end = time.monotonic_ns()
+	elapsed_nanoseconds = elapsed_end - elapsed_start
+
+	return dot, elapsed_nanoseconds
 
 
 class Test_openblas_threads(unittest.TestCase):
 	def test_openblas(self):
 		"""Compare a dot product running with various numbers of OpenBLAS threads."""
 		products = []
-		thread_range = list(range(1, parallelization.cpus() + 1)) + ['']
-		print('{:>7} {:>26} {:>26}'.format('THREADS', 'DOT PRODUCT', 'DIFF FROM 1 THREAD'))
+		total_nanoseconds = 0.0
+		thread_range = [str(c) for c in range(1, parallelization.cpus() + 1)] + ['']
+		print('{:>7}  {:>26} {:>26} {:>11}'.format('THREADS', 'DOT PRODUCT', 'DIFF FROM 1 THREAD', 'NANOSECS'))
 
 		for num_threads in thread_range:
-			env = dict(os.environ, OPENBLAS_NUM_THREADS=str(num_threads))
+			env = dict(os.environ, OPENBLAS_NUM_THREADS=num_threads)
 			command = 'python -m wholecell.tests.utils.test_openblas_threads DOT'.split()
-			dot = float(fp.run_cmd(command, env=env))
+			output = fp.run_cmd(command, env=env).split(',')
+			dot = float(output[0])
+			nanoseconds = float(output[1])
+			total_nanoseconds += nanoseconds
 
 			products.append(dot)
 			diff = dot - products[0]
-			print('{:7} {:26.17g} {:26.17g}'.format(num_threads, dot, diff))
+			print('{:>7}  {:26.17g} {:26.17g} {:11,g}'.format(num_threads, dot, diff, nanoseconds))
 
+		print(f'{"":62} {11 * "-"}')
+		print(f'{"":62} {round(total_nanoseconds):11,}')
 		print()
 
 		# Issue #931: The expected value came from Numpy's copy of openblas on
@@ -75,6 +94,7 @@ class Test_openblas_threads(unittest.TestCase):
 
 if __name__ == '__main__':
 	if sys.argv[1:] == ['DOT']:
-		print(dot_product())
+		product, nanoseconds = dot_product()
+		print(f'{product}, {nanoseconds}')
 	else:
 		unittest.main()
