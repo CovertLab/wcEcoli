58e9f7a9584219be5692b423372dd13b43c29141
Riley Juenemann <rjuene@stanford.edu>
2023-07-19 14:37:09 -0700

    remove options from variant crowding scripts and update with PR comments

--- models/ecoli/analysis/variant/rnap_crowding.py
+++ models/ecoli/analysis/variant/rnap_crowding.py
@@ -11,50 +11,30 @@ import numpy as np
 import os
 from wholecell.io.tablereader import TableReader
 from models.ecoli.analysis import variantAnalysisPlot
-from wholecell.analysis.analysis_tools import read_stacked_columns, \
-	stacked_cell_threshold_mask
-
-# 1 to exclude cells that took full MAX_CELL_LENGTH, 0 otherwise
-exclude_timeout_cells = 1
-
-"""
-1 to plot early (before MIN_LATE_CELL_INDEX), and late generations in
-addition to all generations
-"""
-exclude_early_gens = 1
+from wholecell.analysis.analysis_tools import read_stacked_columns
 
 FONT_SIZE=9
 MAX_VARIANT = 43 # do not include any variant >= this index
-MAX_CELL_INDEX = 16 # do not include any generation >= this index
 
 """
-generations before this may not be representative of dynamics 
+Plot data from generations [MIN_CELL_INDEX, MAX_CELL_INDEX)
+Note that early generations may not be representative of dynamics 
 due to how they are initialized
 """
-MIN_LATE_CELL_INDEX = 4
-
-MAX_CELL_LENGTH = 36000
-if (exclude_timeout_cells==0):
-	MAX_CELL_LENGTH += 1000000
+MIN_CELL_INDEX = 4
+MAX_CELL_INDEX = 16
 
 class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 	def do_plot(self, inputDir, plotOutDir, plotOutFileName, simDataFile,
 				validationDataFile, metadata):
-
 		# Data extraction
 		print("---Data Extraction---")
-		overcrowded_tu_index_dicts = [{}]
-		if exclude_early_gens == 1:
-			overcrowded_tu_index_dicts.append({}) # Early gens
-			overcrowded_tu_index_dicts.append({}) # Late gens
+		overcrowded_tu_index_dict = {}
 
 		variants = self.ap.get_variants()
 		min_variant = min(variants)
-
 		all_generations = {}
-
 		for variant in variants:
-
 			if variant >= MAX_VARIANT:
 				continue
 
@@ -63,13 +43,17 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 			if len(all_cells) == 0:
 				continue
 
-			exclude_timeout_cell_mask = stacked_cell_threshold_mask(
-				all_cells, 'Main', 'time', MAX_CELL_LENGTH,
-				fun=lambda x: (x[-1] - x[0]) / 60.).squeeze()
-			all_cells_gens = \
-				np.array([int(os.path.basename(os.path.dirname(
-					cell_path))[-6:]) for cell_path in all_cells])
+			all_cells_gens = np.array([
+				int(os.path.basename(os.path.dirname(
+				cell_path))[-6:]) for cell_path in all_cells])
 			all_generations[variant] = all_cells_gens
+			cell_mask = np.logical_and(
+				(all_generations[variant] >=MIN_CELL_INDEX),
+				(all_generations[variant] < MAX_CELL_INDEX))
+			if len(cell_mask) == 1:
+				cell_mask = cell_mask.reshape(1)
+			if sum(cell_mask) < 1:
+				continue
 
 			# Get tu ids
 			if variant == min_variant:
@@ -81,73 +65,39 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 				rna_ids = rnap_reader.readAttribute('rnaIds')
 
 			# RNA polymerase overcrowding
-			avg_actual_rna_synth_prob = read_stacked_columns(all_cells,
-				'RnaSynthProb','actual_rna_synth_prob',
-				fun=lambda x: np.mean(x, axis=0))[exclude_timeout_cell_mask]
-			avg_target_rna_synth_prob = read_stacked_columns(all_cells,
-				'RnaSynthProb','target_rna_synth_prob',
-				fun=lambda x: np.mean(x, axis=0))[exclude_timeout_cell_mask]
+			avg_actual_rna_synth_prob = read_stacked_columns(
+				all_cells, 'RnaSynthProb', 'actual_rna_synth_prob',
+				fun=lambda x: np.mean(x, axis=0))
+			avg_target_rna_synth_prob = read_stacked_columns(
+				all_cells, 'RnaSynthProb', 'target_rna_synth_prob',
+				fun=lambda x: np.mean(x, axis=0))
 
 			avg_overcrowded_tu_indexes = np.where(
-				sum(avg_actual_rna_synth_prob <
-					avg_target_rna_synth_prob) > 0)[0]
-
-			if exclude_early_gens == 1:
-				early_cell_mask = all_generations[variant] < MIN_LATE_CELL_INDEX
-				if len(early_cell_mask) == 1:
-					early_cell_mask = early_cell_mask[0]
-				early_cell_mask = early_cell_mask[exclude_timeout_cell_mask]
-				avg_overcrowded_tu_indexes_early = np.where(
-					sum((avg_actual_rna_synth_prob <
-						 avg_target_rna_synth_prob)[early_cell_mask]) > 0)[0]
-
-				late_cell_mask = all_generations[variant] >= MIN_LATE_CELL_INDEX
-				if len(late_cell_mask) == 1:
-					late_cell_mask = late_cell_mask[0]
-				late_cell_mask = late_cell_mask[exclude_timeout_cell_mask]
-				avg_overcrowded_tu_indexes_late = np.where(
-					sum((avg_actual_rna_synth_prob <
-						 avg_target_rna_synth_prob)[late_cell_mask]) > 0)[0]
+				sum((avg_actual_rna_synth_prob <
+					avg_target_rna_synth_prob)[cell_mask]) > 0)[0]
 
 			# Record that these genes were overcrowded this variant
 			for tu_index in avg_overcrowded_tu_indexes:
-				if tu_index not in overcrowded_tu_index_dicts[0]:
-					overcrowded_tu_index_dicts[0][tu_index] = []
-				overcrowded_tu_index_dicts[0][tu_index].append(
-					variant)
-
-			if exclude_early_gens == 1:
-				for tu_index in avg_overcrowded_tu_indexes_early:
-					if tu_index not in overcrowded_tu_index_dicts[1]:
-						overcrowded_tu_index_dicts[1][tu_index] = []
-					overcrowded_tu_index_dicts[1][tu_index].append(
-						variant)
-
-				for tu_index in avg_overcrowded_tu_indexes_late:
-					if tu_index not in overcrowded_tu_index_dicts[2]:
-						overcrowded_tu_index_dicts[2][tu_index] = []
-					overcrowded_tu_index_dicts[2][tu_index].append(
-						variant)
+				if tu_index not in overcrowded_tu_index_dict:
+					overcrowded_tu_index_dict[tu_index] = []
+				overcrowded_tu_index_dict[tu_index].append(variant)
 
 		# Determine the ids corresponding to these tus
-		overcrowded_tu_indexes = overcrowded_tu_index_dicts[0].keys()
-		overcrowded_gene_indexes_to_ids_dict = {tu_index:
-			rna_ids[tu_index] for tu_index in overcrowded_tu_indexes}
+		overcrowded_tu_indexes = overcrowded_tu_index_dict.keys()
+		overcrowded_gene_indexes_to_ids_dict = {
+			tu_index: rna_ids[tu_index] for tu_index in overcrowded_tu_indexes}
 
 		# Output
 		print("---Writing Output---")
-		output_descr = ["_all_gens"]
-		if exclude_early_gens == 1:
-			output_descr += ["_early_gens", "_late_gens"]
+		plot_suffix = "_gens_" + str(MIN_CELL_INDEX) + "_through_" + str(MAX_CELL_INDEX)
 
-		for j in range(len(overcrowded_tu_index_dicts)):
-			with open(os.path.join(plotOutDir, plotOutFileName +
-				output_descr[j] + ".txt"), 'w') as f:
-				for tu_id in overcrowded_tu_index_dicts[j]:
-					overcrowded_variant_string = ", ".join(map(str,
-						overcrowded_tu_index_dicts[j][tu_id]))
-					f.write("" + overcrowded_gene_indexes_to_ids_dict[
-						tu_id] + ": " + overcrowded_variant_string + "\n")
+		with open(os.path.join(plotOutDir, plotOutFileName +
+			plot_suffix + ".txt"), 'w') as f:
+			for tu_id in overcrowded_tu_index_dict:
+				overcrowded_variant_string = ", ".join(map(str,
+					overcrowded_tu_index_dict[tu_id]))
+				f.write(f"{overcrowded_gene_indexes_to_ids_dict[tu_id]}:"
+					f" {overcrowded_variant_string}\n")
 
 if __name__ == "__main__":
 	Plot().cli()
