a0acc462b929ace552c83930ccee508bdb15946f
Gwanggyu Sun <ggsun@stanford.edu>
2023-11-14 17:23:32 -0800

    Add new comparison analysis script for RNAP densities

--- models/ecoli/analysis/comparison/__init__.py
+++ models/ecoli/analysis/comparison/__init__.py
@@ -24,6 +24,7 @@ ACTIVE = [
 	"proteomics_fluxomics_validation.py",
 	"replisome_rnap_collision_counts.py",
 	"rrna_gene_copy_numbers.py",
+	"rrna_gene_rnap_densities.py",
 	"rrna_to_ribosome_yield.py",
 	"tRNA_cistron_expression.py",
 ]
