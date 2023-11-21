2110bf74e04979939bf3f517ffaefe700dcf26c3
Gwanggyu Sun <ggsun@stanford.edu>
2023-11-20 18:14:09 -0800

    Add media options for rRNA variants

--- models/ecoli/sim/variants/rrna_location.py
+++ models/ecoli/sim/variants/rrna_location.py
@@ -1,6 +1,6 @@
 """
 Variant to compare the impacts of changing the locations of rRNA genes on the
-chromosome.
+chromosome in multiple media conditions.
 
 Modifies:
 	sim_data.process.transcription.rna_data["replication_coordinate"]
@@ -10,6 +10,8 @@ Expected variant indices:
 	1: symmetrically flip the chromosomal positions of all rRNA-encoding genes
 	such that most of them lie closer to terC than oriC (Note: there may be
 	some genes that physically overlap as a result of this change)
+	2: same as 1, in rich media conditions
+	3: same as 1, in minimal-to-rich media shift conditions
 """
 
 import numpy as np
@@ -23,7 +25,7 @@ CONTROL_OUTPUT = dict(
 
 
 def rrna_location(sim_data, index):
-	if index == 1:
+	if index > 0:
 		rna_is_rrna = sim_data.process.transcription.rna_data['is_rRNA']
 		rna_coordinates = sim_data.process.transcription.rna_data[
 			"replication_coordinate"]
@@ -53,9 +55,32 @@ def rrna_location(sim_data, index):
 			gene_coordinates[i] = flip_coordinates(
 				gene_coordinates[i], gene_lengths[i])
 
+		# Change media conditions for indexes 2 and 3
+		condition_id = sim_data.condition
+		if index == 2:
+			condition_labels = sim_data.ordered_conditions
+			condition_id = condition_labels[1]  # Index for rich media condition
+			sim_data.condition = condition_id
+			sim_data.external_state.current_timeline_id = condition_id
+			sim_data.external_state.saved_timelines[condition_id] = [
+				(0, sim_data.conditions[condition_id]["nutrients"])
+				]
+		elif index == 3:
+			saved_timelines = sim_data.external_state.saved_timelines
+			timeline_ids = sorted(saved_timelines)
+			condition_id = timeline_ids[28]  # Index for minimal-to-rich media shift
+			sim_data.external_state.current_timeline_id = condition_id
+
+			# Get possible condition from starting nutrients for proper
+			# initialization
+			nutrients = saved_timelines[condition_id][0][1]
+			conditions = [cond for cond in sim_data.condition_active_tfs
+				if sim_data.conditions[cond]['nutrients'] == nutrients]
+			sim_data.condition = conditions[0]
+
 		return dict(
-			shortName = "rrna_relocated",
-			desc = "Simulation with relocated rRNA genes",
+			shortName = f"rrna_relocated_{condition_id}",
+			desc = f"Simulation with relocated rRNA genes in {condition_id}",
 			), sim_data
 
 	else:
--- models/ecoli/sim/variants/rrna_operon_knockout.py
+++ models/ecoli/sim/variants/rrna_operon_knockout.py
@@ -12,13 +12,9 @@ Modifies:
 
 Expected variant indices:
 	0: control
-	1-7: knockout of one rRNA operon
-	8-28: knockout of two rRNA operons
-	29-63: knockout of three rRNA operons
-	64-98: knockout of four rRNA operons
-	99-119: knockout of five rRNA operons
-	120-126: knockout of six rRNA operons
-	127: knockout of all seven rRNA operons
+	1-6: knockout of one to six rRNA operons in minimal media conditions
+	7-12: knockout of one to six rRNA operons in rich media conditions
+	13-18: knockout of one to six rRNA operons in minimal-to-rich shift conditions
 """
 
 import numpy as np
@@ -31,21 +27,18 @@ CONTROL_OUTPUT = dict(
 
 def rrna_operon_knockout(sim_data, index):
 	if index > 0:
-		def int_to_bool_array(i):
-			return np.array([bool(i & (1 << n)) for n in range(7)])
+		n_rrna_to_ko = index % 6
+		if n_rrna_to_ko == 0:
+			n_rrna_to_ko = 6
 
-		# Build arrays for every combination of rRNAs to knock out
-		rrna_ko_array = np.array([
-			int_to_bool_array(i) for i in range(1, 128)
-			])
-
-		# Sort by number of rRNAs being knocked out
-		rrna_ko_array = rrna_ko_array[np.argsort(rrna_ko_array.sum(axis=1)), :]
+		# Build boolean array for rRNAs to knock out
+		rrna_ko_array = np.zeros(7, dtype=bool)
+		rrna_ko_array[:n_rrna_to_ko] = True
 
 		# Get genes to knock out for this variant
 		rRNA_operons_to_ko = [
 			sim_data.molecule_groups.rRNA_operons[i] for i in
-			np.where(rrna_ko_array[index - 1, :])[0]]
+			np.where(rrna_ko_array)[0]]
 		genes_to_ko = []
 		for rRNA_operon in rRNA_operons_to_ko:
 			genes_to_ko.extend(sim_data.molecule_groups.__dict__[rRNA_operon])
@@ -60,9 +53,34 @@ def rrna_operon_knockout(sim_data, index):
 		sim_data.adjust_final_expression(
 			rna_indexes_to_ko, np.zeros_like(rna_indexes_to_ko))
 
+		condition_index = (index - 1)//6
+
+		# Change media conditions for condition indexes 1 and 2
+		condition_id = sim_data.condition
+		if condition_index == 1:
+			condition_labels = sim_data.ordered_conditions
+			condition_id = condition_labels[1]  # Index for rich media condition
+			sim_data.condition = condition_id
+			sim_data.external_state.current_timeline_id = condition_id
+			sim_data.external_state.saved_timelines[condition_id] = [
+				(0, sim_data.conditions[condition_id]["nutrients"])
+				]
+		elif condition_index == 2:
+			saved_timelines = sim_data.external_state.saved_timelines
+			timeline_ids = sorted(saved_timelines)
+			condition_id = timeline_ids[28]  # Index for minimal-to-rich media shift
+			sim_data.external_state.current_timeline_id = condition_id
+
+			# Get possible condition from starting nutrients for proper
+			# initialization
+			nutrients = saved_timelines[condition_id][0][1]
+			conditions = [cond for cond in sim_data.condition_active_tfs
+				if sim_data.conditions[cond]['nutrients'] == nutrients]
+			sim_data.condition = conditions[0]
+
 		return dict(
-			shortName = f"{'_'.join(rRNA_operons_to_ko)}_rrna_knockout",
-			desc = f"Simulation with the {', '.join(rRNA_operons_to_ko)} rRNA operons knocked out"
+			shortName = f"{'_'.join(rRNA_operons_to_ko)}_rrna_knockout_{condition_id}",
+			desc = f"Simulation with the {', '.join(rRNA_operons_to_ko)} rRNA operons knocked out in {condition_id}"
 			), sim_data
 
 	else:
