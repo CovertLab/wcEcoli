a026fa10b59bcc7fbc6b9f79f99511417a570630
mpg19 <miagrahn@stanford.edu>
2024-11-06 12:01:03 -0800

    Made temporary convert to flat file for matching based on synonyms as well as common name.

---
+++ reconstruction/ecoli/scripts/protein_half_lives/convert_to_flat_TEST.py
@@ -0,0 +1,179 @@
+"""
+Extracts some of protein C-limited half-lives from Supplementary Table S1 of Gupta et. al.,
+"Global Protein-Turnover Quantification in Escherichia coli Reveals Cytoplasmic Recycling under Nitrogen Limitation"
+doi: https://doi.org/10.1101/2022.08.01.502339
+"""
+
+import io
+import os
+import time
+from typing import Dict
+import pandas as pd
+from wholecell.io import tsv
+from wholecell.utils.filepath import ROOT_PATH
+
+# Arbitrarily-determined higher half-life time cutoff in minutes. Proteins with
+# half-lives above MAX_HALF_LIFE are all given a half-life of MAX_HALF_LIFE.
+MAX_HALF_LIFE = 5000.
+
+# Number of decimals when rounding half-life values (in units of min)
+ROUND_N_DECIMALS = 1
+
+# Directories
+FILE_LOCATION = os.path.realpath(os.path.dirname(__file__))
+
+# Files
+INPUT = os.path.join(FILE_LOCATION, 'protein_half_lives_Clim2.tsv')
+RNAS_FILE = os.path.join(
+    ROOT_PATH, 'reconstruction', 'ecoli', 'flat', 'rnas.tsv')
+OUTPUT_FLAT_FILE = os.path.join(
+    ROOT_PATH, 'reconstruction', 'ecoli', 'flat',
+    'protein_half_lives_Clim2_TEST_NEW1.tsv')
+
+
+def get_symbols_to_monomer_ids():
+    # type: () -> Dict[str, str]
+    """
+    Builds a mapping from gene symbols to the protein monomer IDs, used in later
+    data import functions.
+
+    Returns: symbols_to_monomer_ids: Dictionary from gene symbol to first
+    associated protein monomer ID.
+    """
+    symbols_to_monomer_ids = {}
+
+    # Map gene/protein symbols to protein monomer IDs
+    with io.open(RNAS_FILE, 'rb') as f:
+        reader = tsv.reader(f, delimiter='\t')
+
+        headers = next(reader)
+        while headers[0].startswith('#'):
+            headers = next(reader)
+
+        gene_symbol_index = headers.index('common_name')
+        protein_id_index = headers.index('monomer_ids')
+
+        # TODO(ahzhang): Handle cistrons with two or more associated proteins
+        # Currently, each gene symbol in data is associated with the first
+        # associated protein id, which is given a half-life in translation.py.
+        # Translation.py also only accounts for first associated protein id when
+        # assigning half-lives.
+        for line in reader:
+            gene_symbol = line[gene_symbol_index]
+            protein_id = list(line[protein_id_index][2:-2].split('", "'))[0]
+
+            symbols_to_monomer_ids[gene_symbol] = protein_id
+
+        return symbols_to_monomer_ids
+
+
+def get_half_lives(symbols_to_monomer_ids):
+    # type: (Dict[str, str]) -> Dict[str, float]
+    """
+    Reads protein half-lives in hours from Gupta et. al. (2022), converts them to minutes,
+    and stores them in a dictionary. Any protein listed that does not exist in the model is skipped.
+
+    Args:
+        symbols_to_monomer_ids: Dictionary that maps gene symbols to protein
+        monomer ID's used by the model.
+
+    Returns:
+        id_to_half_life: Dictionary that maps protein monomer ids to their
+        half-lives as reported in Gupra et. al. (2022).
+
+    """
+
+    # first, check that the right gene symbol (that the model recognizes) is
+    # being used in the input file (and not a synonym)
+    original_input = pd.read_csv(INPUT, sep='\t', )
+    corrected_input = original_input.copy()
+    gene_list = corrected_input['Gene id']
+
+    # load in the rnas file:
+    rnas = pd.read_csv(RNAS_FILE, sep='\t', skiprows=[0, 1, 2, 3], )
+    common_names = rnas['common_name']
+    rnas_synonyms = rnas['synonyms']
+
+    # Check which gene symbols in the input file are not in the common names list:
+    genes_in_sort_not_rnas = gene_list[~gene_list.isin(common_names)]
+    print('Genes in the sort file that are not in the rnas file:', len(genes_in_sort_not_rnas))
+
+    #
+
+    for gene in genes_in_sort_not_rnas: # Ok so I noticed something very strange.
+        # If I loop through every gene, some genes fall into more than one synonym
+        # (whereas in my Jupyter notebook, I only looked at genes that were a miss
+        # match between the sort file and the common_names in rnas.tsv. NOT sure
+        # which is the best way to select the common name if a gene matches 2.
+        # And for some reason, when I do it this way (looping through all the options,
+        # there are less total matches in the output file! (maybe this is because the duplicates are overriden?))
+        for index, synonym in enumerate(rnas_synonyms):
+            # Check if the gene is in the current synonyms list
+            if gene in synonym:
+                # change the value in the sort file to the common name value:
+                corrected_input.loc[corrected_input['Gene id'] == gene, 'Gene id'] = \
+                    rnas['common_name'][index]
+                break  # Stop searching after the first match
+
+    # save the corrected input file (take out the .tsv extension):
+    CORRECTED_INPUT = INPUT[:-4] + '_corrected_TEST1.tsv'
+    corrected_input.to_csv(CORRECTED_INPUT, sep='\t', index=False)
+
+
+    id_to_half_life = {}
+
+    with io.open(CORRECTED_INPUT, 'rb') as f:
+        reader = tsv.reader(f, delimiter='\t')
+
+        headers = next(reader)
+
+        gene_symbol_index = headers.index('Gene id')
+        half_life_index = headers.index('half_life (units.min)')
+
+        for line in reader:
+            gene_symbol = line[gene_symbol_index]
+            if line[half_life_index]:
+                half_life = float(line[
+                                      half_life_index])  # TODO: removed a * 60 here. should I have? Yes because the file was already in minutes.
+                if gene_symbol in symbols_to_monomer_ids:
+                    id_to_half_life[symbols_to_monomer_ids[gene_symbol]] = half_life
+
+    return id_to_half_life
+
+
+def build_half_life_table(raw_half_lives):
+    # type: (Dict[str, float]) -> None
+    """
+    Builds the protein half-life flat file that is used as raw data for
+    simulations using them. Assigns all proteins with half-lives greater than
+    the arbitrarily chosen MAX_HALF_LIFE as having a half-life of MAX_HALF-LIFE.
+
+    Args:
+        raw_half_lives: Dictionary that maps monomer ID's to their half-lives
+        as reported in Gupta et. al. (2022).
+    """
+    # Builds list of tuples that stores half-life data
+    half_life_data = [
+        (monomer_id, round(min(half_life, MAX_HALF_LIFE), ROUND_N_DECIMALS))
+        for (monomer_id, half_life) in raw_half_lives.items()
+    ]
+
+    # Sort by monomer ID
+    half_life_data.sort(key=lambda v: v[0])
+
+    # Write to flat file
+    with io.open(OUTPUT_FLAT_FILE, 'wb') as f:
+        print('Writing to {}'.format(f.name))
+        writer = tsv.writer(f, quotechar="'", lineterminator='\n')
+        writer.writerow(['# Generated by {} on {}'.format(__file__, time.ctime())])
+        writer.writerow(['"id"', '"half_life (units.min)"'])
+
+        for row in half_life_data:
+            writer.writerow([f'"{row[0]}"', f'{row[1]}'])
+
+
+if __name__ == '__main__':
+    symbols_to_monomer_ids = get_symbols_to_monomer_ids()
+    raw_half_lives = get_half_lives(symbols_to_monomer_ids)
+
+    build_half_life_table(raw_half_lives)
