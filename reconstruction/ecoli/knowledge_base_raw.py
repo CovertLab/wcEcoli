48db8147e91747827e5912fd492fedc4d3509df4
Nora <nora.vivanco@gmail.com>
2024-11-08 12:03:27 -0800

    new clim values added

--- reconstruction/ecoli/knowledge_base_raw.py
+++ reconstruction/ecoli/knowledge_base_raw.py
@@ -61,6 +61,7 @@ LIST_OF_DICT_FILENAMES = [
 	"protein_half_lives_n_end_rule.tsv",
 	"protein_half_lives_pulsed_silac.tsv",
 	"protein_half_lives_Clim.tsv",
+	"protein_half_lives_Clim3.tsv",
 	"proteins.tsv",
 	"relative_metabolite_concentrations.tsv",
 	"rna_half_lives.tsv",
