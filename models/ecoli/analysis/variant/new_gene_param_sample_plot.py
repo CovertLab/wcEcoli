3f1dbddf5364438134c0f0f592d3431466adb6f9
Riley Juenemann <rjuene@stanford.edu>
2025-01-21 16:27:20 -0800

    clean up new_gene_param_sample_plot.py code

--- models/ecoli/analysis/variant/new_gene_param_sample_plot.py
+++ models/ecoli/analysis/variant/new_gene_param_sample_plot.py
@@ -1,29 +1,23 @@
 """
-plotting parameters from new_gene_param_sample variant
+Plotting expression factor and translation efficiency parameters used for
+new gene expression from new_gene_param_sample variant in order to verify
+sampling is uniform across the range of values.
 """
 
 from matplotlib import pyplot as plt
-# noinspection PyUnresolvedReferences
 import numpy as np
 
 from models.ecoli.analysis import variantAnalysisPlot
+from models.ecoli.sim.variants.new_gene_param_sampling_internal_shift import (
+	NEW_GENE_EXPRESSION_FACTOR_CONTROL, NEW_GENE_EXPRESSION_FACTOR_MIN, NEW_GENE_EXPRESSION_FACTOR_MAX,
+	NEW_GENE_TRANSLATION_EFFICIENCY_CONTROL, NEW_GENE_TRANSLATION_EFFICIENCY_MIN, NEW_GENE_TRANSLATION_EFFICIENCY_MAX
+	)
 from wholecell.analysis.analysis_tools import exportFigure
-
-
 
 class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 	def do_plot(self, inputDir, plotOutDir, plotOutFileName, simDataFile, validationDataFile, metadata):
 
 		variants = self.ap.get_variants()
-
-
-
-		NEW_GENE_EXPRESSION_FACTOR_CONTROL = 0
-		NEW_GENE_EXPRESSION_FACTOR_MIN = 7
-		NEW_GENE_EXPRESSION_FACTOR_MAX = 10
-		NEW_GENE_TRANSLATION_EFFICIENCY_CONTROL = 0
-		NEW_GENE_TRANSLATION_EFFICIENCY_MIN = np.log10(0.01)
-		NEW_GENE_TRANSLATION_EFFICIENCY_MAX = np.log10(10)
 
 		plt.figure()
 		plt.xlabel("Expression Factor")
@@ -32,40 +26,19 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 		plt.yscale('log')
 
 		for variant in variants:
+			np.random.seed(variant)
+			if variant == 0:
+				expression_factor = NEW_GENE_EXPRESSION_FACTOR_CONTROL
+				trl_eff_value = NEW_GENE_TRANSLATION_EFFICIENCY_CONTROL
+			else:
+				expression_factor = np.random.uniform(NEW_GENE_EXPRESSION_FACTOR_MIN, NEW_GENE_EXPRESSION_FACTOR_MAX)
+				trl_eff_value = 10 ** np.random.uniform(NEW_GENE_TRANSLATION_EFFICIENCY_MIN,NEW_GENE_TRANSLATION_EFFICIENCY_MAX)
 
-				#if variant >1:
-					#continue
-				np.random.seed(variant)
-				if variant == 0:
-					expression_factor = NEW_GENE_EXPRESSION_FACTOR_CONTROL
-					trl_eff_value = NEW_GENE_TRANSLATION_EFFICIENCY_CONTROL
-				else:
-					expression_factor = np.random.uniform(NEW_GENE_EXPRESSION_FACTOR_MIN, NEW_GENE_EXPRESSION_FACTOR_MAX)
-					trl_eff_value = 10 ** np.random.uniform(NEW_GENE_TRANSLATION_EFFICIENCY_MIN,NEW_GENE_TRANSLATION_EFFICIENCY_MAX)
-
-				plt.scatter(expression_factor, trl_eff_value)
-
-
-
-		### Create Plot ###
+			plt.scatter(expression_factor, trl_eff_value)
 
 		plt.tight_layout()
 		exportFigure(plt, plotOutDir, plotOutFileName, metadata)
 		plt.close('all')
 
-
 if __name__ == "__main__":
 	Plot().cli()
-from models.ecoli.analysis import analysisPlot
-from runscripts.manual import analysisVariant
-
-
-
-
-
-
-
-
-
-
-
