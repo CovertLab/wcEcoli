c6e387211c5a2fcef73f35f9fcb92592b28b7a5e
mpg19 <miagrahn@stanford.edu>
2025-02-05 17:36:19 -0800

    Updated indentations and averaging method.

--- models/ecoli/analysis/multigen/new_gene_counts.py
+++ models/ecoli/analysis/multigen/new_gene_counts.py
@@ -66,8 +66,8 @@ class Plot(multigenAnalysisPlot.MultigenAnalysisPlot):
 		time = read_stacked_columns(cell_paths, 'Main', 'time')
 		(new_gene_monomer_counts,) = read_stacked_bulk_molecules(
 			cell_paths, new_gene_monomer_ids)
-		new_gene_mRNA_counts = read_stacked_columns(
-			cell_paths, 'RNACounts', 'mRNA_counts')[:,new_gene_mRNA_indexes]
+		new_gene_mRNA_counts = read_stacked_columns(cell_paths,
+			'RNACounts', 'mRNA_counts')[:,new_gene_mRNA_indexes]
 
 		# Plotting
 		plt.figure(figsize = (8.5, 11))
