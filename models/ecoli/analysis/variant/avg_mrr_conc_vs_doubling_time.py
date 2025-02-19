258ef5c556fbd61f3621cd6a19d6b4b65b83a10c
Yinglin Situ <yinglinsitu@Yinglins-MacBook-Pro.local>
2025-02-19 10:38:52 -0800

    edit the doubling time plot for expression factor readout

--- models/ecoli/analysis/variant/avg_mrr_conc_vs_doubling_time.py
+++ models/ecoli/analysis/variant/avg_mrr_conc_vs_doubling_time.py
@@ -1,8 +1,7 @@
 """
-Plot a scatterplot of the average new gene protein counts vs. doubling time,
-with the option to color the points by expression factor or translation
-efficiency. This plot is intended to be run on simulations where the new gene
-option was enabled.
+Plot a scatterplot of the average mrr concentration vs. doubling time.
+This plot is intended to be run on simulations where
+the new gene option was enabled.
 """
 
 import numpy as np
@@ -12,8 +11,15 @@ import pickle
 import os
 
 from models.ecoli.analysis import variantAnalysisPlot
-from wholecell.analysis.analysis_tools import exportFigure, read_stacked_columns
+from models.ecoli.sim.variants.new_gene_param_sampling_internal_shift import (
+	NEW_GENE_EXPRESSION_FACTOR_CONTROL, NEW_GENE_EXPRESSION_FACTOR_MIN,
+	NEW_GENE_EXPRESSION_FACTOR_MAX, NEW_GENE_TRANSLATION_EFFICIENCY_CONTROL,
+	NEW_GENE_TRANSLATION_EFFICIENCY_MIN, NEW_GENE_TRANSLATION_EFFICIENCY_MAX)
+from wholecell.analysis.analysis_tools import (
+	exportFigure, read_stacked_columns, stacked_cell_threshold_mask)
 from wholecell.io.tablereader import TableReader
+
+
 
 # Remove first N gens from plot
 IGNORE_FIRST_N_GENS = 16
@@ -26,6 +32,7 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 				validationDataFile, metadata):
 
 		variants = self.ap.get_variants()
+
 
 		# Determine new gene ids
 		with open(simDataFile, 'rb') as f:
@@ -45,29 +52,30 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 		if len(new_gene_monomer_ids) == 0:
 			print("This plot is intended to be run on simulations where the "
 				  "new gene option was enabled, but no new gene proteins "
-				  "were found.")
+				  "were "
+				  "found.")
 			return
 		assert len(new_gene_monomer_ids) == len(new_gene_mRNA_ids), \
 			'number of new gene monomers and mRNAs should be equal'
 
 		# Data extraction
 		plot_variant_mask = np.full(len(variants), True)
-		doubling_times = np.zeros(len(variants))
-		avg_ng_monomer = np.zeros(len(variants))
 		trl_eff_values = np.zeros(len(variants))
 		expression_factors = np.zeros(len(variants))
-		colors = np.zeros(len(variants))
+		doubling_times = np.zeros(len(variants))
+		avg_mrr_count = np.zeros(len(variants))
+		avg_mrr_conc = np.zeros(len(variants))
 		n_total_gens = self.ap.n_generation
 
-		min_variant = min(variants)
 		variant_name = metadata["variant"]
 		params_to_use = metadata["params_to_use"]
 
+		# Loop through all variant indexes
 		for i, variant in enumerate(variants):
 
 			all_cells = self.ap.get_cells(
 				variant=[variant],
-				generation= np.arange(IGNORE_FIRST_N_GENS, n_total_gens),
+				generation=np.arange(IGNORE_FIRST_N_GENS, n_total_gens),
 				only_successful=True)
 
 			if len(all_cells) == 0:
@@ -96,73 +104,66 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 				print(variant_name + " is not a valid variant name for this plot")
 				return
 
-			# Doubling times
+			# get doubling time
 			dt = read_stacked_columns(
 				all_cells, 'Main', 'time',
 				fun=lambda x: (x[-1] - x[0]) / 60.).squeeze()
 			doubling_times[i] = np.mean(dt)
+
 			if variant == min_variant:
 				sim_dir = all_cells[0]
 				simOutDir = os.path.join(sim_dir, 'simOut')
 
-				# Extract protein indexes for each new gene
-				monomer_counts_reader = TableReader(os.path.join(
-					simOutDir, "MonomerCounts"))
-				monomer_idx_dict = {monomer: i for i, monomer in
-					enumerate(monomer_counts_reader.readAttribute('monomerIds'))}
-				new_gene_monomer_indexes = [monomer_idx_dict.get(monomer_id)
-					for monomer_id in new_gene_monomer_ids]
+				uniqueMoleculeCounts = TableReader(
+					os.path.join(simOutDir, "UniqueMoleculeCounts"))
+				mrr_index = uniqueMoleculeCounts.readAttribute(
+					"uniqueMoleculeIds").index('mrr')
 
-			avg_new_gene_monomer_counts = read_stacked_columns(all_cells,
-				'MonomerCounts', 'monomerCounts',
-				fun=lambda x: np.mean(x[:,new_gene_monomer_indexes],axis=0))
+			# get average mrr count
+			active_mrr_counts = read_stacked_columns(
+				all_cells, 'UniqueMoleculeCounts', 'uniqueMoleculeCounts',
+				remove_first=True,
+				ignore_exception=True)[:, mrr_index]
+			avg_mrr_count[i] = np.mean(active_mrr_counts)
 
-			avg_ng_monomer[i] = np.mean(avg_new_gene_monomer_counts)
-
-			if COLOR_BY == "translation_efficiency":
-				colors[i] = trl_eff_values[variant] / 10
-			elif COLOR_BY == "expression_factor":
-				colors[i] = expression_factors[variant] / 10
+			# calculate the ribosome concentration
+			counts_to_molar = read_stacked_columns(
+				all_cells, 'EnzymeKinetics', 'countsToMolar',
+				remove_first=True, ignore_exception=True).squeeze()
+			avg_mrr_conc[i] = np.mean(active_mrr_counts * counts_to_molar)
 
 		plt.figure()
-		plt.xlabel("New Gene Protein Counts")
-		plt.ylabel("Doubling Time")
-
-		if COLOR_BY != "same":
-			plt.scatter(
-				avg_ng_monomer[plot_variant_mask],
-				doubling_times[plot_variant_mask], c=colors, cmap='coolwarm')
-			plt.colorbar(orientation='horizontal', label=f'{COLOR_BY} / 10')
-		else:
-			plt.scatter(
-				avg_ng_monomer[plot_variant_mask],
-				doubling_times[plot_variant_mask])
-
-		# plt.plot(
-		# 	avg_ng_monomer[plot_variant_mask],
-		# 	np.poly1d(np.polyfit(avg_ng_monomer[plot_variant_mask],
-		# 	doubling_times[plot_variant_mask], 1))(
-		# 	avg_ng_monomer[plot_variant_mask]))
+		plt.xlabel("mrr Concentration")
+		plt.ylabel("Doubling Time (min)")
+		plt.scatter(
+			avg_mrr_conc[plot_variant_mask],
+			doubling_times[plot_variant_mask])
+		plt.plot(
+			avg_mrr_conc[plot_variant_mask],
+			np.poly1d(np.polyfit(avg_mrr_conc[plot_variant_mask],
+								 doubling_times[plot_variant_mask], 1))(
+				avg_mrr_conc[plot_variant_mask]))
 
 		plt.tight_layout()
-		exportFigure(plt, plotOutDir, plotOutFileName + "_" + COLOR_BY, metadata)
+		exportFigure(plt, plotOutDir, plotOutFileName, metadata)
 		plt.close('all')
 
 		if INTERACTIVE_PLOT:
 			import plotly.express as px
 
 			fig = px.scatter(
-				x=avg_ng_monomer[plot_variant_mask],
+				x=mrr_concentration[plot_variant_mask],
 				y=doubling_times[plot_variant_mask],
 				hover_name=np.array(variants)[plot_variant_mask],
-				labels={'x': 'New Gene Protein Counts', 'y': 'Doubling Time'},
+				labels={'x': 'mrr Concentration', 'y': 'Doubling Time (min)'},
 				hover_data={
-					'Expression Factor': expression_factors[plot_variant_mask],
+					'Expression Factor': np.log10(expression_factors[plot_variant_mask]),
 					'Translation Efficiency': trl_eff_values[plot_variant_mask]})
 
 			fig.write_html(os.path.join(
 				plotOutDir, plotOutFileName + "_" + COLOR_BY + ".html"))
 			fig.show()
+
 
 if __name__ == "__main__":
 	Plot().cli()
