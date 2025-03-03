912ffff9b75f844589f69bb22d2760b967a4ed1f
Yinglin Situ <yinglinsitu@Yinglins-MacBook-Pro.local>
2025-03-02 23:54:36 -0800

    edit ppgpp regulation file for analysis

--- models/ecoli/analysis/multigen/ppgpp_regulation.py
+++ models/ecoli/analysis/multigen/ppgpp_regulation.py
@@ -18,6 +18,12 @@ SPOT_RNA = 'EG10966_RNA'
 
 class Plot(multigenAnalysisPlot.MultigenAnalysisPlot):
 	def do_plot(self, seedOutDir, plotOutDir, plotOutFileName, simDataFile, validationDataFile, metadata):
+		# define variant and seed
+		VARIANT = 3
+		VARIANT_SEED = 2
+		START_GEN = 0
+		END_GEN = 24
+
 		# sim_data values
 		with open(simDataFile, 'rb') as f:
 			sim_data = pickle.load(f)
@@ -42,7 +48,9 @@ class Plot(multigenAnalysisPlot.MultigenAnalysisPlot):
 		synthase_order = ['relA', 'spoT']
 
 		# Load simulation output
-		cell_paths = self.ap.get_cells(variant=np.array([3]),seed=np.array([2]))
+		cell_paths = self.ap.get_cells(variant=np.array([VARIANT]),
+									   generation=np.arange(START_GEN, END_GEN),
+									   seed=np.array([VARIANT_SEED]))
 		time = read_stacked_columns(cell_paths, 'Main', 'time').squeeze() / 60  # min
 		counts_to_molar = read_stacked_columns(cell_paths, 'EnzymeKinetics', 'countsToMolar').squeeze()
 		synth_prob_per_cistron = read_stacked_columns(cell_paths, 'RnaSynthProb', 'actual_rna_synth_prob_per_cistron')
