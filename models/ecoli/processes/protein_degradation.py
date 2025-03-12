85f21910a0a63a0076bfaac0a9f0b4bacbea7a89
mpg19 <miagrahn@stanford.edu>
2025-03-11 22:19:56 -0700

    prepping NE_only1 run with more gens

--- models/ecoli/processes/protein_degradation.py
+++ models/ecoli/processes/protein_degradation.py
@@ -37,7 +37,7 @@ class ProteinDegradation(wholecell.processes.process.Process):
 	def initialize(self, sim, sim_data):
 		super(ProteinDegradation, self).initialize(sim, sim_data)
 
-		# Load protein degradation rates (based on N-end rule)
+		# Load protein degradation rates (based on N-end rule) # this is outdated? I think it is not only the N-end rule, it is whatever is specified in translation.py
 		self.rawDegRate = sim_data.process.translation.monomer_data['deg_rate'].asNumber(1 / units.s)
 
 		# Build metabolite IDs for S matrix
@@ -63,26 +63,36 @@ class ProteinDegradation(wholecell.processes.process.Process):
 		self.proteins = self.bulkMoleculesView(proteinIds)
 
 		self.bulkMoleculesRequestPriorityIs(REQUEST_PRIORITY_DEGRADATION)
+		#import ipdb
+		#ipdb.set_trace()
 
 	def calculateRequest(self):
 
+		hi = 5
+		#import ipdb
+		#ipdb.set_trace()
 		# Determine how many proteins to degrade based on the degradation rates and counts of each protein
 		nProteinsToDegrade = np.fmin(
 			self.randomState.poisson(self._proteinDegRates() * self.proteins.total_counts()),
 			self.proteins.total_counts()
-			)
+			) # for each protein, fmin selects which is smaller, the output of the possion function or the total counts of the protein to be the smallest. if the possion is NaN, fmin will choose the total protein counts value as the smallest, so it is a fail safe?
 
 		# Determine the number of hydrolysis reactions
-		nReactions = np.dot(self.proteinLengths.asNumber(), nProteinsToDegrade)
-
+		nReactions = np.dot(self.proteinLengths.asNumber(), nProteinsToDegrade) # this is doing the length of amminio acids (for each protein) by the # of proteins to degrade for that protein to figure out how many AAs then get removed for hydrolysis
+		#import ipdb
+		#ipdb.set_trace()
 		# Determine the amount of water required to degrade the selected proteins
 		# Assuming one N-1 H2O is required per peptide chain length N
 		self.h2o.requestIs(nReactions - np.sum(nProteinsToDegrade))
-		self.proteins.requestIs(nProteinsToDegrade)
+		self.proteins.requestIs(nProteinsToDegrade) # this is the number of proteins to degrade for each protein, I am assuming it feeds into something somewhere else to calculate the new # of proteins
+		#import ipdb
+		#ipdb.set_trace()
+
 
 
 	def evolveState(self):
-
+		#import ipdb
+		#ipdb.set_trace()
 		# Degrade selected proteins, release amino acids from those proteins back into the cell, 
 		# and consume H_2O that is required for the degradation process
 		self.metabolites.countsInc(np.dot(
@@ -90,6 +100,9 @@ class ProteinDegradation(wholecell.processes.process.Process):
 			self.proteins.counts()
 			))
 		self.proteins.countsIs(0)
+		#import ipdb
+		#ipdb.set_trace()
+
 
 	def _proteinDegRates(self):
 		return self.rawDegRate * self.timeStepSec()
