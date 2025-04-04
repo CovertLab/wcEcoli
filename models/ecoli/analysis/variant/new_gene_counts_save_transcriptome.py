a74579f227c38b13a6f5ac49704e806dbbe93a63
Riley Juenemann <rjuene@stanford.edu>
2025-04-03 22:39:57 -0700

    prep for sherlock run

--- models/ecoli/analysis/variant/new_gene_counts_save_transcriptome.py
+++ models/ecoli/analysis/variant/new_gene_counts_save_transcriptome.py
@@ -1,5 +1,5 @@
 """
-Save the average monomer counts for each variant index as a column in a csv file.
+Save the average RNA counts for each variant index as a column in a csv file.
 """
 
 import os
@@ -35,33 +35,29 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 			variant=[selected_variant_indexes[0]],
 			generation=np.arange(IGNORE_FIRST_N_GENS, n_total_gens),
 			only_successful=True)
-		# Load tables and attributes for proteins
-		monomer_reader = TableReader(
-			os.path.join(all_cells[0], 'simOut', 'MonomerCounts'))
-		monomer_ids = np.array(monomer_reader.readAttribute('monomerIds'))
 
-		# Determine which monomers correspond to essential genes
+		# Load tables and attributes for RNA cistrons
+		RNA_reader = TableReader(
+			os.path.join(all_cells[0], 'simOut', 'RNACounts'))
+		RNA_cistron_ids = np.array(RNA_reader.readAttribute('mRNA_cistron_ids'))
+
+		# Determine which RNAs correspond to essential genes
 		cistron_data = sim_data.process.transcription.cistron_data
-		cistron_ids = cistron_data['id']
-		# Filter list for cistron IDs with associated protein ids
-		protein_id_to_cistron_id = {
-			protein['id']: protein['cistron_id']
-			for protein in sim_data.process.translation.monomer_data}
-		intermediate_cistron_ids = [
-			protein_id_to_cistron_id[monomer_id] for monomer_id in monomer_ids]
 		cistron_id_to_gene_id = {
 			cistron['id']: cistron['gene_id'] for cistron in cistron_data}
 		gene_ids = [
-			cistron_id_to_gene_id[cistron_id] for cistron_id in intermediate_cistron_ids]
+			cistron_id_to_gene_id[cistron_id] for cistron_id in RNA_cistron_ids]
+
+		#TODO: rRNA have to be done separately?
 
 		essential_genes = validation_data.essential_genes.essential_genes
 		is_essential = np.array(
 			[True if gene_id in essential_genes else False for gene_id in gene_ids])
 
 		is_essential = np.reshape(is_essential, (len(is_essential), 1))
-		monomer_ids = np.reshape(monomer_ids, (len(monomer_ids), 1))
+		RNA_cistron_ids = np.reshape(RNA_cistron_ids, (len(RNA_cistron_ids), 1))
 		# Initialize a pandas dataframe to store the data
-		all_avg_monomer_counts = np.zeros((len(monomer_ids), n_variants))
+		all_avg_RNA_cistron_counts = np.zeros((len(RNA_cistron_ids), n_variants))
 
 		# Loop through variant indexes
 		for i, variant_index in enumerate(selected_variant_indexes):
@@ -76,23 +72,21 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 
 			# Read columns
 			# remove_first=True because countsToMolar is 0 at first time step
-			monomer_counts = read_stacked_columns(
-				all_cells, 'MonomerCounts', 'monomerCounts',
+			RNA_cistron_counts = read_stacked_columns(
+				all_cells, 'RNACounts', 'mRNA_cistron_counts',
 				remove_first=True, ignore_exception=True)
 
-			monomer_counts_avg = monomer_counts.mean(axis=0)
+			RNA_cistron_counts_avg = RNA_cistron_counts.mean(axis=0)
 
-			all_avg_monomer_counts[:, i] = np.copy(monomer_counts_avg)
+			all_avg_RNA_cistron_counts[:, i] = np.copy(RNA_cistron_counts_avg)
 
-		# Save all_avg_monomer_counts as a csv file with row names as monomer_ids
-		# and col names as selected_variant_indexes
-		all_labeled_avg_monomer_counts = np.hstack((monomer_ids, is_essential, all_avg_monomer_counts))
-		header = np.array(['monomer_ids'] + ['is_essential'] + selected_variant_indexes)
-		all_labeled_avg_monomer_counts = np.vstack((header, all_labeled_avg_monomer_counts))
+		all_labeled_avg_RNA_cistron_counts = np.hstack((RNA_cistron_ids, is_essential, all_avg_RNA_cistron_counts))
+		header = np.array(['RNA_cistron_ids'] + ['is_essential'] + selected_variant_indexes)
+		all_labeled_avg_RNA_cistron_counts = np.vstack((header, all_labeled_avg_RNA_cistron_counts))
 
 		np.savetxt(
-			os.path.join(plotOutDir, 'all_labeled_avg_monomer_counts.csv'),
-			all_labeled_avg_monomer_counts, delimiter=',', fmt='%s')
+			os.path.join(plotOutDir, 'all_labeled_avg_RNA_cistron_counts.csv'),
+			all_labeled_avg_RNA_cistron_counts, delimiter=',', fmt='%s')
 
 if __name__ == "__main__":
 	Plot().cli()
