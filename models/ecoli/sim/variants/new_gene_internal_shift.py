4db199015b87de7fca8fe172fc2a836353c2078d
Riley Juenemann <rjuene@stanford.edu>
2025-09-20 11:29:48 -0700

    multiple gfp copies

--- models/ecoli/sim/variants/new_gene_internal_shift.py
+++ models/ecoli/sim/variants/new_gene_internal_shift.py
@@ -71,16 +71,23 @@ if NEW_GENE_KNOCKOUT_GEN != -1:
 
 # The variant index will be split into an index for each of these lists
 # which are written to simulation metadata for later use in analysis scripts
+
 # NEW_GENE_EXPRESSION_FACTORS = [0, 7, 8, 9, 10]
 # NEW_GENE_TRANSLATION_EFFICIENCY_VALUES = [10, 5, 1, 0.1, 0] # Should be descending
-NEW_GENE_EXPRESSION_FACTORS = [0, 8.5]
-NEW_GENE_TRANSLATION_EFFICIENCY_VALUES = [ # Should be descending
-    3.40, 0.0]
+
+# NEW_GENE_EXPRESSION_FACTORS = [0, 8.5]
+# NEW_GENE_TRANSLATION_EFFICIENCY_VALUES = [ # Should be descending
+#     3.40, 0.0]
 #	3.40, 3.25, 3.10, 2.95, 2.80,
 #	2.65, 2.50, 2.35, 2.20, 2.05,
 #	1.90, 1.75, 1.60, 1.45, 1.30,
 #	1.15, 1.00, 0.85, 0.70, 0.55,
 #	0.40, 0.25, 0.10, 0]
+
+NEW_GENE_EXPRESSION_FACTORS = [0, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10]
+# NEW_GENE_EXPRESSION_FACTORS = [0, 10]
+NEW_GENE_TRANSLATION_EFFICIENCY_VALUES = [ # Should be descending
+    0.0]
 SEPARATOR = len(NEW_GENE_TRANSLATION_EFFICIENCY_VALUES)
 assert NEW_GENE_EXPRESSION_FACTORS[0] == 0, (
 	"The first new gene expression factor should always be the control sim")
