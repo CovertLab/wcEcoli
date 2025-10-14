3fce8eaf642809e4fe3ebb507db096bfcf766266
mpg19 <miagrahn@stanford.edu>
2025-10-14 14:42:14 -0700

    Added Gupta et al., 2024 protein degradation rate file to knowledge_base_raw.py.

--- reconstruction/ecoli/knowledge_base_raw.py
+++ reconstruction/ecoli/knowledge_base_raw.py
@@ -57,6 +57,7 @@ LIST_OF_DICT_FILENAMES = [
 	"ppgpp_regulation.tsv",
 	"ppgpp_regulation_added.tsv",
 	"ppgpp_regulation_removed.tsv",
+	"protein_half_lives_Clim3a.tsv",
 	"protein_half_lives_measured.tsv",
 	"protein_half_lives_n_end_rule.tsv",
 	"protein_half_lives_pulsed_silac.tsv",
