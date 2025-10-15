e880f26eb6e12bf7c1d8be97d1036b359aebd836
mpg19 <miagrahn@stanford.edu>
2025-10-14 17:28:23 -0700

    Added protein degradation rate combination selection to initRawData.py.py.

--- wholecell/fireworks/firetasks/initRawData.py
+++ wholecell/fireworks/firetasks/initRawData.py
@@ -5,6 +5,7 @@ from fireworks import FiretaskBase, explicit_serialize
 from reconstruction.ecoli.knowledge_base_raw import KnowledgeBaseEcoli
 from wholecell.utils.constants import DEFAULT_OPERON_OPTION
 from wholecell.utils.constants import DEFAULT_NEW_GENES_OPTION
+from wholecell.utils.constants import DEFAULT_PROTEIN_DEGRADATION_COMBO
 
 
 @explicit_serialize
@@ -15,6 +16,7 @@ class InitRawDataTask(FiretaskBase):
 	optional_params = [
 		'operons',
 		'new_genes',
+		'protein_degradation_combo',
 		'remove_rrna_operons',
 		'remove_rrff',
 		'stable_rrna',
@@ -27,9 +29,13 @@ class InitRawDataTask(FiretaskBase):
 		new_gene_option = self.get('new_genes') or DEFAULT_NEW_GENES_OPTION
 		print(f"{time.ctime()}: Instantiating raw_data with new_genes={new_gene_option}")
 
+		protein_degradation_combo = self.get('protein_degradation_combo') or DEFAULT_PROTEIN_DEGRADATION_COMBO
+		print(f"{time.ctime()}: Instantiating raw_data with protein_degradation_combo={protein_degradation_combo}")
+
 		raw_data = KnowledgeBaseEcoli(
 			operons_on=(operon_option == 'on'),
 			new_genes_option=new_gene_option,
+			protein_degradation_combo_option=protein_degradation_combo,
 			remove_rrna_operons=self.get('remove_rrna_operons', False),
 			remove_rrff=self.get('remove_rrff', False),
 			stable_rrna=self.get('stable_rrna', False),
