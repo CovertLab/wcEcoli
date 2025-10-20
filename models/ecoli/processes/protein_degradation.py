0cc2b0a378f8d261f51e5cd13bcc2df8b2b97b3f
mpg19 <miagrahn@stanford.edu>
2025-10-20 16:00:12 -0700

    Edited an error with Km values and made the default False.

--- models/ecoli/processes/protein_degradation.py
+++ models/ecoli/processes/protein_degradation.py
@@ -13,7 +13,7 @@ import wholecell.processes.process
 from wholecell.utils.constants import REQUEST_PRIORITY_DEGRADATION
 from wholecell.utils import units
 
-DYNAMIC_PD = True # todo: make this a simulation or ParCa parameter option
+DYNAMIC_PD = False # todo: make this a simulation or ParCa parameter option
 DYNAMIC_PD_TYPE = 1 # 1 is first order, 2 is optimized degradation with beta
 
 class ProteinDegradation(wholecell.processes.process.Process):
@@ -91,19 +91,24 @@ class ProteinDegradation(wholecell.processes.process.Process):
 			) # NOTE: this will be overwritten for some proteins below
 
 			# Extract the counts_to_molar conversion factor:
-			cell_mass = self.readFromListener("Mass",
-											  "cellMass") * units.fg  # if confused on how this works, look at 05.05.2025 notes
-			# convert cell_mass to g:
-			cell_mass_g = cell_mass.asNumber(units.g)  # now in units of g
-			cell_mass_g = cell_mass_g  # todo: determine if this should be put back in: * units.g # physically puts a "[g]" unit on it
-			cell_density = self.readFromListener("Mass",
-												 "cellDensity")  # todo: figure out if needed (or if the constant value can be assumed here)
-			cell_volume = cell_mass_g / cell_density  # L
-			counts_to_molar = 1 / (
+			def get_counts_to_molar():
+				cell_mass = self.readFromListener("Mass",
+												  "cellMass") * units.fg  # if confused on how this works, look at 05.05.2025 notes
+				# convert cell_mass to g:
+				cell_mass_g = cell_mass.asNumber(units.g)  # now in units of g
+				cell_mass_g = cell_mass_g  # todo: determine if this should be put back in: * units.g # physically puts a "[g]" unit on it
+				cell_density = self.readFromListener("Mass",
+													 "cellDensity")  # todo: figure out if needed (or if the constant value can be assumed here)
+				cell_volume = cell_mass_g / cell_density  # L
+				counts_to_molar = 1 / (
 						self.n_avogadro * cell_volume)  # mol/L ? # todo: confirm units
+
+				return counts_to_molar
+
 
 			# Function for extracting the protease concentration:
 			def get_protease_concentration(protease):
+				counts_to_molar = get_counts_to_molar()
 				protease_idx = np.where(self.complex_IDs == protease)[0][0]
 				lon_complex_counts = self.complexes.total_counts()[
 					protease_idx]  # todo: go back and figure out which lon complex ids are most important (total_counts, _totalCounts, etc.)
@@ -112,6 +117,7 @@ class ProteinDegradation(wholecell.processes.process.Process):
 
 			# Function for extracting the substrate concentrations:
 			def get_substrate_concentrations(substrates):
+				counts_to_molar = get_counts_to_molar()
 				concentrations = []
 				for substrate in substrates:
 					substrate_idx = np.where(self.protein_IDs == substrate)[0][0]
@@ -119,6 +125,28 @@ class ProteinDegradation(wholecell.processes.process.Process):
 												  substrate_idx] * counts_to_molar
 					concentrations.append(substrate_concentration) # mol/L [=] M
 				return concentrations
+
+			# Function for degrading proteins based on k_active values:
+			def degrade_proteins(substrates, k_actives, nProteinsToDegrade):
+				counts_to_molar = get_counts_to_molar()
+				# Obtain the substrate concentrations:
+				substrate_concentrations = get_substrate_concentrations(substrates)
+				for i in range(len(substrate_concentrations)):
+					# Find the protein index in the bulk view:
+					substrate_idx = np.where(self.protein_IDs == substrates[i])[0][0]
+
+					# Calculate the number of proteins degraded:
+					proteins_degraded = k_actives[i] * substrate_concentrations[i]
+
+					# Convert to counts and round:
+					proteins_degraded = proteins_degraded / counts_to_molar
+					proteins_degraded = round(proteins_degraded)
+
+					# Degrade the proteins
+					nProteinsToDegrade[
+						substrate_idx] = proteins_degraded  # todo: check to see that this is working
+
+				return nProteinsToDegrade
 
 			# List the protease:
 			protease = 'CPLX0-2881[c]'  # this is Lon
@@ -139,7 +167,7 @@ class ProteinDegradation(wholecell.processes.process.Process):
 				# Define parameters for dynamic degradation model:
 				kcat = 0.071  # 1/s, https://jbioleng.biomedcentral.com/articles/10.1186/1754-1611-6-9#Sec29
 				# these are from a first order approximation using the half life and average concentrations:
-				Kms = [0.1867, 0.0692, 0.2517, 0.4397, 0.4095, 0,3202] # todo: add units
+				Kms = [0.1867, 0.0692, 0.2517, 0.4397, 0.4095, 0.3202] # todo: add units
 				# NOTE: pay attention to substrate order when inputting here too!
 				self.dynamic_PD_Kms = Kms # todo: add units
 
@@ -159,31 +187,10 @@ class ProteinDegradation(wholecell.processes.process.Process):
 
 					return k_actives
 
-
-				# Determine the proteins to degrade based on active degradation rates:
-				def degrade_proteins_first_order(substrates, k_actives, nProteinsToDegrade):
-					# Obtain the substrate concentrations:
-					substrate_concentrations = get_substrate_concentrations(substrates)
-					for i in range(len(substrate_concentrations)):
-						# Find the protein index in the bulk view:
-						substrate_idx = np.where(self.protein_IDs == substrates[i])[0][0]
-
-						# Calculate the number of proteins degraded:
-						proteins_degraded = k_actives[i] * substrate_concentrations[i]
-
-						# Convert back to counts and round:
-						proteins_degraded = proteins_degraded / counts_to_molar
-						proteins_degraded = round(proteins_degraded)
-
-						# Degrade the proteins accordingly:
-						nProteinsToDegrade[substrate_idx] = proteins_degraded # todo: check that this is working properly
-
-					return nProteinsToDegrade
-
 				# Call the functions:
 				k_actives = calculate_first_order_k_active(protease, substrates, kcat, Kms)
 				# reassign selected proteins in nProteinsToDegrade based on active degradation:
-				nProteinsToDegrade = degrade_proteins_first_order(substrates, k_actives, nProteinsToDegrade)
+				nProteinsToDegrade = degrade_proteins(substrates, k_actives, nProteinsToDegrade)
 
 			if DYNAMIC_PD_TYPE == 2:
 				# Optimized dynamic protein degradation model
@@ -218,32 +225,9 @@ class ProteinDegradation(wholecell.processes.process.Process):
 						k_actives.append(k_active)
 					return k_actives
 
-				# Determine the proteins to degrade based on active degradation rates:
-				# TODO: determine if this function will remain the same every simulation run, and see if just the kactive function can handle the changes so that this can be placed above the method selection.
-				def degrade_proteins(substrates, k_actives, nProteinsToDegrade):
-					# Obtain the substrate concentrations:
-					substrate_concentrations = get_substrate_concentrations(substrates)
-					for i in range(len(substrate_concentrations)):
-						# Find the protein index in the bulk view:
-						substrate_idx = np.where(self.protein_IDs == substrates[i])[0][0]
-
-						# Calculate the number of proteins degraded:
-						proteins_degraded = k_actives[i] * substrate_concentrations[i]
-
-						# Convert to counts and round:
-						proteins_degraded = proteins_degraded / counts_to_molar
-						proteins_degraded = round(proteins_degraded)
-
-						# Degrade the proteins
-						nProteinsToDegrade[substrate_idx] = proteins_degraded # todo: check to see that this is working
-
-					return nProteinsToDegrade
-
-				# call the function:
+				# Call the functions:
 				k_actives = calculate_k_active(protease, substrates, kcat, Kms)
 				nProteinsToDegrade = degrade_proteins(substrates, k_actives, nProteinsToDegrade)
-
-
 
 		else:
 			# Use default model protein degradtion:
