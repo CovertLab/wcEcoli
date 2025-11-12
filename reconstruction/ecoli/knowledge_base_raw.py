6575f4dd40ff18f5cbe394c7fc6ee427057f102d
mpg19 <miagrahn@stanford.edu>
2025-11-12 13:44:58 -0800

    WIP file

--- reconstruction/ecoli/knowledge_base_raw.py
+++ reconstruction/ecoli/knowledge_base_raw.py
@@ -80,6 +80,7 @@ LIST_OF_DICT_FILENAMES = [
 	"ppgpp_regulation.tsv",
 	"ppgpp_regulation_added.tsv",
 	"protease_assignments_Clim0_TEST.tsv",
+	"priority_protease_assignments_0.tsv",
 	"ppgpp_regulation_removed.tsv",
 	"protein_half_lives_Clim3a.tsv",
 	"protein_half_lives_measured.tsv",
