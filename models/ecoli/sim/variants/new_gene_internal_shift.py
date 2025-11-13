8bd027e168f285063729c63d36bafc897dac970b
Riley Juenemann <rjuene@stanford.edu>
2025-11-13 12:03:50 -0800

    add new listeners for metabolism

--- models/ecoli/sim/variants/new_gene_internal_shift.py
+++ models/ecoli/sim/variants/new_gene_internal_shift.py
@@ -71,15 +71,15 @@ if NEW_GENE_KNOCKOUT_GEN != -1:
 
 # The variant index will be split into an index for each of these lists
 # which are written to simulation metadata for later use in analysis scripts
-# NEW_GENE_EXPRESSION_FACTORS = [0, 7, 8, 9, 10]
-# NEW_GENE_TRANSLATION_EFFICIENCY_VALUES = [10, 5, 1, 0.1, 0] # Should be descending
-NEW_GENE_EXPRESSION_FACTORS = [0, 8.5]
-NEW_GENE_TRANSLATION_EFFICIENCY_VALUES = [ # Should be descending
-        3.40, 3.25, 3.10, 2.95, 2.80,
-        2.65, 2.50, 2.35, 2.20, 2.05,
-        1.90, 1.75, 1.60, 1.45, 1.30,
-        1.15, 1.00, 0.85, 0.70, 0.55,
-        0.40, 0.25, 0.10, 0]
+NEW_GENE_EXPRESSION_FACTORS = [0, 7, 8, 9, 10]
+NEW_GENE_TRANSLATION_EFFICIENCY_VALUES = [10, 5, 1, 0.1, 0] # Should be descending
+# NEW_GENE_EXPRESSION_FACTORS = [0, 8.5]
+# NEW_GENE_TRANSLATION_EFFICIENCY_VALUES = [ # Should be descending
+#         3.40, 3.25, 3.10, 2.95, 2.80,
+#         2.65, 2.50, 2.35, 2.20, 2.05,
+#         1.90, 1.75, 1.60, 1.45, 1.30,
+#         1.15, 1.00, 0.85, 0.70, 0.55,
+#         0.40, 0.25, 0.10, 0]
 
 SEPARATOR = len(NEW_GENE_TRANSLATION_EFFICIENCY_VALUES)
 assert NEW_GENE_EXPRESSION_FACTORS[0] == 0, (
