0d662ebf1c79eb7ebad39e48550ea19567c907dd
Riley Juenemann <rjuene@stanford.edu>
2025-11-15 14:21:18 -0800

    plots for comparing weighted and unweighted objective values

--- models/ecoli/analysis/variant/metabolism_kinetic_objective_weight_expanded.py
+++ models/ecoli/analysis/variant/metabolism_kinetic_objective_weight_expanded.py
@@ -12,7 +12,7 @@ import numpy as np
 from models.ecoli.analysis import variantAnalysisPlot
 from wholecell.analysis.analysis_tools import exportFigure, read_stacked_columns
 
-IGNORE_FIRST_N_GENS = 0
+IGNORE_FIRST_N_GENS = 4
 
 class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 	def do_plot(self, inputDir, plotOutDir, plotOutFileName, simDataFile, validationDataFile, metadata):
@@ -23,6 +23,7 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 		homeostatic_weights = np.zeros(len(variant_indexes))
 		kinetic_weights = np.zeros(len(variant_indexes))
 		secretion_penalty_coeffs = np.zeros(len(variant_indexes))
+		completion_rates = np.zeros(len(variant_indexes))
 		avg_homeostatic_obj_values = np.zeros(len(variant_indexes))
 		avg_kinetic_obj_values = np.zeros(len(variant_indexes))
 		avg_overall_obj_values = np.zeros(len(variant_indexes))
@@ -51,6 +52,23 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 			if len(all_cells) == 0:
 				continue
 
+			# Count the number of simulations that reach the last generation
+			num_last_gen = len(
+				self.ap.get_cells(
+					variant=[variant_index], generation=[n_total_gens - 1],
+					only_successful=True)
+				)
+
+			# Count the number of simulations at generation zero
+			num_zero_gen = len(
+				self.ap.get_cells(
+					variant=[variant_index], generation=[0],
+					only_successful=True)
+				)
+
+			# Calculate completion rate
+			completion_rates[i] = num_last_gen / num_zero_gen
+
 			# Get the homeostatic objective value
 			# Weight is 1 for homeostatic so weighted is the same as unweighted
 			homeostatic_obj_values = np.sum(read_stacked_columns(
@@ -76,10 +94,16 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 		plt.figure(figsize=(8.5, 22))
 		subplots = 9
 
-		# TODO: percentage of sims that finished
+		ax = plt.subplot(subplots, 1, 1)
+		plt.bar(
+			[str(w) for w in kinetic_weights],
+			completion_rates,
+			)
+		plt.ylabel(f'% of Seeds\nCompleting {n_total_gens} Gens', fontsize=8)
+		plt.xlabel('Kinetic Objective Weight')
 
 		# Overall objective value
-		ax = plt.subplot(subplots, 1, 1)
+		ax = plt.subplot(subplots, 1, 2)
 		plt.bar(
 			[str(w) for w in kinetic_weights],
 			avg_overall_obj_values,
@@ -88,7 +112,7 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 		plt.xlabel('Kinetic Objective Weight')
 
 		# Weighted kinetic objective value
-		ax = plt.subplot(subplots, 1, 2)
+		ax = plt.subplot(subplots, 1, 3)
 		plt.bar(
 			[str(w) for w in kinetic_weights],
 			avg_kinetic_obj_values * kinetic_weights,
@@ -97,7 +121,7 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 		plt.xlabel('Kinetic Objective Weight')
 
 		# Unweighted kinetic objective value
-		ax = plt.subplot(subplots, 1, 3)
+		ax = plt.subplot(subplots, 1, 4)
 		plt.bar(
 			[str(w) for w in kinetic_weights],
 			avg_kinetic_obj_values,
@@ -106,7 +130,7 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 		plt.xlabel('Kinetic Objective Weight')
 
 		# Homeostatic objective Value
-		ax = plt.subplot(subplots, 1, 4)
+		ax = plt.subplot(subplots, 1, 5)
 		plt.bar(
 			[str(w) for w in kinetic_weights],
 			avg_homeostatic_obj_values,
@@ -115,7 +139,7 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 		plt.xlabel('Kinetic Objective Weight')
 
 		# Weighted leftover objective value
-		ax = plt.subplot(subplots, 1, 5)
+		ax = plt.subplot(subplots, 1, 6)
 		plt.bar(
 			[str(w) for w in kinetic_weights],
 			(
@@ -127,7 +151,7 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 		plt.xlabel('Kinetic Objective Weight')
 
 		# Weighted leftover objective value divided by secretion penalty
-		ax = plt.subplot(subplots, 1, 6)
+		ax = plt.subplot(subplots, 1, 7)
 		plt.bar(
 			[str(w) for w in kinetic_weights],
 			(
@@ -138,14 +162,17 @@ class Plot(variantAnalysisPlot.VariantAnalysisPlot):
 		plt.ylabel('Weighted Leftover\nObjective Value\nDivided by Secretion Penalty Coeff', fontsize=8)
 		plt.xlabel('Kinetic Objective Weight')
 
-		# TODO: homeostatic objective / kinetic objective? maybe log scale?
+		ax = plt.subplot(subplots, 1, 8)
+		plt.bar(
+			[str(w) for w in kinetic_weights],
+			avg_homeostatic_obj_values / (avg_homeostatic_obj_values * kinetic_weights),
+		)
+		plt.ylabel('Ratio of Homeostatic\nto Kinetic\nObjective Value', fontsize=8)
+		plt.xlabel('Kinetic Objective Weight')
 
 		plt.subplots_adjust(hspace=0.6)
 		exportFigure(plt, plotOutDir, '{}_obj'.format(plotOutFileName), metadata)
 		plt.close('all')
-
-		# import ipdb
-		# ipdb.set_trace()
 
 if __name__ == "__main__":
 	Plot().cli()
