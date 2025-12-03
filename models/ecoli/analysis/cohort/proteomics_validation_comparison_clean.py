f246f9f7e67965cb8051fe102b3b71f59882d043
mpg19 <miagrahn@stanford.edu>
2025-12-03 09:48:37 -0800

    Added TODO.

--- models/ecoli/analysis/cohort/proteomics_validation_comparison_clean.py
+++ models/ecoli/analysis/cohort/proteomics_validation_comparison_clean.py
@@ -1005,7 +1005,7 @@ class Plot(cohortAnalysisPlot.CohortAnalysisPlot):
 
         # Make the plots of the validation data source comparisons (against other validation sources):
         # TODO: edit this so it is not filtered by what common names match with the simulation, rather just the full set of common name data available in each validation dataset.
-
+        # TODO: ^ well caution with this, because I want it to match the most updated names
         # Compare the validation data generated from the sim (Schmidt et al. 2015 BW25113) to the Schmidt et al. 2016 ST9 MG1655 data:
         self.compare_validation_dataset_to_validation_dataset(plotOutDir,
             "Simulation Validation Data from Schmidt et al. 2015 (BW25113)", "Schmidt2015_ST6_BW",
