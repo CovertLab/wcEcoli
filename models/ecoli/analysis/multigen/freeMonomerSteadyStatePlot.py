7b8b9c2fc128be414990863758d6c4b963aafec5
mpg19 <miagrahn@stanford.edu>
2026-01-26 17:31:31 -0800

    Created plots with the monomers that never make complexes, monomers in complexation complexes, and monomers in eq complexes plotted ONLY.

--- models/ecoli/analysis/multigen/freeMonomerSteadyStatePlot.py
+++ models/ecoli/analysis/multigen/freeMonomerSteadyStatePlot.py
@@ -23,6 +23,11 @@ HIGHLIGHT_IN_RED = ["PD03867[c]", "EG11734-MONOMER[c]"]#['EG10863-MONOMER[c]','D
 HIGHLIGHT_IN_BLUE = []#['CARBPSYN-SMALL[c]', 'CDPDIGLYSYN-MONOMER[i]','EG10743-MONOMER[c]','GLUTCYSLIG-MONOMER[c]']
 HIGHLIGHT_IN_PURPLE = []#['G6890-MONOMER[c]','PD03938[c]','G6737-MONOMER[c]','RPOD-MONOMER[c]','PD02936[c]','RED-THIOREDOXIN2-MONOMER[c]']
 
+# temporary flag to highlight proteins that make complexes:
+PLOT_BY_COMPLEX_TYPE = True
+
+# TODO: add average complexed counts and average total counts!
+
 
 # function to match gene symbols to monomer ids
 def get_gene_symbols_for_monomer_ids():
@@ -90,7 +95,7 @@ class Plot(multigenAnalysisPlot.MultigenAnalysisPlot):
                             enumerate(monomer_counts_reader.readAttribute(
                                 'monomerIds'))}
 
-        # extract the numbers of interest
+        # extract the monomer IDs of interest
         monomerIds = monomer_counts_reader.readAttribute(
             "monomerIds")  # this is the one that matches the indexing  I used earlier to construct the listeners!
 
@@ -214,17 +219,48 @@ class Plot(multigenAnalysisPlot.MultigenAnalysisPlot):
         def hover_text(protein_ids, x, y, half_lives, common_names, complexation_complex_info,
                        equilibrium_complex_info, average_free_monomer_counts):
             texts = []
+
+            def complex_info_formatter(complexation_info_entry):
+                if isinstance(complexation_info_entry, dict) and complexation_info_entry:
+                    complexation_entries = []
+                    for comp_id, comp_data in complexation_info_entry.items():
+                        if isinstance(comp_data, list) and comp_data:
+                            entry_info = {}
+                            for entry in comp_data:
+                                entry_info.update(entry)
+
+                            # Extract the necessary values
+                            reaction_id = entry_info.get('reaction_id', 'N/A')
+                            stoichiometry = entry_info.get('stoichiometry', 'N/A')
+                            stoich_unknown = entry_info.get('stoich_unknown', 'N/A')
+                            complex_type = entry_info.get('complex_type', 'N/A')
+
+                            # Format the info:
+                            formatted_info = f"{comp_id}, {reaction_id}, {stoichiometry}, {stoich_unknown}, {complex_type}"
+                            complexation_entries.append(formatted_info)
+
+                    # Join the entries into a single string
+                    return ';<br>'.join(
+                        complexation_entries) if complexation_entries else 'No Complexation Info'
+                else:
+                    return 'No Complexation Info'
+
             for i in range(len(protein_ids)):
+                # Extracting complexation and equilibrium info:
+                complexation_info_str = complex_info_formatter(complexation_complex_info[i])
+                equilibrium_info_str = complex_info_formatter(equilibrium_complex_info[i])
+
                 texts.append((
                     f"{protein_ids[i]}<br>"
                     f"Production Rate: {x[i]:.2f}<br>"
                     f"Loss Rate: {y[i]:.2f}<br>"
                     f"Average Free Monomer Count: {average_free_monomer_counts[i]:.2f}<br>"
-                    f"Half Life: {half_lives[i]}<br>"
+                    f"Half Life (mins): {half_lives[i]}<br>"
                     f"Common Name: {common_names[i]}<br>"
-                    f"<span style='font-size: 10px;'>Complexation Info: {complexation_complex_info[i]}<br>"
-                    f"Equilibrium Info: {equilibrium_complex_info[i]}"
+                    f"<span style='font-size: 10px;'>Complexation Info (ID, rxn, stoich, stoich unknown, type): {complexation_info_str}<br>"
+                    f"<span style='font-size: 10px;'>Equilibrium Info (ID, rxn, stoich, stoich unknown, type): {equilibrium_info_str}</span>"
                 ))
+
             return texts
 
         # plot the loss rate vs the production rate:
@@ -310,14 +346,230 @@ class Plot(multigenAnalysisPlot.MultigenAnalysisPlot):
             showlegend=True,
         )
 
-        #save the plot:
+        # Save the plot:
         plot_name = plotOutFileName +"_"+ sim_id +"_red_" + str(HIGHLIGHT_IN_RED) + "_blue_" + str(HIGHLIGHT_IN_BLUE) + "_purple_" + str(HIGHLIGHT_IN_PURPLE) + ".html"
         fig.write_html(os.path.join(plotOutDir, plot_name))
-        #exportFigure(plt, plotOutDir, plotOutFileName, metadata)
+
+        # Function to add lines:
+        def add_lines(fig):
+            # Generate line data
+            x = np.linspace(-4, 3, 100)
+
+            # y = x line (black)
+            fig.add_trace(go.Scatter(
+                x=x, y=x, mode='lines', line=dict(color='black', width=2, ),
+                name='y = x'
+            ))
+
+            # y = x + 1 line (green, dashed)
+            fig.add_trace(go.Scatter(
+                x=x, y=x + 1, mode='lines',
+                line=dict(color='green', width=2, dash='dash'), name='y = x + 1'
+            ))
+
+            # y = x - 1 line (green, dashed)
+            fig.add_trace(go.Scatter(
+                x=x, y=x - 1, mode='lines',
+                line=dict(color='green', width=2, dash='dash'), name='y = x - 1'
+            ))
+
+        # Plot other plots if specified:
+        # TODO: add two-component and ribosome and replisome complexation types
+        if PLOT_BY_COMPLEX_TYPE:
+            # Find all proteins that make complexes:
+            molecules_to_all_downstream_complexes = sim_data.process.complexation.molecules_to_all_downstream_complexes_dict
+            monomers_in_complexes = list(molecules_to_all_downstream_complexes.keys())
+            molecules_to_all_downstream_eq_complexes = sim_data.process.equilibrium.molecules_to_all_downstream_complexes_dict
+            monomers_in_eq_complexes = list(molecules_to_all_downstream_eq_complexes.keys())
+
+            # Find all proteins not in complexes:
+            monomers_not_in_complexes = [monomer for monomer in monomerIds if
+                                        monomer not in monomers_in_complexes and
+                                        monomer not in monomers_in_eq_complexes]
+
+            # Create separate figure for complexation monomers
+            fig_complexation = go.Figure()
+            complexation_monomer_indices = [monomer_idx_dict[monomer] for monomer in
+                                            monomers_in_complexes if
+                                            monomer in monomerIds]
+            # Accessing attributes with proper indexing
+            if len(complexation_monomer_indices) > 0:
+                hover_info_complexation = hover_text(
+                    [monomerIds[i] for i in complexation_monomer_indices],
+                    log_avg_production_rate[complexation_monomer_indices],
+                    log_avg_loss_rate[complexation_monomer_indices],
+                    half_lives[complexation_monomer_indices],
+                    [common_names[i] for i in complexation_monomer_indices],
+                    [complexation_complex_info[i] for i in complexation_monomer_indices],
+                    [equilibrium_complex_info[i] for i in complexation_monomer_indices],
+                    average_free_monomer_counts[complexation_monomer_indices]
+                )
+
+                fig_complexation.add_trace(go.Scatter(
+                    x=log_avg_production_rate[complexation_monomer_indices],
+                    y=log_avg_loss_rate[complexation_monomer_indices],
+                    mode='markers',
+                    hovertext=hover_info_complexation,
+                    marker=dict(size=4, color='orange', opacity=.5),
+                    name=f'Monomers in Complexation Complexes (n={len(complexation_monomer_indices)})'
+                ))
+
+                add_lines(fig_complexation)
+
+            fig_complexation.update_layout(
+                title=f"Free Monomer Average Loss Rate vs Average Production Rate<br>Sim ID: {sim_id}",
+                xaxis_title="Log10 Average Production Rate",
+                yaxis_title="Log10 Average Loss Rate",
+                width=700, height=700
+            )
+
+            # Save the complexation plot
+            complexation_plot_name = "complexation_monomers_" + sim_id + ".html"
+            fig_complexation.write_html(os.path.join(plotOutDir, complexation_plot_name))
+
+            # Create separate figure for equilibrium monomers
+            fig_equilibrium = go.Figure()
+            equilibrium_monomer_indices = [monomer_idx_dict[monomer] for monomer in
+                                           monomers_in_eq_complexes if
+                                           monomer in monomerIds]
+
+            if len(equilibrium_monomer_indices) > 0:
+                hover_info_equilibrium = hover_text(
+                    [monomerIds[i] for i in equilibrium_monomer_indices],
+                    log_avg_production_rate[equilibrium_monomer_indices],
+                    log_avg_loss_rate[equilibrium_monomer_indices],
+                    half_lives[equilibrium_monomer_indices],
+                    [common_names[i] for i in equilibrium_monomer_indices],
+                    [complexation_complex_info[i] for i in equilibrium_monomer_indices],
+                    [equilibrium_complex_info[i] for i in equilibrium_monomer_indices],
+                    average_free_monomer_counts[equilibrium_monomer_indices]
+                )
+
+                fig_equilibrium.add_trace(go.Scatter(
+                    x=log_avg_production_rate[equilibrium_monomer_indices],
+                    y=log_avg_loss_rate[equilibrium_monomer_indices],
+                    mode='markers',
+                    hovertext=hover_info_equilibrium,
+                    marker=dict(size=5, color='lightseagreen', opacity=.6),
+                    name=f'Monomers in Equilibrium Complexes (n={len(equilibrium_monomer_indices)})'
+                ))
+
+                add_lines(fig_equilibrium)
+
+            fig_equilibrium.update_layout(
+                title=f"Free Monomer Average Loss Rate vs Average Production Rate<br>Sim ID: {sim_id}",
+                xaxis_title="Log10 Average Production Rate",
+                yaxis_title="Log10 Average Loss Rate",
+                width=700, height=700
+            )
+
+            # Save the equilibrium plot
+            equilibrium_plot_name = "equilibrium_monomers_" + sim_id + ".html"
+            fig_equilibrium.write_html(os.path.join(plotOutDir, equilibrium_plot_name))
+
+            # Create separate figure for the monomer that makes no complexes
+            fig_no_complex = go.Figure()
+
+            if len(monomers_not_in_complexes) > 0:
+                no_complex_monomer_indices = [monomer_idx_dict[monomer] for monomer in
+                                              monomers_not_in_complexes if
+                                              monomer in monomerIds]
+
+                hover_info_no_complex = hover_text(
+                    [monomerIds[i] for i in no_complex_monomer_indices],
+                    log_avg_production_rate[no_complex_monomer_indices],
+                    log_avg_loss_rate[no_complex_monomer_indices],
+                    half_lives[no_complex_monomer_indices],
+                    [common_names[i] for i in no_complex_monomer_indices],
+                    [complexation_complex_info[i] for i in no_complex_monomer_indices],
+                    [equilibrium_complex_info[i] for i in no_complex_monomer_indices],
+                    average_free_monomer_counts[no_complex_monomer_indices]
+                )
+
+                fig_no_complex.add_trace(go.Scatter(
+                    x=log_avg_production_rate[no_complex_monomer_indices],
+                    y=log_avg_loss_rate[no_complex_monomer_indices],
+                    mode='markers',
+                    hovertext=hover_info_no_complex,
+                    marker=dict(size=4, color='salmon', opacity=.6),
+                    name=f'monomers in zero complexes (n={len(no_complex_monomer_indices)})'
+                ))
+
+                add_lines(fig_no_complex)
+
+            fig_no_complex.update_layout(
+                title=f"Free Monomer Average Loss Rate vs Average Production Rate<br>Sim ID: {sim_id}",
+                xaxis_title="Log10 Average Production Rate",
+                yaxis_title="Log10 Average Loss Rate",
+                width=700, height=700
+            )
+
+            # Save the no complex plot
+            no_complex_plot_name = "no_complex_monomer_" + sim_id + ".html"
+            fig_no_complex.write_html(os.path.join(plotOutDir, no_complex_plot_name))
+
+            # Create a combined figure for all three categories
+            fig_combined = go.Figure()
+
+            # Plot monomers that make no complexes
+            fig_combined.add_trace(go.Scatter(
+                x=log_avg_production_rate[no_complex_monomer_indices],
+                y=log_avg_loss_rate[no_complex_monomer_indices],
+                mode='markers',
+                hovertext=hover_info_no_complex,
+                marker=dict(size=3, color='salmon', opacity=.5),
+                name=f'Monomers never in Complexes (n={len(no_complex_monomer_indices)})'
+            ))
+
+            # Monomers in complexation complexes
+            fig_combined.add_trace(go.Scatter(
+                x=log_avg_production_rate[complexation_monomer_indices],
+                y=log_avg_loss_rate[complexation_monomer_indices],
+                mode='markers',
+                hovertext=hover_info_complexation,
+                marker=dict(size=3, color='orange', opacity=0.4),
+                name=f'Monomers in Complexation Complexes (n={len(complexation_monomer_indices)})'
+            ))
+
+            # Monomers in equilibrium complexes
+            fig_combined.add_trace(go.Scatter(
+                x=log_avg_production_rate[equilibrium_monomer_indices],
+                y=log_avg_loss_rate[equilibrium_monomer_indices],
+                mode='markers',
+                hovertext=hover_info_equilibrium,
+                marker=dict(size=5, color='green', opacity=0.6),
+                name=f'Monomers in Equilibrium Complexes (n={len(equilibrium_monomer_indices)})'
+            ))
 
 
-        # compute the average synthesis rate for each protein:
-        avg_synthesis_rate = avg_elongated_counts
+            add_lines(fig_combined)
+
+            # TODO: fix where the legend falls on the plot!
+
+            fig_combined.update_layout(
+                title=f"Free Monomer Average Loss Rate vs Average Production Rate<br>Sim ID: {sim_id}",
+                xaxis_title="Log10 Average Production Rate",
+                yaxis_title="Log10 Average Loss Rate",
+                width=700, height=700,
+                margin=dict(l=40, r=40, t=50, b=40),
+                legend=dict(
+                    x=.01,
+                    y=0.99,
+                    traceorder='normal',
+                    font=dict(size=12),
+                ),
+                title_font=dict(size=16),
+                xaxis=dict(title_font=dict(size=14)),
+                yaxis=dict(title_font=dict(size=14)),
+            )
+
+            # Lock the aspect ratio to keep it square
+            fig.update_yaxes(scaleanchor="x", scaleratio=1)
+
+
+            # Save the combined plot
+            combined_plot_name = "combined_monomers_" + sim_id + ".html"
+            fig_combined.write_html(os.path.join(plotOutDir, combined_plot_name))
 
 
 if __name__ == '__main__':
