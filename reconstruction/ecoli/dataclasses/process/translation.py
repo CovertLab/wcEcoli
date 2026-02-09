d1005bdec8ee7f78a89c0887a204967b10078934
mpg19 <miagrahn@stanford.edu>
2026-02-08 16:36:58 -0800

    Small updates to comment lines.

--- reconstruction/ecoli/dataclasses/process/translation.py
+++ reconstruction/ecoli/dataclasses/process/translation.py
@@ -181,7 +181,8 @@ class Translation(object):
 				# If measured rates are unavailable, use N-end rule
 				else:
 					seq = protein['seq']
-					assert seq[0] == 'M'  # All protein sequences should start with methionine
+					# All protein sequences should start w/ methionine:
+					assert seq[0] == 'M'
 					# Set N-end residue as second amino acid if initial methionine
 					# is cleaved
 					n_end_residue = seq[protein['cleavage_of_initial_methionine']]
@@ -205,8 +206,8 @@ class Translation(object):
 				# If measured rates are unavailable, use N-end rule
 				else:
 					seq = protein['seq']
-					assert seq[0] == 'M'  # All protein sequences should start
-					# with methionine
+					# All protein sequences should start w/ methionine:
+					assert seq[0] == 'M'
 					# Set N-end residue as second amino acid if initial methionine
 					# is cleaved
 					n_end_residue = seq[protein['cleavage_of_initial_methionine']]
@@ -230,10 +231,10 @@ class Translation(object):
 				# If measured rates are unavailable, use N-end rule
 				else:
 					seq = protein['seq']
-					assert seq[0] == 'M'  # All protein sequences should start
-					# with methionine
-					# Set N-end residue as second amino acid if initial methionine
-					# is cleaved
+					# All protein sequences should start w/ methionine:
+					assert seq[0] == 'M'
+					# Set N-end residue as 2nd amino acid if initial methionine
+					# is cleaved:
 					n_end_residue = seq[protein['cleavage_of_initial_methionine']]
 					deg_rate[i] = n_end_rule_deg_rates[n_end_residue]
 					half_life_source_ID[i] = 'N_end_rule'
